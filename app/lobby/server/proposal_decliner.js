ff5201be92ffa328e6966ae26756e63099b8abb3
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-10 11:52:30 -0500

    Convert Decline Notification to Use New Streamy Methods

--- app/lobby/server/proposal_decliner.js
+++ app/lobby/server/proposal_decliner.js
@@ -2,8 +2,6 @@ ProposalDecliner = class ProposalDecliner {
 
   constructor(proposal_id) {
     this.proposal = Proposals.findOne(proposal_id)
-    this.player_ids = this.find_player_ids()
-    this.player_sockets = this.find_player_sockets()
   }
 
   decline() {
@@ -12,29 +10,21 @@ ProposalDecliner = class ProposalDecliner {
   }
 
   notify_players() {
-    _.each(this.player_sockets, this.send_decline)
+    Streamy.groupEmit('decline',
+      {
+        decliner: {
+          id: Meteor.userId(),
+          username: Meteor.user().username
+        }
+      },
+      Streamy.userSockets(this.player_ids())
+    )
   }
 
-  send_decline(socket) {
-    Streamy.emit('decline', {
-      decliner: {
-        id: Meteor.userId(),
-        username: Meteor.user().username
-      }
-    }, socket)
-  }
-
-  find_player_sockets() {
-    return _.filter(Streamy.sockets(), this.is_participating_player.bind(this))
-  }
-
-  find_player_ids() {
+  player_ids() {
     return _.map(this.proposal.players, function(player) {
       return player.id
     })
   }
 
-  is_participating_player(socket) {
-    return this.player_ids.indexOf(socket._meteorSession.userId) != -1
-  }
 }
