e452476c03a5ffa4b6d598663d65fa2ab4f7c488
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-16 15:53:26 -0500

    Assign Game Id to Players In Game

--- app/lobby/server/game_creater.js
+++ app/lobby/server/game_creater.js
@@ -6,9 +6,22 @@ GameCreater = class GameCreater {
   }
 
   create() {
-    Games.insert({
+    game_id = this.create_game()
+    this.assign_game_to_players()
+  }
+
+  create_game() {
+    return Games.insert({
       players: this.players,
       cards: this.cards
+    })
+  }
+
+  assign_game_to_players() {
+    _.each(this.players, function(player) {
+      Meteor.users.update(player._id, {
+        $set: {current_game: game_id}
+      })
     })
   }
 
