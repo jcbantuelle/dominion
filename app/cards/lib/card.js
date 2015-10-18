79d05ce743062f38fa7687eced452d0bf53c5cd2
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-18 10:37:39 -0500

    Generate Common Cards During Game Creation

--- app/cards/lib/card.js
+++ app/cards/lib/card.js
@@ -1,9 +1,16 @@
 Card = class Card {
 
+  potion_cost() {
+    return 0
+  }
+
   to_h() {
     return {
       name: s.humanize(this.constructor.name),
-      types: this.types().join(' ')
+      image: s.underscored(this.constructor.name),
+      types: this.types().join(' '),
+      coin_cost: this.coin_cost(),
+      potion_cost: this.potion_cost()
     }
   }
 }
--- app/lobby/server/game_creater.js
+++ app/lobby/server/game_creater.js
@@ -13,7 +13,8 @@ GameCreater = class GameCreater {
   create_game() {
     return Games.insert({
       players: this.players,
-      cards: this.cards
+      kingdom_cards: this.kingdom_cards(),
+      common_cards: this.common_cards()
     })
   }
 
@@ -23,6 +24,71 @@ GameCreater = class GameCreater {
         $set: {current_game: game_id}
       })
     })
+  }
+
+  kingdom_cards() {
+    return _.map(this.cards, (card) => {
+      return this.game_card(card)
+    })
+  }
+
+  common_cards() {
+    let cards = _.map(this.common_card_names(), function(card_name) {
+      let card = new this[card_name]
+      return card.to_h()
+    })
+
+    return _.map(cards, (card) => {
+      return this.game_card(card)
+    })
+  }
+
+  common_card_names() {
+    return this.victory_card_names().concat(this.treasure_card_names()).concat(this.miscellaneous_card_names())
+  }
+
+  victory_card_names() {
+    return ['Estate','Duchy','Province']
+  }
+
+  treasure_card_names() {
+    return ['Copper','Silver','Gold']
+  }
+
+  miscellaneous_card_names() {
+    return ['Curse']
+  }
+
+  game_card(card) {
+    let card_stack = this.create_card_stack(card)
+    return {
+      name: card.name,
+      count: _.size(card_stack),
+      top_card: _.first(card_stack),
+      stack: card_stack
+    }
+  }
+
+  create_card_stack(card) {
+    return _.times(this.stack_size(card), function(counter) {
+      return card
+    })
+  }
+
+  stack_size(card) {
+    if (s.include(card.types, 'victory')) {
+      return _.size(this.players) < 3 ? 8 : 12
+    } else if (card.name === 'Curse') {
+      return _.size(this.players) === 1 ? 10 : _.size(this.players) * 10 - 10
+    } else if (card.name === 'Copper') {
+      return 60
+    } else if (card.name === 'Silver') {
+      return 40
+    } else if (card.name === 'Gold') {
+      return 30
+    } else {
+      return 10
+    }
   }
 
 }
