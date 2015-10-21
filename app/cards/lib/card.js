0851d1bd83edfd71b1a9167402d8e626247064a8
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-21 16:51:29 -0500

    Move Card Class Functionality Into Methods for Reuse

--- app/cards/lib/card.js
+++ app/cards/lib/card.js
@@ -4,11 +4,23 @@ Card = class Card {
     return 0
   }
 
+  name() {
+    return s.humanize(this.constructor.name)
+  }
+
+  image() {
+    return s.underscored(this.constructor.name)
+  }
+
+  type_class() {
+    return this.types().join(' ')
+  }
+
   to_h() {
     return {
-      name: s.humanize(this.constructor.name),
-      image: s.underscored(this.constructor.name),
-      types: this.types().join(' '),
+      name: this.name(),
+      image: this.image(),
+      types: this.type_class(),
       coin_cost: this.coin_cost(),
       potion_cost: this.potion_cost()
     }
