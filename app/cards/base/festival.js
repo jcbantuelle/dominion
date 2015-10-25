d54d7a27bbd08b822e79e6129c602d912479cc66
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-25 11:35:38 -0500

    Implement Festival

--- app/cards/base/festival.js
+++ app/cards/base/festival.js
@@ -8,4 +8,14 @@ Festival = class Festival extends Card {
     return 5
   }
 
+  play() {
+    this.game = Games.findOne(Meteor.user().current_game)
+
+    this.game.turn.actions += 2
+    this.game.turn.buys += 1
+    this.game.turn.coins += 2
+    this.game.log.push(`&nbsp;&nbsp;<strong>${Meteor.user().username}</strong> gets +2 actions, +1 buy, and +$2`)
+    Games.update(this.game._id, this.game)
+  }
+
 }
