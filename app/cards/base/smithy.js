f89ea2061ec1f324af035b455e1f59b4a586dfd9
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-25 17:37:01 -0500

    Implement Smithy

--- app/cards/base/smithy.js
+++ app/cards/base/smithy.js
@@ -8,4 +8,11 @@ Smithy = class Smithy extends Card {
     return 4
   }
 
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(player_cards, game);
+    [player_cards, game] = card_drawer.draw(3, true)
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+  }
+
 }
