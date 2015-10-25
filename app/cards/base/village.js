cbdec2541096af2e4b2929956924e3da00b81f55
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-25 11:23:13 -0500

    Implement Village

--- app/cards/base/village.js
+++ app/cards/base/village.js
@@ -8,4 +8,22 @@ Village = class Village extends Card {
     return 3
   }
 
+  play() {
+    this.game = Games.findOne(Meteor.user().current_game)
+    this.player_cards = PlayerCards.findOne({
+      player_id: Meteor.userId(),
+      game_id: this.game._id
+    })
+
+    let card_drawer = new CardDrawer(this.player_cards, this.game)
+    let [player_cards, game] = card_drawer.draw(1, true)
+    this.player_cards = player_cards
+    this.game = game
+
+    this.game.turn.actions += 2
+    this.game.log.push(`&nbsp;&nbsp;<strong>${Meteor.user().username}</strong> gets +2 actions`)
+    Games.update(this.game._id, this.game)
+    PlayerCards.update(this.player_cards._id, this.player_cards)
+  }
+
 }
