ffa9f81c47ad310cd1311082e6e34cfe0d9f9de0
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-27 21:15:13 -0500

    Implement Card Trasher Service

---
+++ app/game/server/card_trasher.js
@@ -0,0 +1,34 @@
+CardTrasher = class CardTrasher {
+
+  constructor(game, username, source, card_name) {
+    this.game = game
+    this.username = username
+    this.source = source
+    this.card_name = card_name
+  }
+
+  trash() {
+    this.card_index = this.find_card_index()
+    if (this.card_index !== -1) {
+      this.update_log()
+      this.trash_card()
+    }
+    return [this.game, this.source]
+  }
+
+  find_card_index() {
+    return _.findIndex(this.source, (card) => {
+      return card.name === this.card_name
+    })
+  }
+
+  trash_card() {
+    this.game.trash.push(this.source[this.card_index])
+    this.source.splice(this.card_index, 1)
+  }
+
+  update_log(card) {
+    this.game.log.push(`<strong>${this.username}</strong> trashes <span class="${this.source[this.card_index].types}">${this.source[this.card_index].name}</span>`)
+  }
+
+}
