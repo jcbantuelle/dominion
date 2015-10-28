1e25b017ef4fc865c4b988f119af45b5bb43703a
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-28 09:10:15 -0500

    Implement Witch

--- app/cards/base/witch.js
+++ app/cards/base/witch.js
@@ -8,4 +8,25 @@ Witch = class Witch extends Card {
     return 5
   }
 
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(player_cards, game);
+    [player_cards, game] = card_drawer.draw(2)
+
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+  }
+
+  attack(game, player) {
+    let player_cards = PlayerCards.findOne({
+      player_id: player._id,
+      game_id: game._id
+    })
+
+    let card_gainer = new CardGainer(game, player.username, player_cards.discard, 'Curse');
+    [game, player_cards.discard] = card_gainer.gain_common_card()
+
+    PlayerCards.update(player_cards._id, player_cards)
+    Games.update(game._id, game)
+  }
+
 }
