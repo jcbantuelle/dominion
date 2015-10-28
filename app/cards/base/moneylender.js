6ac112faffcd5f8983507be6f7966e9fdf584cf7
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-27 21:15:26 -0500

    Refactor Moneylender To Use Card Trasher

--- app/cards/base/moneylender.js
+++ app/cards/base/moneylender.js
@@ -9,24 +9,18 @@ Moneylender = class Moneylender extends Card {
   }
 
   play(game, player_cards) {
-    let copper = this.find_copper(player_cards)
-    if (copper !== -1) {
-      game.trash.push(player_cards.hand[copper])
+    let original_hand_size = _.size(player_cards.hand)
+    let card_trasher = new CardTrasher(game, player_cards.username, player_cards.hand, 'Copper');
+    [game, player_cards.hand] = card_trasher.trash()
+    if (_.size(player_cards.hand) < original_hand_size) {
       game.turn.coins += 3
-      player_cards.hand.splice(copper, 1)
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> trashes a <span class="treasure">Copper</span>, getting +$3`)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$3`)
     } else {
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no copper to trash`)
     }
 
     Games.update(game._id, game)
     PlayerCards.update(player_cards._id, player_cards)
-  }
-
-  find_copper(player_cards) {
-    return _.findIndex(player_cards.hand, function(card) {
-      return card.name === 'Copper'
-    })
   }
 
 }
