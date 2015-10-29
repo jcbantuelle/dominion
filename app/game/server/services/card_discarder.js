f767fd6218d24a470a2eed51779b960e3bcb85b0
Justin Bantuelle <justin@mudbugmedia.com>
2015-10-29 10:16:48 -0500

    Implement Partial Discard Mechanic

--- app/game/server/services/card_discarder.js
+++ app/game/server/services/card_discarder.js
@@ -15,13 +15,31 @@ CardDiscarder = class CardDiscarder {
     return [this.game, this.player_cards]
   }
 
+  discard_some(cards, announce = true) {
+    if (announce) {
+      this.update_log(cards)
+    }
+    this.move_to_discard(cards)
+    this.remove_from_source(cards)
+    return [this.game, this.player_cards]
+  }
+
   move_to_discard(cards) {
     this.player_cards.discard = cards.concat(this.player_cards.discard)
   }
 
+  remove_from_source(cards) {
+    _.each(cards, (card) => {
+      let card_index = _.findIndex(this.player_cards[this.source], function(source_card) {
+        return source_card.name === card.name
+      })
+      this.player_cards[this.source].splice(card_index, 1)
+    })
+  }
+
   update_log(cards) {
     let card_list = _.map(cards, function(card) {
-      `<span class="${card.types}">${card.name}</span>`
+      return `<span class="${card.types}">${card.name}</span>`
     }).join(' ')
     this.game.log.push(`&nbsp;&nbsp;<strong>${this.player_cards.username}</strong> discards ${card_list}`)
   }
