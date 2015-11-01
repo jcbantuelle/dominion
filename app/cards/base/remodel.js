78afb594c5e08dd96a4cf9d9c22347f6fbf2072e
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-01 10:44:23 -0600

    Implement Remodel

--- app/cards/base/remodel.js
+++ app/cards/base/remodel.js
@@ -8,4 +8,68 @@ Remodel = class Remodel extends Card {
     return 4
   }
 
+  play(game, player_cards) {
+    if (_.size(player_cards.hand) > 0) {
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Choose a card to trash:',
+        cards: player_cards.hand,
+        minimum: 1,
+        maximum: 1,
+        finished: false
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      return turn_event_processor.process(Remodel.trash_card)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are cards in hand`)
+      Games.update(game._id, game)
+    }
+  }
+
+  static trash_card(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+
+    let card_trasher = new CardTrasher(game, player_cards.username, player_cards.hand, selected_card.name)
+    card_trasher.trash()
+
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+
+    let eligible_cards = _.filter(game.kingdom_cards.concat(game.common_cards), function(card) {
+      return card.count > 0 && card.top_card.purchasable && card.top_card.coin_cost <= (selected_card.coin_cost + 2) && card.top_card.potion_cost <= selected_card.potion_cost
+    })
+
+    if (_.size(eligible_cards) > 0) {
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        game_cards: true,
+        instructions: 'Choose a card to gain:',
+        cards: eligible_cards,
+        minimum: 1,
+        maximum: 1,
+        finished: false
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      return turn_event_processor.process(Remodel.gain_card)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+      Games.update(game._id, game)
+    }
+  }
+
+  static gain_card(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+    let card_gainer = new CardGainer(game, player_cards.username, player_cards.discard, selected_card.name)
+    card_gainer[`gain_${selected_card.source}_card`]()
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+  }
+
 }
