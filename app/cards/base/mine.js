5e41d74b2a303197d0ec1a214f8e907898d87d34
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-01 11:58:03 -0600

    Implement Mine

--- app/cards/base/mine.js
+++ app/cards/base/mine.js
@@ -8,4 +8,73 @@ Mine = class Mine extends Card {
     return 5
   }
 
+  play(game, player_cards) {
+    let eligible_cards = _.filter(player_cards.hand, function(card) {
+      return _.contains(card.types, 'treasure')
+    })
+
+    if (_.size(eligible_cards) > 0) {
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Choose a treasure to trash:',
+        cards: eligible_cards,
+        minimum: 1,
+        maximum: 1,
+        finished: false
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      return turn_event_processor.process(Mine.trash_card)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no treasures in hand`)
+      Games.update(game._id, game)
+    }
+  }
+
+  static trash_card(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+
+    let card_trasher = new CardTrasher(game, player_cards.username, player_cards.hand, selected_card.name)
+    card_trasher.trash()
+
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+
+    let eligible_cards = _.filter(game.kingdom_cards.concat(game.common_cards), function(card) {
+      return card.count > 0 && card.top_card.purchasable && _.contains(card.top_card.types, 'treasure') && card.top_card.coin_cost <= (selected_card.coin_cost + 3) && card.top_card.potion_cost <= selected_card.potion_cost
+    })
+
+    if (_.size(eligible_cards) > 0) {
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        game_cards: true,
+        instructions: 'Choose a card to gain:',
+        cards: eligible_cards,
+        minimum: 1,
+        maximum: 1,
+        finished: false
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      return turn_event_processor.process(Mine.gain_card)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no available treasures to gain`)
+      Games.update(game._id, game)
+    }
+  }
+
+  static gain_card(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+    let card_gainer = new CardGainer(game, player_cards.username, player_cards.hand, selected_card.name)
+    card_gainer[`gain_${selected_card.source}_card`]()
+    game.log.push(`&nbsp;&nbsp;putting it in hand`)
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+  }
+
 }
