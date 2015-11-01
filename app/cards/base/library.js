0976a377c56ad73c5d7c4df9fa48391b886d24c1
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-01 12:25:59 -0600

    Implement Library

--- app/cards/base/library.js
+++ app/cards/base/library.js
@@ -8,4 +8,64 @@ Library = class Library extends Card {
     return 5
   }
 
+  play(game, player_cards) {
+    if (_.size(player_cards.deck) > 0 || _.size(player_cards.discard) > 0) {
+      player_cards.aside = []
+      Library.draw_cards(game, player_cards)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no cards to draw`)
+      Games.update(game._id, game)
+    }
+  }
+
+  static draw_cards(game, player_cards) {
+    if (_.size(player_cards.hand) >= 7 || (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0)) {
+      if (_.size(player_cards.aside) > 0) {
+        let card_discarder = new CardDiscarder(game, player_cards, 'aside')
+        card_discarder.discard_all(true)
+      }
+      delete player_cards.aside
+      Games.update(game._id, game)
+      PlayerCards.update(player_cards._id, player_cards)
+    } else {
+      if (_.size(player_cards.deck) === 0) {
+        let deck_shuffler = new DeckShuffler(player_cards)
+        deck_shuffler.shuffle()
+      }
+      let top_card = player_cards.deck.shift()
+      if (_.contains(top_card.types, 'action')) {
+        player_cards.pending = top_card
+        let turn_event_id = TurnEvents.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_yes_no',
+          instructions: `Put <span class="${player_cards.pending.types}">${player_cards.pending.name}</span> in Hand?`,
+          minimum: 1,
+          maximum: 1,
+          finished: false
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        return turn_event_processor.process(Library.card_choice)
+      } else {
+        player_cards.hand.push(top_card)
+        PlayerCards.update(player_cards._id, player_cards)
+        Library.draw_cards(game, player_cards)
+      }
+    }
+  }
+
+  static card_choice(game, player_cards, response) {
+    if (response === 'yes') {
+      player_cards.hand.push(player_cards.pending)
+      PlayerCards.update(player_cards._id, player_cards)
+    } else {
+      player_cards.aside.push(player_cards.pending)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> sets aside <span class="${player_cards.pending.types}">${player_cards.pending.name}</span>`)
+      Games.update(game._id, game)
+    }
+    delete player_cards.pending
+    Library.draw_cards(game, player_cards)
+  }
+
 }
