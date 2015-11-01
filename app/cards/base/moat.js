2c5ee622a96f66474da9afeade5a81722e1ced71
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-01 17:58:28 -0600

    Implement Moat

--- app/cards/base/moat.js
+++ app/cards/base/moat.js
@@ -8,4 +8,16 @@ Moat = class Moat extends Card {
     return 2
   }
 
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(2)
+
+    Games.update(game._id, game)
+    PlayerCards.update(player_cards._id, player_cards)
+  }
+
+  attack_reaction(game, player_cards) {
+    player_cards.moat = true
+  }
+
 }
