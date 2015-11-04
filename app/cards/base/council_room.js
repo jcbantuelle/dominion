d45b310445e41f99bb6306396bbdd0a50c495690
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-03 19:12:34 -0600

    Remove Unnecessary DB Writes From Council Room

--- app/cards/base/council_room.js
+++ app/cards/base/council_room.js
@@ -12,16 +12,14 @@ CouncilRoom = class CouncilRoom extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(4)
 
+    game.turn.buys += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy`)
+
     this.other_players_cards(game, player_cards.username).forEach(function(other_player_cards) {
       let other_player_card_drawer = new CardDrawer(game, other_player_cards)
       other_player_card_drawer.draw(1)
       PlayerCards.update(other_player_cards._id, other_player_cards)
     })
-
-    game.turn.buys += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy`)
-    Games.update(game._id, game)
-    PlayerCards.update(player_cards._id, player_cards)
   }
 
 }
