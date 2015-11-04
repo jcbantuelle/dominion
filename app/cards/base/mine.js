3733df7a833ae4156a32940ed17fc07ba9551b8f
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-03 18:22:21 -0600

    Remove Unnecessary DB Writes From Mine

--- app/cards/base/mine.js
+++ app/cards/base/mine.js
@@ -23,14 +23,12 @@ Mine = class Mine extends Card {
         instructions: 'Choose a treasure to trash:',
         cards: eligible_cards,
         minimum: 1,
-        maximum: 1,
-        finished: false
+        maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      return turn_event_processor.process(Mine.trash_card)
+      turn_event_processor.process(Mine.trash_card)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no treasures in hand`)
-      Games.update(game._id, game)
     }
   }
 
@@ -39,9 +37,6 @@ Mine = class Mine extends Card {
 
     let card_trasher = new CardTrasher(game, player_cards.username, player_cards.hand, selected_card.name)
     card_trasher.trash()
-
-    Games.update(game._id, game)
-    PlayerCards.update(player_cards._id, player_cards)
 
     let eligible_cards = _.filter(game.kingdom_cards.concat(game.common_cards), function(card) {
       return card.count > 0 && card.top_card.purchasable && _.contains(card.top_card.types, 'treasure') && card.top_card.coin_cost <= (selected_card.coin_cost + 3) && card.top_card.potion_cost <= selected_card.potion_cost
@@ -57,14 +52,12 @@ Mine = class Mine extends Card {
         instructions: 'Choose a card to gain:',
         cards: eligible_cards,
         minimum: 1,
-        maximum: 1,
-        finished: false
+        maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      return turn_event_processor.process(Mine.gain_card)
+      turn_event_processor.process(Mine.gain_card)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available treasures to gain`)
-      Games.update(game._id, game)
     }
   }
 
@@ -73,8 +66,6 @@ Mine = class Mine extends Card {
     let card_gainer = new CardGainer(game, player_cards.username, player_cards.hand, selected_card.name)
     card_gainer[`gain_${selected_card.source}_card`]()
     game.log.push(`&nbsp;&nbsp;putting it in hand`)
-    Games.update(game._id, game)
-    PlayerCards.update(player_cards._id, player_cards)
   }
 
 }
