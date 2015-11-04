e97dae090b359ae44f4cf9d2c9bcd5902780c5fb
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-03 19:10:09 -0600

    Remove Unnecessary DB Writes From Feast

--- app/cards/base/feast.js
+++ app/cards/base/feast.js
@@ -17,8 +17,6 @@ Feast = class Feast extends Card {
     })
 
     if (_.size(eligible_cards) > 0) {
-      Games.update(game._id, game)
-      PlayerCards.update(player_cards._id, player_cards)
       let turn_event_id = TurnEvents.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -28,15 +26,12 @@ Feast = class Feast extends Card {
         instructions: 'Choose a card to gain:',
         cards: eligible_cards,
         minimum: 1,
-        maximum: 1,
-        finished: false
+        maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      return turn_event_processor.process(Feast.gain_card)
+      turn_event_processor.process(Feast.gain_card)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
-      Games.update(game._id, game)
-      PlayerCards.update(player_cards._id, player_cards)
     }
   }
 
@@ -44,8 +39,6 @@ Feast = class Feast extends Card {
     let selected_card = selected_cards[0]
     let card_gainer = new CardGainer(game, player_cards.username, player_cards.discard, selected_card.name)
     card_gainer[`gain_${selected_card.source}_card`]()
-    Games.update(game._id, game)
-    PlayerCards.update(player_cards._id, player_cards)
   }
 
 }
