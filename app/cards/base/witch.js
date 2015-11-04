e74d478cdd53833cdd448fb577b42e111404b6ce
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-03 19:29:48 -0600

    Remove Unnecessary DB Writes From Witch

--- app/cards/base/witch.js
+++ app/cards/base/witch.js
@@ -11,22 +11,11 @@ Witch = class Witch extends Card {
   play(game, player_cards) {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(2)
-
-    Games.update(game._id, game)
-    PlayerCards.update(player_cards._id, player_cards)
   }
 
-  attack(game, player) {
-    let player_cards = PlayerCards.findOne({
-      player_id: player._id,
-      game_id: game._id
-    })
-
-    let card_gainer = new CardGainer(game, player.username, player_cards.discard, 'Curse')
+  attack(game, player_cards) {
+    let card_gainer = new CardGainer(game, player_cards.username, player_cards.discard, 'Curse')
     card_gainer.gain_common_card()
-
-    PlayerCards.update(player_cards._id, player_cards)
-    Games.update(game._id, game)
   }
 
 }
