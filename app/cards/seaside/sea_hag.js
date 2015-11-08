a7c3232d0dce556a9dd0d84b32ad3c6782dca6ac
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-07 19:43:32 -0600

    Improve Sea Hag Feedback

--- app/cards/seaside/sea_hag.js
+++ app/cards/seaside/sea_hag.js
@@ -22,6 +22,7 @@ SeaHag = class SeaHag extends Card {
       player_cards.revealed.push(player_cards.deck.shift())
       let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
       card_discarder.discard_all()
+      game.log.push(`&nbsp;&nbsp;from the top of their deck`)
     }
 
     let card_gainer = new CardGainer(game, player_cards.username, player_cards.deck, 'Curse')
