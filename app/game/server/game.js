63b40d95d92b1ee78a81223860de4c7111fa29e3
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-07 19:49:56 -0600

    Add Semaphore Lock on Server Actions to Prevent Concurrency Issues

--- app/game/server/game.js
+++ app/game/server/game.js
@@ -7,21 +7,37 @@ Meteor.methods({
   },
   playCard: function(card_name) {
     Future.task(Meteor.bindEnvironment(function() {
-      let card_player = new CardPlayer(game(), player_cards(), card_name)
-      card_player.play()
+      if (!ActionLock) {
+        ActionLock = true
+        let card_player = new CardPlayer(game(), player_cards(), card_name)
+        card_player.play()
+        ActionLock = false
+      }
     })).detach()
   },
   buyCard: function(card_name) {
-    let card_buyer = new CardBuyer(game(), player_cards(), card_name)
-    card_buyer.buy()
+    if (!ActionLock) {
+      ActionLock = true
+      let card_buyer = new CardBuyer(game(), player_cards(), card_name)
+      card_buyer.buy()
+      ActionLock = false
+    }
   },
   endTurn: function() {
-    let turn_ender = new TurnEnder(game(), player_cards())
-    turn_ender.end_turn()
+    if (!ActionLock) {
+      ActionLock = true
+      let turn_ender = new TurnEnder(game(), player_cards())
+      turn_ender.end_turn()
+      ActionLock = false
+    }
   },
   playAllCoin: function() {
-    let all_coin_player = new AllCoinPlayer(game(), player_cards())
-    all_coin_player.play()
+    if (!ActionLock) {
+      ActionLock = true
+      let all_coin_player = new AllCoinPlayer(game(), player_cards())
+      all_coin_player.play()
+      ActionLock = false
+    }
   },
   turnEvent: function(selected_cards) {
     let turn_event = TurnEvents.findOne({
