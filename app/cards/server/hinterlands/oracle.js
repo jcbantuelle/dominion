81f37098a5c74bcfdcfebcf400bfd67558ccdc67
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-11 17:51:24 -0600

    Implement Oracle

---
+++ app/cards/server/hinterlands/oracle.js
@@ -0,0 +1,78 @@
+Oracle = class Oracle extends Card {
+
+  types() {
+    return ['action', 'attack']
+  }
+
+  coin_cost() {
+    return 3
+  }
+
+  play(game, player_cards) {
+    Oracle.reveal_cards(game, player_cards)
+
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(2)
+  }
+
+  attack(game, player_cards) {
+    Oracle.reveal_cards(game, player_cards)
+  }
+
+  static reveal_cards(game, player_cards) {
+    if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards in deck`)
+    } else {
+      if (_.size(player_cards.deck) === 0) {
+        this.shuffle_discard(player_cards)
+      }
+
+      player_cards.revealed = _.take(player_cards.deck, 2)
+      player_cards.deck = _.drop(player_cards.deck, 2)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.revealed)}`)
+
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: game.turn.player._id,
+        username: game.turn.player.username,
+        type: 'choose_yes_no',
+        instructions: `Make <strong>${player_cards.username}</strong> discard ${CardView.render(player_cards.revealed)}?`,
+        minimum: 1,
+        maximum: 1
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Oracle.discard_card)
+    }
+  }
+
+  static discard_card(game, player_cards, response) {
+    if (response === 'yes') {
+      let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
+      card_discarder.discard_all()
+    } else {
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'sort_cards',
+        instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
+        cards: player_cards.revealed
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Oracle.replace_cards)
+    }
+  }
+
+  static replace_cards(game, player_cards, ordered_card_names) {
+    _.each(ordered_card_names.reverse(), function(card_name) {
+      let revealed_card_index = _.findIndex(player_cards.revealed, function(card) {
+        return card.name === card_name
+      })
+      let revealed_card = player_cards.revealed.splice(revealed_card_index, 1)[0]
+      player_cards.deck.unshift(revealed_card)
+    })
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places the cards back on their deck`)
+    player_cards.revealed = []
+  }
+
+}
