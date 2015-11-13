ead14608e75a3197e34baa874682a8f018b4c3b5
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-12 19:44:52 -0600

    Implement Noble Brigand

---
+++ app/cards/server/hinterlands/noble_brigand.js
@@ -0,0 +1,89 @@
+NobleBrigand = class NobleBrigand extends Card {
+
+  types() {
+    return ['action', 'attack']
+  }
+
+  coin_cost() {
+    return 4
+  }
+
+  play(game, player_cards) {
+    game.turn.coins += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$1`)
+
+    this.attack_action(game, player_cards)
+  }
+
+  buy_event(buyer) {
+    this.attack_action(buyer.game, buyer.player_cards)
+  }
+
+  attack_action(game, player_cards) {
+    game.turn.trashed_treasures = []
+    let player_attacker = new PlayerAttacker(game, this)
+    player_attacker.attack()
+    _.each(game.turn.trashed_treasures, function(treasure) {
+      let card_gainer = new CardGainer(game, player_cards, 'discard', treasure.name)
+      card_gainer.gain_trash_card()
+    })
+    delete game.turn.trashed_treasure
+  }
+
+  attack(game, player_cards) {
+    if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards in deck`)
+    } else {
+      if (_.size(player_cards.deck) < 2 && _.size(player_cards.discard) > 0) {
+        DeckShuffler.shuffle(player_cards)
+      }
+      player_cards.revealed = _.take(player_cards.deck, 2)
+      player_cards.deck = _.drop(player_cards.deck, 2)
+
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.revealed)}`)
+
+      let revealed_treasures = _.filter(player_cards.revealed, function(card) {
+        return _.contains(['Silver', 'Gold'], card.name)
+      })
+      if (_.isEmpty(revealed_treasures)) {
+        let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
+        card_discarder.discard_all()
+        let card_gainer = new CardGainer(game, player_cards, 'discard', 'Copper')
+        card_gainer.gain_game_card()
+      } else if (_.size(revealed_treasures) === 1) {
+        return NobleBrigand.trash_treasure(game, player_cards, revealed_treasures[0])
+      } else {
+        let turn_event_id = TurnEvents.insert({
+          game_id: game._id,
+          player_id: game.turn.player._id,
+          username: game.turn.player.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `Choose one of <strong>${player_cards.username}'s</strong> treasures to trash:`,
+          cards: revealed_treasures,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(NobleBrigand.choose_trashed_treasure)
+      }
+    }
+  }
+
+  static choose_trashed_treasure(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+    return NobleBrigand.trash_treasure(game, player_cards, selected_card)
+  }
+
+  static trash_treasure(game, player_cards, trashed_treasure) {
+    game.turn.trashed_treasures.push(trashed_treasure)
+    let card_trasher = new CardTrasher(game, player_cards, 'revealed', trashed_treasure.name)
+    card_trasher.trash()
+
+    let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
+    card_discarder.discard_all()
+
+    Games.update(game._id, game)
+  }
+
+}
