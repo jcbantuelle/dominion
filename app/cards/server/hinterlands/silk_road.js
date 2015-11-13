07ad21c77847fa83d5aa49582b6e93dd12afab26
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-13 16:19:25 -0600

    Implement Silk Road

---
+++ app/cards/server/hinterlands/silk_road.js
@@ -0,0 +1,21 @@
+SilkRoad = class SilkRoad extends Card {
+
+  types() {
+    return ['victory']
+  }
+
+  coin_cost() {
+    return 4
+  }
+
+  victory_points(player_cards) {
+    return Math.floor(this.point_variable(player_cards) / 4)
+  }
+
+  point_variable(player_cards) {
+    return _.size(_.filter(player_cards, function(card) {
+      return _.contains(card.types, 'victory')
+    }))
+  }
+
+}
