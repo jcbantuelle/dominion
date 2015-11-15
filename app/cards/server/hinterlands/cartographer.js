9be80ec5f6dd4411b8cc4eed22dace3186033d20
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-15 16:53:00 -0600

    Implement Cartographer

---
+++ app/cards/server/hinterlands/cartographer.js
@@ -0,0 +1,82 @@
+Cartographer = class Cartographer extends Card {
+
+  types() {
+    return ['action']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+
+    if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
+      game.log.push(`&nbsp;&nbsp;but has no cards in deck`)
+    } else {
+      if (_.size(player_cards.deck) < 4 && _.size(player_cards.discard) > 0) {
+        DeckShuffler.shuffle(player_cards)
+      }
+      player_cards.revealed = _.take(player_cards.deck, 4)
+      player_cards.deck = _.drop(player_cards.deck, 4)
+
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> looks at the top ${_.size(player_cards.revealed)} cards of their deck`)
+
+      let turn_event_id = TurnEvents.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Choose any number of cards to discard:',
+        cards: player_cards.revealed,
+        minimum: 0,
+        maximum: 0
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Cartographer.discard_cards)
+    }
+  }
+
+  static discard_cards(game, player_cards, selected_cards) {
+    if (_.size(selected_cards) === 0) {
+      game.log.push(`&nbsp;&nbsp;but does not discard anything`)
+    } else {
+      let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
+      card_discarder.discard_some(selected_cards)
+
+      if (_.size(player_cards.revealed) > 0) {
+        let turn_event_id = TurnEvents.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'sort_cards',
+          instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
+          cards: player_cards.revealed
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Cartographer.replace_cards)
+      }
+    }
+  }
+
+  static replace_cards(game, player_cards, ordered_card_names) {
+    _.each(ordered_card_names.reverse(), function(card_name) {
+      let revealed_card_index = _.findIndex(player_cards.revealed, function(card) {
+        return card.name === card_name
+      })
+      let revealed_card = player_cards.revealed.splice(revealed_card_index, 1)[0]
+      player_cards.deck.unshift(revealed_card)
+    })
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places the remaining cards back on their deck`)
+    player_cards.revealed = []
+  }
+
+}
+
+
+
