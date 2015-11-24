379072a7298cd02d5142e05df79d3d25c7349a30
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-24 12:03:45 -0600

    Refactor Treasury

--- app/cards/seaside/treasury.js
+++ app/cards/seaside/treasury.js
@@ -18,28 +18,23 @@ Treasury = class Treasury extends Card {
   }
 
   discard_from_play(game, player_cards) {
-    let victory_cards = _.filter(game.turn.bought_cards, function(card) {
-      return _.contains(card.types, 'victory')
+    let turn_event_id = TurnEvents.insert({
+      game_id: game._id,
+      player_id: player_cards.player_id,
+      username: player_cards.username,
+      type: 'choose_yes_no',
+      instructions: `Put ${CardView.render(this)} On Top of Deck?`,
+      minimum: 1,
+      maximum: 1
     })
-    if (_.isEmpty(victory_cards)) {
-      let turn_event_id = TurnEvents.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_yes_no',
-        instructions: 'Put Treasury On Top of Deck?',
-        minimum: 1,
-        maximum: 1
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Treasury.put_on_deck)
-    }
+    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+    turn_event_processor.process(Treasury.put_on_deck)
   }
 
   static put_on_deck(game, player_cards, response) {
     if (response === 'yes') {
       player_cards.deck.unshift(player_cards.discarding.pop())
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.card_html('action', 'Treasury')} on top of their deck`)
+      game.log.push(`<strong>${player_cards.username}</strong> puts ${CardView.card_html('action', 'Treasury')} on top of their deck`)
     }
   }
 
