d11a31c50d33575d8353a8c8dda7347d585c037c
Justin Bantuelle <justin@mudbugmedia.com>
2015-11-27 19:33:17 -0600

    Implement Shanty Town

---
+++ app/cards/intrigue/shanty_town.js
@@ -0,0 +1,26 @@
+ShantyTown = class ShantyTown extends Card {
+
+  types() {
+    return ['action']
+  }
+
+  coin_cost() {
+    return 3
+  }
+
+  play(game, player_cards) {
+    game.turn.actions += 2
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.hand)}`)
+
+    let has_actions = _.any(player_cards.hand, function(card) {
+      return _.contains(card.types, 'action')
+    })
+    if (!has_actions) {
+      let card_drawer = new CardDrawer(game, player_cards)
+      card_drawer.draw(2)
+    }
+  }
+
+}
