7d6f2df24a3448f0d87d2524a09215cee7924035
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-02 13:17:59 -0600

    Implement Trash Event Processor Mechanic

--- app/game/server/services/card_trasher.js
+++ app/game/server/services/card_trasher.js
@@ -9,15 +9,36 @@ CardTrasher = class CardTrasher {
   }
 
   trash() {
+    let has_event_cards = _.any(this.card_names, (card_name) => {
+      return _.contains(TrashEventProcessor.event_cards(), card_name)
+    })
+    let has_reaction_cards = _.any(this.player_cards.hand, (card) => {
+      return _.contains(TrashEventProcessor.reaction_cards(), card.name)
+    })
+    if (_.size(this.card_names) > 1 && (has_event_cards || has_reaction_cards)) {
+      let card_list = _.map(this.card_names, function(card_name) {
+        return ClassCreator.create(card_name).to_h()
+      })
+      let turn_event_id = TurnEventModel.insert({
+        game_id: this.game._id,
+        player_id: this.player_cards.player_id,
+        username: this.player_cards.username,
+        type: 'sort_cards',
+        instructions: 'Choose order to trash cards: (leftmost will be first)',
+        cards: card_list
+      })
+      let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, this)
+      turn_event_processor.process(CardTrasher.order_cards)
+    }
     _.each(this.card_names, (card_name) => {
       let card_index = this.find_card_index(card_name)
+      this.update_log(this.player_cards[this.source][card_index])
+      let trash_event_processor = new TrashEventProcessor(this, card_name)
+      trash_event_processor.process()
       if (card_index !== -1) {
         this.trash_card(card_index)
       }
     })
-    if (!_.isEmpty(this.trashed_cards)) {
-      this.update_log()
-    }
   }
 
   find_card_index(card_name) {
@@ -35,12 +56,16 @@ CardTrasher = class CardTrasher {
     this.trashed_cards = this.trashed_cards.concat(this.player_cards[this.source].splice(card_index, 1))
   }
 
-  update_log() {
-    let log_message = `&nbsp;&nbsp;<strong>${this.player_cards.username}</strong> trashes ${CardView.render(this.trashed_cards)}`
+  update_log(card) {
+    let log_message = `&nbsp;&nbsp;<strong>${this.player_cards.username}</strong> trashes ${CardView.render(card)}`
     if (this.game.turn.possessed) {
       log_message += ', setting the cards aside'
     }
     this.game.log.push(log_message)
+  }
+
+  static order_cards(game, player_cards, ordered_card_names, trasher) {
+    trasher.card_names = ordered_card_names
   }
 
 }
---
+++ app/game/server/services/trash_event_processor.js
@@ -0,0 +1,89 @@
+TrashEventProcessor = class BuyEventProcessor {
+
+  static reaction_cards() {
+    return ['Market Square']
+  }
+
+  static event_cards() {
+    return ['Overgrown Estate']
+  }
+
+  constructor(trasher, card_name) {
+    this.trasher = trasher
+    this.card = _.find(trasher.player_cards[trasher.source], (trash_card) => {
+      return trash_card.name === card_name
+    })
+    this.find_trash_events()
+  }
+
+  find_trash_events() {
+    this.trash_events = []
+    if (_.contains(TrashEventProcessor.event_cards(), this.card.name)) {
+      this.trash_events.push(this.card)
+    }
+
+    _.each(this.trasher.player_cards.hand, (card) => {
+      if (_.contains(TrashEventProcessor.reaction_cards(), card.name)) {
+        this.trash_events.push(card)
+      }
+    })
+  }
+
+  process() {
+    if (!_.isEmpty(this.trash_events)) {
+      let mandatory_trash_events = _.filter(this.trash_events, function(event) {
+        return _.contains(TrashEventProcessor.event_cards(), event.name)
+      })
+      if (_.size(this.trash_events) === 1 && !_.isEmpty(mandatory_trash_events)) {
+        TrashEventProcessor.trash_event(this.trasher.game, this.trasher.player_cards, this.trash_events, this)
+      } else {
+        GameModel.update(this.trasher.game._id, this.trasher.game)
+        let instructions = `Choose Trash Event To Resolve for ${CardView.render(this.card)}`
+        let minimum = 1
+        if (_.isEmpty(mandatory_trash_events)) {
+          instructions += ' (Or none to skip)'
+          minimum = 0
+        }
+        let turn_event_id = TurnEventModel.insert({
+          game_id: this.trasher.game._id,
+          player_id: this.trasher.player_cards.player_id,
+          username: this.trasher.player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `${instructions}:`,
+          cards: this.trash_events,
+          minimum: minimum,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(this.trasher.game, this.trasher.player_cards, turn_event_id, this)
+        turn_event_processor.process(TrashEventProcessor.trash_event)
+      }
+    }
+  }
+
+  static trash_event(game, player_cards, selected_cards, trash_event_processor) {
+    if (!_.isEmpty(selected_cards)) {
+      let selected_card = ClassCreator.create(selected_cards[0].name)
+      if (_.contains(TrashEventProcessor.reaction_cards(), selected_card.name())) {
+        selected_card.trash_reaction(game, player_cards, trash_event_processor.trasher)
+        if (selected_cards[0].name === 'Market Square') {
+          let trash_event_index = _.findIndex(trash_event_processor.trash_events, function(event) {
+            return event.name === 'Market Square'
+          })
+          trash_event_processor.trash_events.splice(trash_event_index, 1)
+        }
+      } else {
+        selected_card.trash_event(trash_event_processor.trasher)
+        let trash_event_index = _.findIndex(trash_event_processor.trash_events, function(event) {
+          return event.name === selected_cards[0].name
+        })
+        trash_event_processor.trash_events.splice(trash_event_index, 1)
+      }
+
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
+      trash_event_processor.process()
+    }
+  }
+
+}
