bce28a68fb3a5285fe24b94b8fb67832b2a43551
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-02 13:18:21 -0600

    Implement Necropolis

---
+++ app/cards/dark_ages/necropolis.js
@@ -0,0 +1,20 @@
+Necropolis = class Necropolis extends Card {
+
+  is_purchasable() {
+    return false
+  }
+
+  types() {
+    return ['action', 'shelter']
+  }
+
+  coin_cost() {
+    return 1
+  }
+
+  play(game, player_cards) {
+    game.turn.actions += 2
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+  }
+
+}
