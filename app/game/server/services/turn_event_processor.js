eca081d369d2a7af952aa1bfe3a101998c1ee366
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-01 20:15:45 -0600

    Overhaul Turn Event Data Source

--- app/game/server/services/turn_event_processor.js
+++ app/game/server/services/turn_event_processor.js
@@ -15,7 +15,7 @@ TurnEventProcessor = class TurnEventProcessor {
     Future.wait(...turn_events)
     _.each(this.turn_event_ids, (turn_event_id) => {
       let response = TurnEventFutures[turn_event_id].get()
-      TurnEventModel.remove(turn_event_id)
+      TurnEventModel.remove(this.game._id, turn_event_id)
       delete TurnEventFutures[turn_event_id]
       event_action(this.game, this.player_cards, response, this.source)
     })
