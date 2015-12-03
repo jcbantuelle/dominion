1439f18c6a17532acd0938d36fdd2046899600d5
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-03 17:24:39 -0600

    Fix Additional Card Trash Event Issues

--- app/game/server/services/card_trasher.js
+++ app/game/server/services/card_trasher.js
@@ -5,17 +5,20 @@ CardTrasher = class CardTrasher {
     this.player_cards = player_cards
     this.source = source
     this.card_names = _.isArray(card_names) ? card_names : [card_names]
-    this.trashed_cards = []
   }
 
   trash() {
+    _.each(this.card_names, (card_name) => {
+      this.player_cards.trash.push(this.find_card(card_name))
+    })
+
     let has_event_cards = _.any(this.card_names, (card_name) => {
       return _.contains(TrashEventProcessor.event_cards(), card_name)
     })
     let has_reaction_cards = _.any(this.player_cards.hand, (card) => {
       return _.contains(TrashEventProcessor.reaction_cards(), card.name)
     })
-    if (_.size(this.card_names) > 1 && (has_event_cards || has_reaction_cards)) {
+    if (_.size(this.player_cards.trash) > 1 && (has_event_cards || has_reaction_cards)) {
       let card_list = _.map(this.card_names, function(card_name) {
         return ClassCreator.create(card_name).to_h()
       })
@@ -25,39 +28,43 @@ CardTrasher = class CardTrasher {
         username: this.player_cards.username,
         type: 'sort_cards',
         instructions: 'Choose order to trash cards: (leftmost will be first)',
-        cards: card_list
+        cards: this.player_cards.trash
       })
       let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, this)
       turn_event_processor.process(CardTrasher.order_cards)
     }
-    _.each(this.card_names, (card_name) => {
-      let card_index = this.find_card_index(card_name)
-      let trashed_card = this.player_cards[this.source].splice(card_index, 1)[0]
-      this.player_cards.trashing.push(trashed_card)
-      this.trashed_card_count = _.size(this.player_cards.trashing)
+
+    _.each(this.player_cards.trash, (trashed_card) => {
+      this.track_trashed_card(trashed_card)
       this.update_log(trashed_card)
       let trash_event_processor = new TrashEventProcessor(this, trashed_card)
       trash_event_processor.process()
-      if (_.size(this.player_cards.trashing) === this.trashed_card_count) {
-        this.trash_card(card_index)
-      }
+      this.put_card_in_trash(trashed_card)
     })
+
+    this.player_cards.trash = []
   }
 
-  find_card_index(card_name) {
-    return _.findIndex(this.player_cards[this.source], (card) => {
+  find_card(card_name) {
+    let card_index = _.findIndex(this.player_cards[this.source], (card) => {
       return card.name === card_name
     })
+    return this.player_cards[this.source].splice(card_index, 1)[0]
   }
 
-  trash_card(card_index) {
-    let trashed_card = this.player_cards.trashing.pop()
-    if (this.game.turn.possessed) {
-      this.player_cards.possession_trash.push(trashed_card)
-    } else {
-      this.game.trash.push(trashed_card)
+  track_trashed_card(trashed_card) {
+    this.player_cards.trashing.push(trashed_card)
+    this.trashed_card_count = _.size(this.player_cards.trashing)
+  }
+
+  put_card_in_trash(trashed_card) {
+    if (_.size(this.player_cards.trashing) === this.trashed_card_count) {
+      if (this.game.turn.possessed) {
+        this.player_cards.possession_trash.push(trashed_card)
+      } else {
+        this.game.trash.push(trashed_card)
+      }
     }
-    this.trashed_cards = this.trashed_cards.concat(this.player_cards[this.source].splice(card_index, 1))
   }
 
   update_log(card) {
@@ -69,7 +76,14 @@ CardTrasher = class CardTrasher {
   }
 
   static order_cards(game, player_cards, ordered_card_names, trasher) {
-    trasher.card_names = ordered_card_names
+    let new_trash_order = []
+    _.each(ordered_card_names, function(card_name) {
+      let trash_card = _.find(player_cards.trash, function(card) {
+        return card.name === card_name
+      })
+      new_trash_order.push(trash_card)
+    })
+    player_cards.trash = new_trash_order
   }
 
 }
