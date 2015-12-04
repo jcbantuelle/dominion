566071ad7a02826086aa5d0a951a17cd12e55da6
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-03 20:39:52 -0600

    Implement Spoils

---
+++ app/cards/dark_ages/spoils.js
@@ -0,0 +1,35 @@
+Spoils = class Spoils extends Card {
+
+  types() {
+    return ['treasure']
+  }
+
+  coin_cost() {
+    return 0
+  }
+
+  play(game, player_cards) {
+    game.turn.coins += 3
+
+    let spoils_index = _.findIndex(player_cards.playing, function(card) {
+      return card.name === 'Spoils'
+    })
+
+    if (spoils_index !== -1) {
+
+      let spoils_pile = _.find(game.cards, function(card) {
+        return card.name === 'Spoils'
+      })
+
+      let spoils_card = player_cards.playing.splice(spoils_index, 1)[0]
+
+      spoils_pile.count += 1
+      spoils_pile.stack.unshift(spoils_card)
+      spoils_pile.top_card = _.first(spoils_pile.stack)
+
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(this)} to the Spoils pile`)
+    }
+
+  }
+
+}
