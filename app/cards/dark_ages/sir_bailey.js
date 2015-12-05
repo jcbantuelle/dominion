99142e5fdd6f2a3481a887c5fdba7e1682760ba2
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-04 21:53:58 -0600

    Implement Sir Bailey

---
+++ app/cards/dark_ages/sir_bailey.js
@@ -0,0 +1,24 @@
+SirBailey = class SirBailey extends Knights {
+
+  types() {
+    return ['action', 'attack', 'knight']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+
+    let player_attacker = new PlayerAttacker(game, this)
+    player_attacker.attack(player_cards)
+
+    this.trash_knight(game, player_cards)
+  }
+
+}
