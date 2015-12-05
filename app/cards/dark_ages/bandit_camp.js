124873fea7d86a0569bba7fe1e45150e8f94fd97
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-05 00:24:01 -0600

    Implement Bandit Camp

---
+++ app/cards/dark_ages/bandit_camp.js
@@ -0,0 +1,22 @@
+BanditCamp = class BanditCamp extends Card {
+
+  types() {
+    return ['action']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 2
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+
+    let card_gainer = new CardGainer(game, player_cards, 'discard', 'Spoils')
+    card_gainer.gain_game_card()
+  }
+
+}
