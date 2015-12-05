ba348f7d534007924825dcfddb69851f3936d52c
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-04 21:53:50 -0600

    Implement Dame Josephine

---
+++ app/cards/dark_ages/dame_josephine.js
@@ -0,0 +1,22 @@
+DameJosephine = class DameJosephine extends Knights {
+
+  types() {
+    return ['action', 'attack', 'knight', 'victory']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  victory_points() {
+    return 2
+  }
+
+  play(game, player_cards) {
+    let player_attacker = new PlayerAttacker(game, this)
+    player_attacker.attack(player_cards)
+
+    this.trash_knight(game, player_cards)
+  }
+
+}
