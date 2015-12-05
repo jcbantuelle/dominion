858b9c06b5083eb76ebe46f5d7e9762202bfb4d5
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-05 00:15:49 -0600

    Fix Procession Bugs

--- app/cards/dark_ages/procession.js
+++ app/cards/dark_ages/procession.js
@@ -33,46 +33,47 @@ Procession = class Procession extends Card {
   }
 
   static play_twice(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
     if (!_.isEmpty(selected_cards)) {
+      let selected_card = selected_cards[0]
+
       let repeat_card_player = new RepeatCardPlayer(game, player_cards, selected_card.name)
       repeat_card_player.play(2, 'Procession')
-    }
 
-    let played_card_index = _.findIndex(player_cards.playing, function(card) {
-      return card.name == selected_card.name
-    })
-    if (played_card_index !== -1) {
-      let card_trasher = new CardTrasher(game, player_cards, 'playing', selected_card.name)
-      card_trasher.trash()
-    }
-
-    let all_player_cards = PlayerCardsModel.find(game._id)
-    let selected_card_coin_cost = CostCalculator.calculate(game, selected_card, all_player_cards)
-
-    let eligible_cards = _.filter(game.cards, function(card) {
-      let game_card_coin_cost = CostCalculator.calculate(game, card.top_card, all_player_cards)
-      return card.count > 0 && card.top_card.purchasable && game_card_coin_cost === (selected_card_coin_cost + 1) && card.top_card.potion_cost === selected_card.potion_cost
-    })
-
-    if (_.size(eligible_cards) > 0) {
-      GameModel.update(game._id, game)
-      PlayerCardsModel.update(game._id, player_cards)
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        game_cards: true,
-        instructions: `Choose a card to gain:`,
-        cards: eligible_cards,
-        minimum: 1,
-        maximum: 1
+      let played_card_index = _.findIndex(player_cards.playing, function(card) {
+        return card.name == repeat_card_player.card.name()
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Procession.gain_card)
-    } else {
-      game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+      if (played_card_index !== -1) {
+        let card_trasher = new CardTrasher(game, player_cards, 'playing', repeat_card_player.card.name())
+        card_trasher.trash()
+      }
+
+      let all_player_cards = PlayerCardsModel.find(game._id)
+      let selected_card_coin_cost = CostCalculator.calculate(game, selected_card, all_player_cards)
+
+      let eligible_cards = _.filter(game.cards, function(card) {
+        let game_card_coin_cost = CostCalculator.calculate(game, card.top_card, all_player_cards)
+        return card.count > 0 && card.top_card.purchasable && _.contains(_.words(card.top_card.types), 'action') && game_card_coin_cost === (selected_card_coin_cost + 1) && card.top_card.potion_cost === selected_card.potion_cost
+      })
+
+      if (_.size(eligible_cards) > 0) {
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          game_cards: true,
+          instructions: `Choose a card to gain:`,
+          cards: eligible_cards,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Procession.gain_card)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no available actions to gain`)
+      }
     }
   }
 
