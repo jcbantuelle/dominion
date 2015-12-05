85d3b7bc22500146e84d5846540dbcb585cb42f7
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-04 21:57:29 -0600

    Implement Sir Martin

---
+++ app/cards/dark_ages/sir_martin.js
@@ -0,0 +1,21 @@
+SirMartin = class SirMartin extends Knights {
+
+  types() {
+    return ['action', 'attack', 'knight']
+  }
+
+  coin_cost() {
+    return 4
+  }
+
+  play(game, player_cards) {
+    game.turn.buys += 2
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 buys`)
+
+    let player_attacker = new PlayerAttacker(game, this)
+    player_attacker.attack(player_cards)
+
+    this.trash_knight(game, player_cards)
+  }
+
+}
