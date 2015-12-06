4a363e98914cd11e7be78d6e0ff5202a85ea3eac
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-05 20:44:02 -0600

    Implement Graverobber

---
+++ app/cards/dark_ages/graverobber.js
@@ -0,0 +1,130 @@
+Graverobber = class Graverobber extends Card {
+
+  types() {
+    return ['action']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let turn_event_id = TurnEventModel.insert({
+      game_id: game._id,
+      player_id: player_cards.player_id,
+      username: player_cards.username,
+      type: 'choose_options',
+      instructions: `Choose One:`,
+      minimum: 1,
+      maximum: 1,
+      options: [
+        {text: 'Gain a card from the trash', value: 'gain'},
+        {text: 'Trash an action from your hand', value: 'trash'}
+      ]
+    })
+    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+    turn_event_processor.process(Graverobber.process_response)
+  }
+
+  static process_response(game, player_cards, response) {
+    response = response[0]
+    if (response === 'gain') {
+      let all_player_cards = PlayerCardsModel.find(game._id)
+
+      let eligible_cards = _.filter(game.trash, function(card) {
+        let coin_cost = CostCalculator.calculate(game, card, all_player_cards)
+        return coin_cost >= 3 && coin_cost <= 6 && card.potion_cost === 0
+      })
+
+      if (_.size(eligible_cards) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          game_cards: true,
+          instructions: 'Choose a card to gain:',
+          cards: eligible_cards,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Graverobber.gain_from_trash)
+      } else if (_.size(eligible_cards) === 1) {
+        Graverobber.gain_from_trash(game, player_cards, eligible_cards)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+      }
+    } else if (response === 'trash') {
+      let eligible_cards = _.filter(player_cards.hand, function(card) {
+        return _.contains(_.words(card.types), 'action')
+      })
+      if (_.size(eligible_cards) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: 'Choose an action to trash:',
+          cards: eligible_cards,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Graverobber.trash_card)
+      } else if (_.size(eligible_cards) === 1) {
+        Graverobber.trash_card(game, player_cards, eligible_cards)
+      } else {
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> chooses to trash an action, but has none in their hand`)
+      }
+    }
+  }
+
+  static trash_card(game, player_cards, selected_cards) {
+    let selected_card = selected_cards[0]
+
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card.name)
+    card_trasher.trash()
+
+    let all_player_cards = PlayerCardsModel.find(game._id)
+
+    let trashed_cost = CostCalculator.calculate(game, selected_card, all_player_cards)
+
+    let eligible_cards = _.filter(game.cards, function(card) {
+      let coin_cost = CostCalculator.calculate(game, card.top_card, all_player_cards)
+      return card.count > 0 && card.top_card.purchasable && coin_cost <= (trashed_cost + 3) && card.top_card.potion_cost <= selected_card.potion_cost
+    })
+
+    if (_.size(eligible_cards) > 0) {
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        game_cards: true,
+        instructions: 'Choose a card to gain:',
+        cards: eligible_cards,
+        minimum: 1,
+        maximum: 1
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Graverobber.gain_from_supply)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+    }
+  }
+
+  static gain_from_supply(game, player_cards, selected_cards) {
+    let card_gainer = new CardGainer(game, player_cards, 'discard', selected_cards[0].name)
+    card_gainer.gain_game_card()
+  }
+
+  static gain_from_trash(game, player_cards, selected_cards) {
+    let card_gainer = new CardGainer(game, player_cards, 'deck', selected_cards[0].name)
+    card_gainer.gain_trash_card()
+  }
+
+}
