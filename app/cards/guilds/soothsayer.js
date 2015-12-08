f6c084d573224cbd9d11141dfbfbcbd56b601895
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-07 22:48:10 -0600

    Implement Soothsayer

---
+++ app/cards/guilds/soothsayer.js
@@ -0,0 +1,29 @@
+Soothsayer = class Soothsayer extends Card {
+
+  types() {
+    return ['action', 'attack']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let card_gainer = new CardGainer(game, player_cards, 'discard', 'Gold')
+    card_gainer.gain_game_card()
+
+    let player_attacker = new PlayerAttacker(game, this)
+    player_attacker.attack(player_cards)
+  }
+
+  attack(game, player_cards) {
+    let card_gainer = new CardGainer(game, player_cards, 'discard', 'Curse')
+    let gained_card = card_gainer.gain_game_card()
+
+    if (gained_card && gained_card.name === 'Curse') {
+      let card_drawer = new CardDrawer(game, player_cards)
+      card_drawer.draw(1)
+    }
+  }
+
+}
--- app/lobby/server/card_list.js
+++ app/lobby/server/card_list.js
@@ -32,7 +32,7 @@ CardList = class CardList {
       'Butcher',
       'Journeyman',
       'MerchantGuild',
-      /*'Soothsayer'*/
+      'Soothsayer'
     ]
   }
 
