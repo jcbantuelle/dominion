b2ec72f737ee40454cd50f557961eca33a0adec2
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-08 17:36:48 -0600

    Add Log Message to Coin Token Player

--- app/game/server/services/coin_token_player.js
+++ app/game/server/services/coin_token_player.js
@@ -8,6 +8,7 @@ CoinTokenPlayer = class CoinTokenPlayer {
   play() {
     if (this.can_play()) {
       this.update_phase()
+      this.game.log.push(`<strong>${this.player_cards.username}</strong> spends a coin token`)
       this.player_cards.coin_tokens -= 1
       this.game.turn.coins += 1
       GameModel.update(this.game._id, this.game)
