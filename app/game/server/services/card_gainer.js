f9b83b236ea64a18b7bcec8b3f404e2de93c3b75
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-10 13:04:36 -0600

    Avoid Clobbering PlayerCard Data On Gain Event

--- app/game/server/services/card_gainer.js
+++ app/game/server/services/card_gainer.js
@@ -129,6 +129,7 @@ CardGainer = class CardGainer {
     this.game.turn.gain_event_stack.push(this.card_name)
 
     let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(this.game, this.player_cards)
+    ordered_player_cards[0] = this.player_cards
     _.each(ordered_player_cards, (player_cards) => {
       let gain_event_processor = new GainEventProcessor(this, player_cards)
       gain_event_processor.process()
