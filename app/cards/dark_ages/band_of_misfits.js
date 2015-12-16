2d39305d01f1c08b0527caca7c07aed0d7d3941d
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-16 16:06:23 -0600

    Update Band of Misfits to Route Selected Card Through Card Player

--- app/cards/dark_ages/band_of_misfits.js
+++ app/cards/dark_ages/band_of_misfits.js
@@ -8,7 +8,7 @@ BandOfMisfits = class BandOfMisfits extends Card {
     return 5
   }
 
-  play(game, player_cards, player) {
+  play(game, player_cards) {
     let all_player_cards = PlayerCardsModel.find(game._id)
 
     let self_cost = CostCalculator.calculate(game, this.to_h(), all_player_cards)
@@ -30,26 +30,28 @@ BandOfMisfits = class BandOfMisfits extends Card {
         minimum: 1,
         maximum: 1
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, player)
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(BandOfMisfits.copy_card)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to copy`)
     }
-
-    game.turn.played_actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> plays ${CardView.render(this)} as ${CardView.render(player.card)}`)
-    GameModel.update(game._id, game)
-    return player.card.play(game, player_cards, player)
   }
 
-  static copy_card(game, player_cards, selected_cards, player) {
-    player.card = ClassCreator.create(selected_cards[0].name)
-    let copy = player.card.to_h()
-    copy.misfit = true
+  static copy_card(game, player_cards, selected_cards) {
     let played_misfit_index = _.findIndex(player_cards.playing, function(card) {
       return card.name === 'Band Of Misfits'
     })
-    player_cards.playing.splice(played_misfit_index, 1, copy)
+    let misfit = player_cards.playing.splice(played_misfit_index, 1)
+
+    let copy = ClassCreator.create(selected_cards[0].name).to_h()
+    copy.misfit = true
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> plays ${CardView.render(misfit)} as ${CardView.render(copy)}`)
+
+    GameModel.update(game._id, game)
+
+    player_cards.hand.push(copy)
+    let card_player = new CardPlayer(game, player_cards, copy.name, true)
+    card_player.play()
   }
 
 }
