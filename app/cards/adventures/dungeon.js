e21575e9421c028697109b38224aade7d7e6cad0
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-18 12:00:16 -0600

    Implement Dungeon

---
+++ app/cards/adventures/dungeon.js
@@ -0,0 +1,59 @@
+Dungeon = class Dungeon extends Card {
+
+  types() {
+    return ['action', 'duration']
+  }
+
+  coin_cost() {
+    return 3
+  }
+
+  play(game, player_cards) {
+    player_cards.duration_effects.push(this.to_h())
+
+    game.turn.actions += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+
+    this.draw_and_discard(game, player_cards)
+    return 'duration'
+  }
+
+  duration(game, player_cards, duration_card) {
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> resolves ${CardView.render(duration_card)}`)
+    this.draw_and_discard(game, player_cards)
+  }
+
+  draw_and_discard(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(2)
+
+    PlayerCardsModel.update(game._id, player_cards)
+
+    if (_.size(player_cards.hand) > 2) {
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Discard 2 cards:',
+        cards: player_cards.hand,
+        minimum: 2,
+        maximum: 2
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Dungeon.discard_cards)
+    } else if (_.size(player_cards.hand) === 0) {
+      game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
+    } else {
+      let card_discarder = new CardDiscarder(game, player_cards, 'hand')
+      card_discarder.discard()
+    }
+  }
+
+  static discard_cards(game, player_cards, selected_cards) {
+    let card_discarder = new CardDiscarder(game, player_cards, 'hand', _.pluck(selected_cards, 'name'))
+    card_discarder.discard()
+  }
+
+}
