4f9bcf40d5a72c1254cb3f4fdd8c50b7f7c70555
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-21 18:46:05 -0600

    Implement Artificer

---
+++ app/cards/adventures/artificer.js
@@ -0,0 +1,82 @@
+Artificer = class Artificer extends Card {
+
+  types() {
+    return ['action']
+  }
+
+  coin_cost() {
+    return 5
+  }
+
+  play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 1
+    game.turn.coins += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action and +$1`)
+
+    if (_.size(player_cards.hand) > 0) {
+      PlayerCardsModel.update(game._id, player_cards)
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Choose any number of cards to discard:',
+        cards: player_cards.hand,
+        minimum: 0,
+        maximum: 0
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Artificer.discard_cards)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
+    }
+  }
+
+  static discard_cards(game, player_cards, selected_cards) {
+    if (_.size(selected_cards) === 0) {
+      game.log.push(`&nbsp;&nbsp;but does not discard anything`)
+    } else {
+      let card_discarder = new CardDiscarder(game, player_cards, 'hand', _.pluck(selected_cards, 'name'))
+      card_discarder.discard()
+
+      let all_player_cards = PlayerCardsModel.find(game._id)
+
+      let eligible_cards = _.filter(game.cards, function(card) {
+        let coin_cost = CostCalculator.calculate(game, card.top_card, all_player_cards)
+        return card.count > 0 && card.top_card.purchasable && coin_cost === _.size(selected_cards) && card.top_card.potion_cost === 0
+      })
+
+      if (_.size(eligible_cards) > 0) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          game_cards: true,
+          instructions: 'Choose a card to gain (Or none to skip):',
+          cards: eligible_cards,
+          minimum: 0,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Artificer.gain_card)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+      }
+    }
+  }
+
+  static gain_card(game, player_cards, selected_cards) {
+    if (!_.isEmpty(selected_cards)) {
+      let card_gainer = new CardGainer(game, player_cards, 'deck', selected_cards[0].name)
+      card_gainer.gain_game_card()
+    } else {
+      game.log.push(`&nbsp;&nbsp;but chooses not to gain anything`)
+    }
+  }
+
+}
