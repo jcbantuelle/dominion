62fe7ecccf884c824cf86ca4b112c57d0ef9c1a8
Justin Bantuelle <justin@mudbugmedia.com>
2015-12-30 10:51:20 -0600

    Fix Minus Coin Token Discard Log Issue

--- app/game/server/services/coin_gainer.js
+++ app/game/server/services/coin_gainer.js
@@ -3,7 +3,7 @@ CoinGainer = class CoinGainer {
   static gain(game, player_cards, amount) {
     if (player_cards.tokens.minus_coin) {
       amount -= 1
-      game.log.push(`&nbsp;&nbsp;${player_cards.username} discards their -$1 coin token`)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> discards their -$1 coin token`)
       delete player_cards.tokens.minus_coin
     }
     game.turn.coins += amount
