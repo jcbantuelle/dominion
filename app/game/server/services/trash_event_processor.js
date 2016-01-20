4aaa17ddc8899116df2f4a33168f0290f1d2142e
Justin Bantuelle <justin@mudbugmedia.com>
2016-01-20 12:20:35 -0600

    Add Rats to Trash Event Processor

--- app/game/server/services/trash_event_processor.js
+++ app/game/server/services/trash_event_processor.js
@@ -5,7 +5,7 @@ TrashEventProcessor = class BuyEventProcessor {
   }
 
   static event_cards() {
-    return ['Overgrown Estate', 'Squire', 'Feodum', 'Fortress', 'Sir Vander', 'Cultist', 'Hunting Grounds']
+    return ['Overgrown Estate', 'Squire', 'Feodum', 'Fortress', 'Sir Vander', 'Cultist', 'Hunting Grounds', 'Rats']
   }
 
   constructor(trasher, card) {
