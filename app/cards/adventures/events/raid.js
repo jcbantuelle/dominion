2eaf8db6f03aa4e238df0b3a39ef3a056dfabeb2
Justin Bantuelle <justin@mudbugmedia.com>
2016-01-22 16:03:58 -0600

    Implement Raid

---
+++ app/cards/adventures/events/raid.js
@@ -0,0 +1,28 @@
+Raid = class Raid extends Event {
+
+  coin_cost() {
+    return 5
+  }
+
+  buy(game, player_cards) {
+    let silvers = _.filter(player_cards.in_play, function(card) {
+      return card.name === 'Silver'
+    })
+    _.times(_.size(silvers), function() {
+      let card_gainer = new CardGainer(game, player_cards, 'discard', 'Silver')
+      card_gainer.gain_game_card()
+    })
+
+    let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(game)
+    ordered_player_cards.shift()
+    _.each(ordered_player_cards, function(other_player_cards) {
+      if (!other_player_cards.tokens.minus_card) {
+        game.log.push(`&nbsp;&nbsp;<strong>${other_player_cards.username}</strong> takes their -1 card token`)
+        other_player_cards.tokens.minus_card = true
+      } else {
+        game.log.push(`&nbsp;&nbsp;<strong>${other_player_cards.username}</strong> already has their -1 card token`)
+      }
+    })
+  }
+
+}
