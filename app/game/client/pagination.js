dbd716bc0322a2a6e3f886d5346792d1ab3a5c5b
Justin Bantuelle <justin@mudbugmedia.com>
2016-03-07 08:04:34 -0600

    Hide/Disable Inactive Pagination Links

--- app/game/client/pagination.js
+++ app/game/client/pagination.js
@@ -4,11 +4,20 @@ Template.pagination.helpers({
   },
   next_page: function (page_name) {
     return pageShift(page_name, 1)
+  },
+  show_previous_page: function (page_name) {
+    return parseInt(Session.get(page_name)) != 1
+  },
+  show_next_page: function(page_name) {
+    return parseInt(Session.get(page_name)) != _.ceil(Counts.get('game_history_count') / Pagination.per_page)
+  },
+  current_page: function(page_name, page_number) {
+    return page_number == Session.get(page_name)
   }
 })
 
 Template.pagination.events({
-  "click .pagination span": changePage
+  "click .pagination li": changePage
 })
 
 function pageShift (page_name, shift) {
@@ -19,7 +28,7 @@ function pageShift (page_name, shift) {
 }
 
 function changePage(event) {
-  let page_name = $(event.target).attr('data-page-name')
-  let page_number = $(event.target).attr('data-page')
+  let page_name = $(event.target).first('span').attr('data-page-name')
+  let page_number = $(event.target).first('span').attr('data-page')
   Session.set(page_name, page_number)
 }
