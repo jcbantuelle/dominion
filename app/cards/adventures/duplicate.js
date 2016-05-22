51e3d79e3ead1c1699868f6b86de79fb42331bc1
Justin Bantuelle <justin@mudbugmedia.com>
2016-05-22 10:51:29 -0500

    Improve Duplicate Event Prompt Clarity

--- app/cards/adventures/duplicate.js
+++ app/cards/adventures/duplicate.js
@@ -19,12 +19,13 @@ Duplicate = class Duplicate extends Card {
         return card.name === 'Estate'
       })
     }
+    let gained_card = ClassCreator.create(gainer.card_name)
     let turn_event_id = TurnEventModel.insert({
       game_id: gainer.game._id,
       player_id: gainer.player_cards.player_id,
       username: gainer.player_cards.username,
       type: 'choose_yes_no',
-      instructions: `Call ${CardView.render(tavern_card)}?`,
+      instructions: `Call ${CardView.render(tavern_card)}? to gain a copy of ${CardView.render(gained_card)}`,
       minimum: 1,
       maximum: 1
     })
