2827230f93c5269611c9e81611cbcd563b500783
Justin Bantuelle <justin@mudbugmedia.com>
2016-07-25 12:40:46 -0500

    Improve All Coin Player Log Ordering

--- app/game/server/services/all_coin_player.js
+++ app/game/server/services/all_coin_player.js
@@ -10,13 +10,13 @@ AllCoinPlayer = class AllCoinPlayer {
   }
 
   play() {
-    let played_cards = _.map(this.find_playable_cards(), (card) => {
-      let card_player = new CardPlayer(this.game, this.player_cards, card.name)
-      card_player.play(false)
-      return card
-    })
-    if (_.size(played_cards) > 0) {
-      this.game.log.push(`<strong>${this.player_cards.username}</strong> plays ${CardView.render(played_cards)}`)
+    let playable_cards = this.find_playable_cards()
+    if (_.size(playable_cards) > 0) {
+      this.game.log.push(`<strong>${this.player_cards.username}</strong> plays ${CardView.render(playable_cards)}`)
+      _.each(playable_cards, (card) => {
+        let card_player = new CardPlayer(this.game, this.player_cards, card.name)
+        card_player.play(false)
+      })
       GameModel.update(this.game._id, this.game)
       PlayerCardsModel.update(this.game._id, this.player_cards)
     }
