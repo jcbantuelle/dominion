2e399a6ab1419920baa81466ea318c07629cbc3a
Justin Bantuelle <justin@mudbugmedia.com>
2016-08-12 17:38:15 -0500

    Implement Baths

---
+++ app/game/server/services/end_turn_event_processor.js
@@ -0,0 +1,64 @@
+EndTurnEventProcessor = class EndTurnEventProcessor {
+
+  static landmark_events() {
+    return ['Baths']
+  }
+
+  constructor(game, player_cards) {
+    this.game = game
+    this.player_cards = player_cards
+    this.find_end_turn_events()
+  }
+
+  find_end_turn_events() {
+    let landmark_events = _.filter(this.game.landmarks, (card) => {
+      if (_.includes(EndTurnEventProcessor.landmark_events(), card.name)) {
+        if (card.name === 'Baths') {
+          return card.victory_tokens > 0 && _.size(this.game.turn.gained_cards) == 0
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+    })
+
+    this.end_turn_events = landmark_events
+  }
+
+  process() {
+    if (!_.isEmpty(this.end_turn_events)) {
+      if (_.size(this.end_turn_events) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: this.game._id,
+          player_id: this.player_cards.player_id,
+          username: this.player_cards.username,
+          type: 'sort_cards',
+          instructions: 'Choose order to resolve end of turn events (leftmost will be first):',
+          cards: this.end_turn_events
+        })
+        let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, this.end_turn_events)
+        turn_event_processor.process(EndTurnEventProcessor.event_order)
+      } else {
+        EndTurnEventProcessor.event_order(this.game, this.player_cards, _.map(this.end_turn_events, 'name'), this.end_turn_events)
+      }
+    }
+  }
+
+  static event_order(game, player_cards, event_name_order, events) {
+    _.each(event_name_order, function(event_name) {
+      let event_index = _.findIndex(events, function(event) {
+        return event.name === event_name
+      })
+      let event = events.splice(event_index, 1)[0]
+      if (event_name === 'Estate' && player_cards.tokens.estate) {
+        event_name = 'InheritedEstate'
+      }
+      let selected_event = ClassCreator.create(event_name)
+      selected_event.end_turn_event(game, player_cards)
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
+    })
+  }
+
+}
