0a1478b601842e6ecd3beec8e03c7bb6f42fd6bc
Justin Bantuelle <justin@mudbugmedia.com>
2016-08-12 17:37:52 -0500

    Adjust Basilica Behavior

--- app/cards/empires/landmarks/basilica.js
+++ app/cards/empires/landmarks/basilica.js
@@ -3,21 +3,24 @@ Basilica = class Basilica extends Landmark {
   buy_event(buyer) {
     let game = buyer.game
     let player_cards = buyer.player_cards
-    let basilica_stack = _.find(game.landmarks, (card) => {
-      return card.name === 'Basilica'
-    })
 
-    let victory_tokens = Math.min(2, basilica_stack.victory_tokens)
-    basilica_stack.victory_tokens = Math.max(0, basilica_stack.victory_tokens - 2)
+    if (game.turn.coins >= 2) {
+      let basilica_stack = _.find(game.landmarks, (card) => {
+        return card.name === 'Basilica'
+      })
 
-    if (game.turn.possessed) {
-      possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-      possessing_player_cards.victory_tokens += victory_tokens
-      game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +${victory_tokens} &nabla; from ${CardView.render(this)}`)
-      PlayerCardsModel.update(game._id, possessing_player_cards)
-    } else {
-      player_cards.victory_tokens += victory_tokens
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +${victory_tokens} &nabla; from ${CardView.render(this)}`)
+      let victory_tokens = Math.min(2, basilica_stack.victory_tokens)
+      basilica_stack.victory_tokens = Math.max(0, basilica_stack.victory_tokens - 2)
+
+      if (game.turn.possessed) {
+        possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
+        possessing_player_cards.victory_tokens += victory_tokens
+        game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +${victory_tokens} &nabla; from ${CardView.render(this)}`)
+        PlayerCardsModel.update(game._id, possessing_player_cards)
+      } else {
+        player_cards.victory_tokens += victory_tokens
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +${victory_tokens} &nabla; from ${CardView.render(this)}`)
+      }
     }
 
   }
