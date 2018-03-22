7f9a6254a260582b04461027b14092be2d9e9261
Justin Bantuelle <justin@mudbugmedia.com>
2018-03-21 20:15:11 -0500

    Implement Merchant

--- app/cards/common/silver.js
+++ app/cards/common/silver.js
@@ -10,6 +10,12 @@ Silver = class Silver extends Card {
 
   play(game, player_cards) {
     CoinGainer.gain(game, player_cards, 2)
+
+    if (game.turn.played_merchant && !game.turn.gained_merchant_silver) {
+      game.turn.gained_merchant_silver = true
+      let gained_coins = CoinGainer.gain(game, player_cards, 1)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins} from ${CardView.card_html('action', 'Merchant')}`)
+    }
   }
 
 }
