ed188c733fac88338ce9342d0e7342f1dca95c57
Justin Bantuelle <justin@mudbugmedia.com>
2018-03-22 19:16:06 -0500

    Implement Sentry

--- app/cards/base/sentry.js
+++ app/cards/base/sentry.js
@@ -9,6 +9,105 @@ Sentry = class Sentry extends Card {
   }
 
   play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+
+    PlayerCardsModel.update(game._id, player_cards)
+    GameModel.update(game._id, game)
+
+    if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
+      game.log.push(`&nbsp;&nbsp;but has no cards in deck`)
+    } else {
+      player_cards.revealed = _.take(player_cards.deck, 2)
+      player_cards.deck = _.drop(player_cards.deck, 2)
+
+      let revealed_card_count = _.size(player_cards.revealed)
+      if (revealed_card_count < 2 && _.size(player_cards.discard) > 0) {
+        DeckShuffler.shuffle(game, player_cards)
+        player_cards.revealed = player_cards.revealed.concat(_.take(player_cards.deck, 2 - revealed_card_count))
+        player_cards.deck = _.drop(player_cards.deck, 2 - revealed_card_count)
+      }
+
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> looks at the top ${_.size(player_cards.revealed)} cards of their deck`)
+
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: 'Choose any number of cards to trash (You may discard after this):',
+        cards: player_cards.revealed,
+        minimum: 0,
+        maximum: 0
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Sentry.trash_cards)
+
+      if (_.size(player_cards.revealed) > 0) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: 'Choose any number of cards to discard:',
+          cards: player_cards.revealed,
+          minimum: 0,
+          maximum: 0
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Sentry.discard_cards)
+      }
+
+      if (_.size(player_cards.revealed) === 1) {
+        Sentry.replace_cards(game, player_cards, _.map(player_cards.revealed, 'name'))
+      } else if (_.size(player_cards.revealed) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'sort_cards',
+          instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
+          cards: player_cards.revealed
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Sentry.replace_cards)
+      }
+    }
+  }
+
+  static trash_cards(game, player_cards, selected_cards) {
+    if (_.size(selected_cards) === 0) {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> does not trash anything`)
+    } else {
+      let card_trasher = new CardTrasher(game, player_cards, 'revealed', _.map(selected_cards, 'name'))
+      card_trasher.trash()
+    }
+  }
+
+  static discard_cards(game, player_cards, selected_cards) {
+    if (_.size(selected_cards) === 0) {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> does not discard anything`)
+    } else {
+      let card_discarder = new CardDiscarder(game, player_cards, 'revealed', _.map(selected_cards, 'name'))
+      card_discarder.discard()
+    }
+  }
+
+  static replace_cards(game, player_cards, ordered_card_names) {
+    _.each(ordered_card_names.reverse(), function(card_name) {
+      let revealed_card_index = _.findIndex(player_cards.revealed, function(card) {
+        return card.name === card_name
+      })
+      let revealed_card = player_cards.revealed.splice(revealed_card_index, 1)[0]
+      player_cards.deck.unshift(revealed_card)
+    })
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places the remaining cards back on their deck`)
+    player_cards.revealed = []
   }
 
 }
