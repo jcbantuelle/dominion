c71fa748feb8fd2ace79239895b5a53fe8e13d18
Justin Bantuelle <justin@mudbugmedia.com>
2018-03-22 19:52:24 -0500

    Update Throne Room To Be Optional As Per Errata

--- app/cards/base/mine.js
+++ app/cards/base/mine.js
@@ -20,44 +20,48 @@ Mine = class Mine extends Card {
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Choose a treasure to trash:',
+        instructions: 'Choose a treasure to trash (Or none to skip):',
         cards: eligible_cards,
-        minimum: 1,
+        minimum: 0,
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Mine.trash_card)
     } else {
-      game.log.push(`&nbsp;&nbsp;but there are no treasures in hand`)
+      game.log.push(`&nbsp;&nbsp;but does not trash a treasure`)
     }
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
+    if (!_.isEmpty(selected_cards)) {
+      let selected_card = selected_cards[0]
 
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card.name)
-    card_trasher.trash()
+      let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card.name)
+      card_trasher.trash()
 
-    let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && _.includes(_.words(card.top_card.types), 'treasure') && CardCostComparer.card_less_than(game, selected_card, card.top_card, 4)
-    })
-
-    if (_.size(eligible_cards) > 0) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        game_cards: true,
-        instructions: 'Choose a card to gain:',
-        cards: eligible_cards,
-        minimum: 1,
-        maximum: 1
+      let eligible_cards = _.filter(game.cards, function(card) {
+        return card.count > 0 && card.top_card.purchasable && _.includes(_.words(card.top_card.types), 'treasure') && CardCostComparer.card_less_than(game, selected_card, card.top_card, 4)
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Mine.gain_card)
+
+      if (_.size(eligible_cards) > 0) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          game_cards: true,
+          instructions: 'Choose a card to gain:',
+          cards: eligible_cards,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Mine.gain_card)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no available treasures to gain`)
+      }
     } else {
-      game.log.push(`&nbsp;&nbsp;but there are no available treasures to gain`)
+      game.log.push(`&nbsp;&nbsp;but does not trash a treasure`)
     }
   }
 
