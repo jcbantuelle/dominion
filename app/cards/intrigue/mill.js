5209b54b26fba094a8fe6482173a5c4f2b0a622d
Justin Bantuelle <justin@mudbugmedia.com>
2018-03-25 14:55:18 -0500

    Implement Mill

--- app/cards/intrigue/mill.js
+++ app/cards/intrigue/mill.js
@@ -13,6 +13,65 @@ Mill = class Mill extends Card {
   }
 
   play(game, player_cards) {
+    let card_drawer = new CardDrawer(game, player_cards)
+    card_drawer.draw(1)
+
+    game.turn.actions += 1
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+
+    if (_.size(player_cards.hand) > 0) {
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_yes_no',
+        instructions: 'Discard 2 Cards?',
+        minimum: 1,
+        maximum: 1
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Mill.choose_discard)
+    } else {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards in hand`)
+    }
+  }
+
+  static choose_discard(game, player_cards, response) {
+    if (response === 'yes') {
+      if (_.size(player_cards.hand) > 2) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: 'Choose 2 cards to discard:',
+          cards: player_cards.hand,
+          minimum: 2,
+          maximum: 2
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Mill.discard_cards)
+      } else {
+        Mill.discard_cards(game, player_cards, player_cards.hand)
+      }
+    } else {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> chooses not to discard`)
+    }
+  }
+
+  static discard_cards(game, player_cards, selected_cards) {
+    let discarded_card_count = _.size(selected_cards)
+
+    let card_discarder = new CardDiscarder(game, player_cards, 'hand', _.map(selected_cards, 'name'))
+    card_discarder.discard()
+
+    if (discarded_card_count === 2) {
+      let gained_coins = CoinGainer.gain(game, player_cards, 2)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    }
   }
 
 }
