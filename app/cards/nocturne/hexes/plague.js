76899e58bca8c50a6b47735083ed206e926bf120
Justin Bantuelle <justin@mudbugmedia.com>
2018-04-07 21:55:38 -0500

    Implement Plague

---
+++ app/cards/nocturne/hexes/plague.js
@@ -0,0 +1,8 @@
+Plague = class Plague extends Hex {
+
+  receive(game, player_cards) {
+    let card_gainer = new CardGainer(game, player_cards, 'hand', 'Curse')
+    card_gainer.gain_game_card()
+  }
+
+}
