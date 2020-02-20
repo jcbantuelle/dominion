ed00a041b62a5eb82049ec98abaa7c98c2938cf4
Justin Bantuelle <justin@mudbugmedia.com>
2020-02-19 18:20:30 -0600

    Render Hover Effect for Landmarks and Events correctly

--- app/game/server/view_objects/card_view.js
+++ app/game/server/view_objects/card_view.js
@@ -23,7 +23,7 @@ CardView = class CardView {
       let width = 220
       let height = 341
       types = _.words(types)
-      if (_.includes(types, 'boon') || _.includes(types, 'state') || _.includes(types, 'hex') || _.includes(types, 'artifact')) {
+      if (_.includes(types, 'boon') || _.includes(types, 'state') || _.includes(types, 'hex') || _.includes(types, 'artifact') || _.includes(types, 'event') || _.includes(types, 'landmark')) {
         width = 341
         height = 220
       }
