edae81df602eed95a584faccec2916f5d59ba012
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-12 09:36:42 -0500

    Temporarily Remove Duchess Logic From Duchy

--- app/cards/common/duchy.js
+++ app/cards/common/duchy.js
@@ -12,25 +12,25 @@ Duchy = class Duchy extends Card {
     return 3
   }
 
-  gain_event(gainer) {
-    let turn_event_id = TurnEventModel.insert({
-      game_id: gainer.game._id,
-      player_id: gainer.player_cards.player_id,
-      username: gainer.player_cards.username,
-      type: 'choose_yes_no',
-      instructions: `Gain a ${CardView.render(new Duchess())}?`,
-      minimum: 1,
-      maximum: 1
-    })
-    let turn_event_processor = new TurnEventProcessor(gainer.game, gainer.player_cards, turn_event_id)
-    turn_event_processor.process(Duchy.gain_duchess)
-  }
+  // gain_event(gainer) {
+  //   let turn_event_id = TurnEventModel.insert({
+  //     game_id: gainer.game._id,
+  //     player_id: gainer.player_cards.player_id,
+  //     username: gainer.player_cards.username,
+  //     type: 'choose_yes_no',
+  //     instructions: `Gain a ${CardView.render(new Duchess())}?`,
+  //     minimum: 1,
+  //     maximum: 1
+  //   })
+  //   let turn_event_processor = new TurnEventProcessor(gainer.game, gainer.player_cards, turn_event_id)
+  //   turn_event_processor.process(Duchy.gain_duchess)
+  // }
 
-  static gain_duchess(game, player_cards, response) {
-    if (response === 'yes') {
-      let card_gainer = new CardGainer(game, player_cards, 'discard', 'Duchess')
-      card_gainer.gain_game_card()
-    }
-  }
+  // static gain_duchess(game, player_cards, response) {
+  //   if (response === 'yes') {
+  //     let card_gainer = new CardGainer(game, player_cards, 'discard', 'Duchess')
+  //     card_gainer.gain_game_card()
+  //   }
+  // }
 
 }
