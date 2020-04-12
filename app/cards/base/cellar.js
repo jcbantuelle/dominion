fac1fcd30ba8e1b1ea1117f00ee474f113087c9f
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-12 09:55:35 -0500

    Refactor Cellar

--- app/cards/base/cellar.js
+++ app/cards/base/cellar.js
@@ -9,8 +9,8 @@ Cellar = class Cellar extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
     if (_.size(player_cards.hand) > 0) {
       let turn_event_id = TurnEventModel.insert({
