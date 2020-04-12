dee599d6ff55091e8b127ce6a9d9a7fe575a927d
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-12 18:05:41 -0500

    Refactor Militia

--- app/cards/base/militia.js
+++ app/cards/base/militia.js
@@ -9,8 +9,8 @@ Militia = class Militia extends Card {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 2)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2)
 
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
@@ -20,13 +20,14 @@ Militia = class Militia extends Card {
     let number_to_discard = _.size(player_cards.hand) - 3
 
     if (number_to_discard > 0) {
+      let card_text = number_to_discard === 1 ? 'card' : 'cards'
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: `Choose ${number_to_discard} cards to discard from hand:`,
+        instructions: `Choose ${number_to_discard} ${card_text} to discard from hand:`,
         cards: player_cards.hand,
         minimum: number_to_discard,
         maximum: number_to_discard
