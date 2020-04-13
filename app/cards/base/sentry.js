65e6d4c84247c69fb0369ce1c0f67870b6426fed
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-12 22:40:50 -0500

    Refactor Sentry

--- app/cards/base/sentry.js
+++ app/cards/base/sentry.js
@@ -12,8 +12,8 @@ Sentry = class Sentry extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
     PlayerCardsModel.update(game._id, player_cards)
     GameModel.update(game._id, game)
@@ -21,17 +21,8 @@ Sentry = class Sentry extends Card {
     if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
       game.log.push(`&nbsp;&nbsp;but has no cards in deck`)
     } else {
-      player_cards.revealed = _.take(player_cards.deck, 2)
-      player_cards.deck = _.drop(player_cards.deck, 2)
-
-      let revealed_card_count = _.size(player_cards.revealed)
-      if (revealed_card_count < 2 && _.size(player_cards.discard) > 0) {
-        DeckShuffler.shuffle(game, player_cards)
-        player_cards.revealed = player_cards.revealed.concat(_.take(player_cards.deck, 2 - revealed_card_count))
-        player_cards.deck = _.drop(player_cards.deck, 2 - revealed_card_count)
-      }
-
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> looks at the top ${_.size(player_cards.revealed)} cards of their deck`)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal_from_deck(2, false)
 
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
@@ -39,7 +30,7 @@ Sentry = class Sentry extends Card {
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Choose any number of cards to trash (You may discard after this):',
+        instructions: 'Choose any number of cards to trash (you may discard after this):',
         cards: player_cards.revealed,
         minimum: 0,
         maximum: 0
@@ -63,20 +54,7 @@ Sentry = class Sentry extends Card {
         turn_event_processor.process(Sentry.discard_cards)
       }
 
-      if (_.size(player_cards.revealed) === 1) {
-        Sentry.replace_cards(game, player_cards, _.map(player_cards.revealed, 'name'))
-      } else if (_.size(player_cards.revealed) > 1) {
-        let turn_event_id = TurnEventModel.insert({
-          game_id: game._id,
-          player_id: player_cards.player_id,
-          username: player_cards.username,
-          type: 'sort_cards',
-          instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
-          cards: player_cards.revealed
-        })
-        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-        turn_event_processor.process(Sentry.replace_cards)
-      }
+      Card.place_revealed_cards_on_deck(game, player_cards)
     }
   }
 
@@ -96,18 +74,6 @@ Sentry = class Sentry extends Card {
       let card_discarder = new CardDiscarder(game, player_cards, 'revealed', selected_cards)
       card_discarder.discard()
     }
-  }
-
-  static replace_cards(game, player_cards, ordered_cards) {
-    _.each(ordered_cards.reverse(), function(ordered_card) {
-      let revealed_card_index = _.findIndex(player_cards.revealed, function(card) {
-        return card.id === ordered_card.id
-      })
-      let revealed_card = player_cards.revealed.splice(revealed_card_index, 1)[0]
-      player_cards.deck.unshift(revealed_card)
-    })
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places the remaining cards back on their deck`)
-    player_cards.revealed = []
   }
 
 }
