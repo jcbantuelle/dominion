a9d3a70b799c6bbc7e9c117b20c180f9d4adbb67
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-14 16:16:07 -0500

    Refactor Upgrade

--- app/cards/intrigue/upgrade.js
+++ app/cards/intrigue/upgrade.js
@@ -12,10 +12,10 @@ Upgrade = class Upgrade extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
-    if (_.size(player_cards.hand) > 0) {
+    if (_.size(player_cards.hand) > 1) {
       PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
@@ -30,6 +30,8 @@ Upgrade = class Upgrade extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Upgrade.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      Upgrade.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
@@ -40,10 +42,10 @@ Upgrade = class Upgrade extends Card {
     card_trasher.trash()
 
     let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_equal_to(game, selected_cards[0], card.top_card, 1)
+      return card.count > 0 && card.supply && CardCostComparer.card_equal_to(game, selected_cards[0], card.top_card, 1)
     })
 
-    if (_.size(eligible_cards) > 0) {
+    if (_.size(eligible_cards) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -57,6 +59,8 @@ Upgrade = class Upgrade extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Upgrade.gain_card)
+    } else if (_.size(eligible_cards) === 1) {
+      Upgrade.gain_card(game, player_cards, eligible_cards)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
     }
