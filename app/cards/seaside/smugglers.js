af0ad8b9ed44a82d76c79c96d49e7db5abb82f40
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-14 22:45:21 -0500

    Refactor Smugglers

--- app/cards/seaside/smugglers.js
+++ app/cards/seaside/smugglers.js
@@ -13,13 +13,12 @@ Smugglers = class Smugglers extends Card {
     let previous_player_cards = PlayerCardsModel.findOne(game._id, previous_player_query.previous_player()._id)
 
     let eligible_cards = _.filter(previous_player_cards.last_turn_gained_cards, function(gained_card) {
-      let game_stack = _.find(game.cards, function(stack) {
-        return stack.name === gained_card.stack_name
+      return _.find(game.cards, function(stack) {
+        return stack.name === gained_card.stack_name && stack.supply && gained_card.name === stack.top_card.name && CardCostComparer.coin_less_than(game, stack.top_card, 7)
       })
-      return game_stack && gained_card.name === game_stack.top_card.name && CardCostComparer.coin_less_than(game, game_stack.top_card, 7)
     })
 
-    if (_.size(eligible_cards) > 0) {
+    if (_.size(eligible_cards) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -33,25 +32,16 @@ Smugglers = class Smugglers extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Smugglers.gain_card)
+    } else if (_.size(eligible_cards) === 1) {
+      Smugglers.gain_card(game, player_cards, eligible_cards)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
     }
   }
 
   static gain_card(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> selects ${CardView.render(selected_card)}`)
-
-    let game_stack = _.find(game.cards, function(stack) {
-      return stack.name === selected_card.stack_name
-    })
-
-    if (game_stack.count > 0 && game_stack.top_card.purchasable && game_stack.top_card.name === selected_card.name) {
-      let card_gainer = new CardGainer(game, player_cards, 'discard', selected_card.name)
-      card_gainer.gain_game_card()
-    } else {
-      game.log.push(`&nbsp;&nbsp;but can not gain a copy of it`)
-    }
+    let card_gainer = new CardGainer(game, player_cards, 'discard', selected_cards[0].name)
+    card_gainer.gain_game_card()
   }
 
 }
