e38fcb2fb10ebab8d5269986f3a17ad95e753691
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-15 15:25:45 -0500

    Refactor Bazaar

--- app/cards/seaside/bazaar.js
+++ app/cards/seaside/bazaar.js
@@ -12,9 +12,11 @@ Bazaar = class Bazaar extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 2
-    let gained_coins = CoinGainer.gain(game, player_cards, 1)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions and +$${gained_coins}`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(2)
+
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(1)
   }
 
 }
