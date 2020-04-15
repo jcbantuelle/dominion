1a4684904873dc686e61afcccab4d3e9800afea0
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-15 14:09:54 -0500

    Refactor Salvager

--- app/cards/seaside/salvager.js
+++ app/cards/seaside/salvager.js
@@ -9,10 +9,10 @@ Salvager = class Salvager extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.buys += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy`)
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
 
-    if (_.size(player_cards.hand) > 0) {
+    if (_.size(player_cards.hand) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -26,20 +26,21 @@ Salvager = class Salvager extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Salvager.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      Salvager.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-    let coin_cost = CostCalculator.calculate(game, selected_card)
+    let coin_cost = CostCalculator.calculate(game, selected_cards[0])
 
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card)
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
     card_trasher.trash()
 
-    let gained_coins = CoinGainer.gain(game, player_cards, coin_cost)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(coin_cost)
   }
 
 }
