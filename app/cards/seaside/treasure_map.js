72aacf88268d0d9aae791412643177b4bcba68cd
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-15 15:15:31 -0500

    Refactor Treasure Map

--- app/cards/seaside/treasure_map.js
+++ app/cards/seaside/treasure_map.js
@@ -8,22 +8,31 @@ TreasureMap = class TreasureMap extends Card {
     return 4
   }
 
-  play(game, player_cards, player) {
-    let card_trasher = new CardTrasher(game, player_cards, 'playing', player.played_card)
-    card_trasher.trash()
+  play(game, player_cards, card_player) {
+    let card_trasher = new CardTrasher(game, player_cards, 'in_play', card_player.card)
+    let trashed_cards = card_trasher.trash()
+
+    let first_trashed = _.find(trashed_cards, (card) => {
+      return card.id === card_player.card.id
+    })
 
     let map_in_hand = _.find(player_cards.hand, function(card) {
       return card.name === 'Treasure Map'
     })
-
     if (map_in_hand) {
       let card_trasher = new CardTrasher(game, player_cards, 'hand', map_in_hand)
-      card_trasher.trash()
+      trashed_cards = card_trasher.trash()
 
-      _.times(4, function() {
-        let card_gainer = new CardGainer(game, player_cards, 'deck', 'Gold')
-        card_gainer.gain_game_card()
+      let second_trashed = _.find(trashed_cards, (card) => {
+        return card.id === map_in_hand.id
       })
+
+      if (first_trashed && second_trashed) {
+        _.times(4, function() {
+          let card_gainer = new CardGainer(game, player_cards, 'deck', 'Gold')
+          card_gainer.gain_game_card()
+        })
+      }
     }
   }
 
