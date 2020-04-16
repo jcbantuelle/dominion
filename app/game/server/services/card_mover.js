325508717e7063cc295b2b98b0614668438eac48
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-16 18:49:24 -0500

    Allow CardMover To Move From Supply

--- app/game/server/services/card_mover.js
+++ app/game/server/services/card_mover.js
@@ -35,6 +35,24 @@ CardMover = class CardMover {
     })
   }
 
+  take_from_supply(destination, supply_name) {
+    let supply_pile = _.find(this.game.cards, (pile) => {
+      return pile.name === supply_name
+    })
+    if (supply_pile && supply_pile.count > 0) {
+      let card = supply_pile.stack.splice(0, 1)
+      destination.splice(0, 0, card)
+      supply_pile.count -= 1
+
+      if (supply_pile.count > 0) {
+        supply_pile.top_card = _.head(supply_pile.stack)
+      }
+      return true
+    } else {
+      return false
+    }
+  }
+
   card_index(source, card) {
     return _.findIndex(source, (source_card) => {
       return source_card.id === card.id
