2ac792f854a449fae138e42ba828800b5087b885
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-15 20:40:58 -0500

    Refactor Apprentice

--- app/cards/alchemy/apprentice.js
+++ app/cards/alchemy/apprentice.js
@@ -9,10 +9,10 @@ Apprentice = class Apprentice extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
-    if (_.size(player_cards.hand) > 0) {
+    if (_.size(player_cards.hand) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -26,24 +26,24 @@ Apprentice = class Apprentice extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Apprentice.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      Apprentice.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-    let coin_cost = CostCalculator.calculate(game, selected_card)
-
-    if (selected_card.potion_cost > 0) {
-      coin_cost += 2
+    let cards_to_draw = CostCalculator.calculate(game, selected_cards[0])
+    if (selected_cards[0].potion_cost > 0) {
+      cards_to_draw += 2
     }
 
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card)
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards[0])
     card_trasher.trash()
 
     let card_drawer = new CardDrawer(game, player_cards)
-    card_drawer.draw(coin_cost)
+    card_drawer.draw(cards_to_draw)
   }
 
 }
