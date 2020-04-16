d253f886c5ae1a1400207247ca2c278369572560
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-15 19:15:05 -0500

    Improve Scout Log Feedback

--- app/cards/intrigue/scout.js
+++ app/cards/intrigue/scout.js
@@ -18,12 +18,16 @@ Scout = class Scout extends Card {
       let card_revealer = new CardRevealer(game, player_cards)
       card_revealer.reveal_from_deck(4)
 
-      _.each(_.clone(player_cards.revealed), function(card) {
-        if (_.includes(_.words(card.types), 'victory')) {
+      let victory_cards = _.filter(player_cards.revealed, (card) => {
+        return _.includes(_.words(card.types), 'victory')
+      })
+      if (!_.isEmpty(victory_cards)) {
+        _.each(victory_cards, function(card) {
           let card_mover = new CardMover(game, player_cards)
           card_mover.move(player_cards.revealed, player_cards.hand, card)
-        }
-      })
+        })
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(victory_cards)} in hand`)
+      }
 
       GameModel.update(game._id, game)
       PlayerCardsModel.update(game._id, player_cards)
