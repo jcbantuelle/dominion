48d58feb05561c3f2af891d2a158a8be1b7402cd
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 04:33:43 -0500

    Refactor Develop

--- app/cards/hinterlands/develop.js
+++ app/cards/hinterlands/develop.js
@@ -9,7 +9,7 @@ Develop = class Develop extends Card {
   }
 
   play(game, player_cards) {
-    if (_.size(player_cards.hand) > 0) {
+    if (_.size(player_cards.hand) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -23,23 +23,26 @@ Develop = class Develop extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Develop.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      Develop.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    game.turn.develop_card = selected_cards[0]
-
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', game.turn.develop_card)
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards[0])
     card_trasher.trash()
+
+    GameModel.update(game._id, game)
+    PlayerCardsModel.update(game._id, player_cards)
 
     let turn_event_id = TurnEventModel.insert({
       game_id: game._id,
       player_id: player_cards.player_id,
       username: player_cards.username,
       type: 'choose_options',
-      instructions: `Choose Which To Gain First:`,
+      instructions: `Choose which to gain first:`,
       minimum: 1,
       maximum: 1,
       options: [
@@ -47,31 +50,27 @@ Develop = class Develop extends Card {
         {text: 'Gain -$1 Card', value: 'gain_less'}
       ]
     })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, selected_cards[0])
     turn_event_processor.process(Develop.process_response)
-
-    delete game.turn.develop_card
   }
 
-  static process_response(game, player_cards, response) {
-    response = response[0]
-
-    let first_cost = response === 'gain_more' ? 1 : -1
+  static process_response(game, player_cards, response, trashed_card) {
+    let first_cost = response[0] === 'gain_more' ? 1 : -1
     let second_cost = first_cost * -1
 
-    Develop.choose_card(game, player_cards, first_cost)
+    Develop.choose_card(game, player_cards, first_cost, trashed_card)
     GameModel.update(game._id, game)
-    Develop.choose_card(game, player_cards, second_cost)
+    Develop.choose_card(game, player_cards, second_cost, trashed_card)
   }
 
-  static choose_card(game, player_cards, coin_modifier) {
+  static choose_card(game, player_cards, coin_modifier, trashed_card) {
     let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_equal_to(game, game.turn.develop_card, card.top_card, coin_modifier)
+      return card.count > 0 && card.supply && CardCostComparer.card_equal_to(game, trashed_card, card.top_card, coin_modifier)
     })
 
-    let coin_cost = CostCalculator.calculate(game, game.turn.develop_card) + coin_modifier
-    let debt_cost = game.turn.develop_card.debt_cost > 0 ? `<span class="debt">${game.turn.develop_card.debt_cost}</span>` : ''
-    let potion_symbols = _.times(game.turn.develop_card.potion_cost, function() {
+    let coin_cost = CostCalculator.calculate(game, trashed_card) + coin_modifier
+    let debt_cost = trashed_card.debt_cost > 0 ? `<span class="debt">${game.turn.develop_card.debt_cost}</span>` : ''
+    let potion_symbols = _.times(trashed_card.potion_cost, function() {
       return '&oplus;'
     }).join('')
 
