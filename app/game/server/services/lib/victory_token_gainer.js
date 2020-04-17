a6e1f34e1ebd9892ea049ad0a7ead2a1e3060a46
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-16 22:44:17 -0500

    Add Source Option To Victory Token Gainer

--- app/game/server/services/lib/victory_token_gainer.js
+++ app/game/server/services/lib/victory_token_gainer.js
@@ -1,13 +1,15 @@
 VictoryTokenGainer = class VictoryTokenGainer {
 
-  constructor(game, player_cards) {
+  constructor(game, player_cards, source) {
     this.game = game
     this.player_cards = player_cards
+    this.source = source
   }
 
   gain(amount) {
     this.player_cards.victory_tokens += amount
-    this.game.log.push(`&nbsp;&nbsp;<strong>${this.player_cards.username}</strong> takes +${amount} &nabla;`)
+    let source_text = this.source ? ` due to ${CardView.render(this.source)}` : ''
+    this.game.log.push(`&nbsp;&nbsp;<strong>${this.player_cards.username}</strong> takes +${amount} &nabla;${source_text}`)
   }
 
 }
