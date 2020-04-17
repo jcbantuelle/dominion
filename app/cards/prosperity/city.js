4acb6cfcf7161277db7b2063ed637bd67c0a1c92
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-16 20:45:00 -0500

    Refactor City

--- app/cards/prosperity/city.js
+++ app/cards/prosperity/city.js
@@ -10,25 +10,23 @@ City = class City extends Card {
 
   play(game, player_cards) {
     let empty_pile_count = _.size(_.filter(game.cards, function(game_card) {
-      return game_card.count === 0 && game_card.top_card.purchasable
+      return game_card.count === 0 && game_card.supply
     }))
 
-    let cards = 1
-    if (empty_pile_count > 0) {
-      cards += 1
-    }
+    let cards = empty_pile_count > 0 ? 2 : 1
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(cards)
 
-    game.turn.actions += 2
-    let log_message = `&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`
-    if (empty_pile_count > 1) {
-      game.turn.buys += 1
-      let gained_coins = CoinGainer.gain(game, player_cards, 1)
-      log_message += `, +1 buy, and +$${gained_coins}`
-    }
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(2)
 
-    game.log.push(log_message)
+    if (empty_pile_count > 1) {
+      let buy_gainer = new BuyGainer(game, player_cards)
+      buy_gainer.gain(1)
+
+      let coin_gainer = new CoinGainer(game, player_cards)
+      coin_gainer.gain(1)
+    }
   }
 
 }
