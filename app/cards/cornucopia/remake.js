1e309e40224c92ba25735ae05db9b87f8b4846f0
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 02:18:08 -0500

    Refactor Remake

--- app/cards/cornucopia/remake.js
+++ app/cards/cornucopia/remake.js
@@ -9,42 +9,37 @@ Remake = class Remake extends Card {
   }
 
   play(game, player_cards) {
-    this.choose_card(game, player_cards)
+    _.times(2, () => {
+      if (_.size(player_cards.hand) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `Choose a card to trash:`,
+          cards: player_cards.hand,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Remake.trash_card)
+      } else if (_.size(player_cards.hand) === 1) {
+        Remake.trash_card(game, player_cards, player_cards.hand)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
+      }
+    })
     GameModel.update(game._id, game)
     PlayerCardsModel.update(game._id, player_cards)
-    this.choose_card(game, player_cards)
-  }
-
-  choose_card(game, player_cards) {
-    if (_.size(player_cards.hand) > 1) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: `Choose a card to trash:`,
-        cards: player_cards.hand,
-        minimum: 1,
-        maximum: 1
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Remake.trash_card)
-    } else if (_.size(player_cards.hand) === 1) {
-      Remake.trash_card(game, player_cards, player_cards.hand)
-    } else {
-      game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
-    }
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let trashed_card = selected_cards[0]
-
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', trashed_card)
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
     card_trasher.trash()
 
-    let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable  && CardCostComparer.card_equal_to(game, trashed_card, card.top_card, 1)
+    let eligible_cards = _.filter(game.cards, (card) => {
+      return card.count > 0 && card.supply  && CardCostComparer.card_equal_to(game, selected_cards[0], card.top_card, 1)
     })
 
     if (_.size(eligible_cards) > 0) {
