48e989396e4cf32ea2bd4d85d658c616402b0ac9
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 02:55:20 -0500

    Refactor Diadem

--- app/cards/cornucopia/diadem.js
+++ app/cards/cornucopia/diadem.js
@@ -9,8 +9,8 @@ Diadem = class Diadem extends Card {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 2 + game.turn.actions)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2 + game.turn.actions)
   }
 
 }
