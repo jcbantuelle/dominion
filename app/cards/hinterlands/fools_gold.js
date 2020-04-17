fa19f90ea241d99a4285fdbc0aee34767868ce99
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 04:26:56 -0500

    Refactor Fools Gold

--- app/cards/hinterlands/fools_gold.js
+++ app/cards/hinterlands/fools_gold.js
@@ -8,37 +8,22 @@ FoolsGold = class FoolsGold extends Card {
     return 2
   }
 
-  play(game, player_cards) {
-    if (game.turn.fools_gold) {
-      CoinGainer.gain(game, player_cards, 4)
-    } else {
+  play(game, player_cards, card_player) {
+    let coin_to_gain = 4
+    if (!game.turn.fools_gold) {
       game.turn.fools_gold = true
-      CoinGainer.gain(game, player_cards, 1)
+      coin_to_gain = 1
     }
+    let coin_gainer = new CoinGainer(game, player_cards, card_player.card)
+    coin_gainer.gain(coin_to_gain)
   }
 
-  gain_reaction(game, player_cards, gainer, card) {
-    let turn_event_id = TurnEventModel.insert({
-      game_id: game._id,
-      player_id: player_cards.player_id,
-      username: player_cards.username,
-      type: 'choose_yes_no',
-      instructions: `Trash ${CardView.render(card)} to gain a ${CardView.render(new Gold())}?`,
-      minimum: 1,
-      maximum: 1
-    })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, card)
-    turn_event_processor.process(FoolsGold.trash_for_gold)
-  }
+  gain_reaction(game, player_cards, gainer, fools_gold) {
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', fools_gold)
+    card_trasher.trash()
 
-  static trash_for_gold(game, player_cards, response, card) {
-    if (response === 'yes') {
-      let card_trasher = new CardTrasher(game, player_cards, 'hand', card.id)
-      card_trasher.trash()
-
-      let card_gainer = new CardGainer(game, player_cards, 'deck', 'Gold')
-      card_gainer.gain()
-    }
+    let card_gainer = new CardGainer(game, player_cards, 'deck', 'Gold')
+    card_gainer.gain()
   }
 
 }
