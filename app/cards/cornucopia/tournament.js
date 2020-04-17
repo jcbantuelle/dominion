89147aa80480e7c5621a8d68036033caece3865b
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 02:54:45 -0500

    Refactor Tournament

--- app/cards/cornucopia/tournament.js
+++ app/cards/cornucopia/tournament.js
@@ -9,8 +9,8 @@ Tournament = class Tournament extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
     game.turn.self_revealed_province = false
     game.turn.opponent_revealed_province = false
@@ -32,6 +32,8 @@ Tournament = class Tournament extends Card {
         })
         let turn_event_processor = new TurnEventProcessor(game, other_player_cards, turn_event_id, province)
         turn_event_processor.process(Tournament.reveal_province)
+      } else {
+        game.log.push(`&nbsp;&nbsp;<strong>${other_player_cards.username}</strong> does not reveal a ${CardView.render(new Province())}`)
       }
     })
 
@@ -39,29 +41,40 @@ Tournament = class Tournament extends Card {
       let card_discarder = new CardDiscarder(game, player_cards, 'hand', game.turn.self_revealed_province)
       card_discarder.discard()
 
-      let duchy = new Duchy()
-
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: `Choose a prize or ${CardView.render(duchy)} to gain:`,
-        cards: game.prizes.concat(duchy.to_h()),
-        minimum: 1,
-        maximum: 1
+      let duchies = _.find(game.cards, (card) => {
+        return card.name === 'Duchy'
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Tournament.gain_prize)
+      let eligible_cards = _.clone(game.prizes)
+      if (duchies.count > 0) {
+        eligible_cards.push(duchies.top_card)
+      }
+      if (_.size(eligible_cards) > 1) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `Choose a prize to gain:`,
+          cards: eligible_cards,
+          minimum: 1,
+          maximum: 1
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Tournament.gain_prize)
+      } else if (_.size(eligible_cards) === 1) {
+        Tournament.gain_prize(game, player_cards, eligible_cards)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but there are no Prizes left to gain`)
+      }
     }
 
     if (!game.turn.opponent_revealed_province) {
       let card_drawer = new CardDrawer(game, player_cards)
       card_drawer.draw(1)
 
-      let gained_coins = CoinGainer.gain(game, player_cards, 1)
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+      let coin_gainer = new CoinGainer(game, player_cards)
+      coin_gainer.gain(1)
     }
 
     delete game.turn.self_revealed_province
@@ -70,22 +83,24 @@ Tournament = class Tournament extends Card {
 
   static reveal_province(game, player_cards, response, province) {
     if (response === 'yes') {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(new Province())}`)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal('hand', province)
       if (game.turn.player._id === player_cards.player_id) {
-        game.turn.self_revealed_province = province
+        game.turn.self_revealed_province = _.clone(province)
       } else {
-        game.turn.opponent_revealed_province = province
+        game.turn.opponent_revealed_province = true
       }
+    } else {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> does not reveal a ${CardView.render(province)}`)
     }
   }
 
   static gain_prize(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-    let card_gainer = new CardGainer(game, player_cards, 'deck', selected_card.name)
-    if (selected_card.name === 'Duchy') {
+    let card_gainer = new CardGainer(game, player_cards, 'deck', selected_cards[0].name)
+    if (selected_cards[0].name === 'Duchy') {
       card_gainer.gain()
     } else {
-      card_gainer.gain_prize_card()
+      card_gainer.gain(game.prizes)
     }
   }
 
