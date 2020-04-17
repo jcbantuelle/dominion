289bb63862de0e4ffe2edbdf5ae61bf5b963816a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 02:55:14 -0500

    Refactor Followers

--- app/cards/cornucopia/followers.js
+++ app/cards/cornucopia/followers.js
@@ -26,6 +26,7 @@ Followers = class Followers extends Card {
     let number_to_discard = _.size(player_cards.hand) - 3
 
     if (number_to_discard > 0) {
+      GameModel.update(game._id, game)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -40,7 +41,8 @@ Followers = class Followers extends Card {
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Followers.discard_from_hand)
     } else {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
+      let card_text = _.size(player_cards.hand) === 1 ? 'card' : 'cards'
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} ${card_text} in hand`)
     }
   }
 
