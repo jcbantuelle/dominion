22d85c1408c265734a1cd31f2843e535a8696aec
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 01:38:04 -0500

    Refactor Menagerie

--- app/cards/cornucopia/menagerie.js
+++ app/cards/cornucopia/menagerie.js
@@ -9,13 +9,13 @@ Menagerie = class Menagerie extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.hand)}`)
+    let card_revealer = new CardRevealer(game, player_cards)
+    card_revealer.reveal('hand')
 
     let unique_cards = _.uniqBy(player_cards.hand, 'name')
-
     let card_count = _.size(unique_cards) === _.size(player_cards.hand) ? 3 : 1
 
     let card_drawer = new CardDrawer(game, player_cards)
