60c4dac00d62df513efe61e6916cb02df3568496
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 19:31:54 -0500

    Refactor Beggar

--- app/cards/dark_ages/beggar.js
+++ app/cards/dark_ages/beggar.js
@@ -15,8 +15,8 @@ Beggar = class Beggar extends Card {
     })
   }
 
-  attack_event(game, player_cards, card) {
-    let card_discarder = new CardDiscarder(game, player_cards, 'hand', card)
+  attack_event(game, player_cards, beggar) {
+    let card_discarder = new CardDiscarder(game, player_cards, 'hand', beggar)
     card_discarder.discard()
 
     let card_gainer = new CardGainer(game, player_cards, 'deck', 'Silver')
