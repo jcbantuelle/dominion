7ee9e245425f8fa4ed54fd4ae589b04a068d71c3
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 21:35:15 -0500

    Refactor Storeroom

--- app/cards/dark_ages/storeroom.js
+++ app/cards/dark_ages/storeroom.js
@@ -9,68 +9,48 @@ Storeroom = class Storeroom extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.buys += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy`)
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
 
-    if (_.size(player_cards.hand) > 0) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: 'Choose any number of cards to discard:',
-        cards: player_cards.hand,
-        minimum: 0,
-        maximum: 0
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Storeroom.discard_for_cards)
-
+    _.each(['cards', 'coins'], (discard_type) => {
       if (_.size(player_cards.hand) > 0) {
+        GameModel.update(game._id, game)
         PlayerCardsModel.update(game._id, player_cards)
+
         let turn_event_id = TurnEventModel.insert({
           game_id: game._id,
           player_id: player_cards.player_id,
           username: player_cards.username,
           type: 'choose_cards',
           player_cards: true,
-          instructions: 'Choose any number of cards to discard:',
+          instructions: `Choose any number of cards to discard for ${discard_type}:`,
           cards: player_cards.hand,
           minimum: 0,
           maximum: 0
         })
-        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-        turn_event_processor.process(Storeroom.discard_for_coins)
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, discard_type)
+        turn_event_processor.process(Storeroom.discard_cards)
       } else {
         game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
+        return false
       }
-    } else {
-      game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
-    }
+    })
   }
 
-  static discard_for_cards(game, player_cards, selected_cards) {
+  static discard_cards(game, player_cards, selected_cards, option) {
     if (_.size(selected_cards) === 0) {
       game.log.push(`&nbsp;&nbsp;but does not discard anything`)
     } else {
       let card_discarder = new CardDiscarder(game, player_cards, 'hand', selected_cards)
       card_discarder.discard()
 
-      let card_drawer = new CardDrawer(game, player_cards)
-      card_drawer.draw(_.size(selected_cards))
-    }
-  }
-
-  static discard_for_coins(game, player_cards, selected_cards) {
-    if (_.size(selected_cards) === 0) {
-      game.log.push(`&nbsp;&nbsp;but does not discard anything`)
-    } else {
-      let card_discarder = new CardDiscarder(game, player_cards, 'hand', selected_cards)
-      card_discarder.discard()
-
-      let gained_coins = CoinGainer.gain(game, player_cards, _.size(selected_cards))
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+      if (option === 'cards') {
+        let card_drawer = new CardDrawer(game, player_cards)
+        card_drawer.draw(_.size(selected_cards))
+      } else if (option === 'coins') {
+        let coin_gainer = new CoinGainer(game, player_cards)
+        coin_gainer.gain(_.size(selected_cards))
+      }
     }
   }
 
