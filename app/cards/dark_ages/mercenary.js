5aaeb5f803767d71ba270e78b5140f5791ea1126
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-18 10:03:24 -0500

    Refactor Mercenary

--- app/cards/dark_ages/mercenary.js
+++ app/cards/dark_ages/mercenary.js
@@ -27,27 +27,31 @@ Mercenary = class Mercenary extends Card {
 
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
+
+    delete game.turn.mercenary_attack
   }
 
   attack(game, player_cards) {
-    let number_to_discard = _.size(player_cards.hand) - 3
+    if (game.turn.mercenary_attack) {
+      let number_to_discard = _.size(player_cards.hand) - 3
 
-    if (number_to_discard > 0) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: `Choose ${number_to_discard} cards to discard from hand:`,
-        cards: player_cards.hand,
-        minimum: number_to_discard,
-        maximum: number_to_discard
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Mercenary.discard_from_hand)
-    } else {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
+      if (number_to_discard > 0) {
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `Choose ${number_to_discard} cards to discard from hand:`,
+          cards: player_cards.hand,
+          minimum: number_to_discard,
+          maximum: number_to_discard
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Mercenary.discard_from_hand)
+      } else {
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
+      }
     }
   }
 
@@ -75,21 +79,23 @@ Mercenary = class Mercenary extends Card {
       } else {
         Mercenary.trash_cards(game, player_cards, player_cards.hand)
       }
+    } else {
+      game.log.push(`&nbsp;&nbsp;but chooses not to trash`)
     }
   }
 
   static trash_cards(game, player_cards, selected_cards) {
-    let trashed_card_count = _.size(selected_cards)
-
     let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
-    card_trasher.trash()
+    let trashed_card_count = _.size(card_trasher.trash())
 
     if (trashed_card_count === 2) {
       let card_drawer = new CardDrawer(game, player_cards)
       card_drawer.draw(2)
 
-      let gained_coins = CoinGainer.gain(game, player_cards, 2)
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+      let coin_gainer = new CoinGainer(game, player_cards)
+      coin_gainer.gain(2)
+
+      game.turn.mercenary_attack = true
     }
   }
 
