56317d9933402b05f6bbc0d873a52ec58dadaaec
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 20:46:30 -0500

    Refactor Market Square

--- app/cards/dark_ages/market_square.js
+++ app/cards/dark_ages/market_square.js
@@ -12,13 +12,15 @@ MarketSquare = class MarketSquare extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 1
-    game.turn.buys += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action and +1 buy`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
+
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
   }
 
-  trash_reaction(game, player_cards, trasher, card) {
-    let card_discarder = new CardDiscarder(game, player_cards, 'hand', card)
+  trash_reaction(game, player_cards, trasher, market_square) {
+    let card_discarder = new CardDiscarder(game, player_cards, 'hand', market_square)
     card_discarder.discard()
 
     let card_gainer = new CardGainer(game, player_cards, 'discard', 'Gold')
