001ac418eac848c597fc875bac99cfa79c812995
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-18 10:04:05 -0500

    Refactor Urchin

--- app/cards/dark_ages/urchin.js
+++ app/cards/dark_ages/urchin.js
@@ -12,8 +12,11 @@ Urchin = class Urchin extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
+
+    GameModel.update(game._id, game)
+    PlayerCardsModel.update(game._id, player_cards)
 
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
@@ -38,6 +41,16 @@ Urchin = class Urchin extends Card {
       turn_event_processor.process(Urchin.discard_from_hand)
     } else {
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
+    }
+  }
+
+  play_card_event(card_player, urchin) {
+    let card_trasher = new CardTrasher(card_player.game, card_player.player_cards, 'in_play', urchin)
+    let trashed_cards = card_trasher.trash()
+
+    if (!_.isEmpty(trashed_cards) && trashed_cards[0].id === urchin.id) {
+      let card_gainer = new CardGainer(card_player.game, card_player.player_cards, 'discard', 'Mercenary')
+      card_gainer.gain()
     }
   }
 
