5c2f00d8e1e9b21b29eb8c0c635fdc40c5848ae9
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-18 14:32:44 -0500

    Refactor Ruined Village

--- app/cards/dark_ages/ruined_village.js
+++ app/cards/dark_ages/ruined_village.js
@@ -1,8 +1,8 @@
 RuinedVillage = class RuinedVillage extends Ruins {
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
   }
 
 }
