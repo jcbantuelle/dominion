42790f0277ddaccbc8fc3a4703a6af5ce9fce30a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 19:22:51 -0500

    Refactor Necropolis

--- app/cards/dark_ages/necropolis.js
+++ app/cards/dark_ages/necropolis.js
@@ -9,8 +9,8 @@ Necropolis = class Necropolis extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 2
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(2)
   }
 
 }
