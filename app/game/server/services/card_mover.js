588aee4623e0f3300254f7a61e45c963e86df0f0
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-17 20:38:18 -0500

    Improve CardMover#return_to_supply Feedback

--- app/game/server/services/card_mover.js
+++ app/game/server/services/card_mover.js
@@ -27,12 +27,18 @@ CardMover = class CardMover {
     let supply_pile = _.find(this.game.cards, (pile) => {
       return pile.name === supply_name
     })
+    let return_count = 0
     _.each(cards, (card) => {
-      source.splice(this.card_index(source, card), 1)
-      supply_pile.count += 1
-      supply_pile.stack.splice(0, 0, card)
-      supply_pile.top_card = card
+      let card_index = this.card_index(source, card)
+      if (card_index !== -1) {
+        source.splice(card_index, 1)
+        supply_pile.count += 1
+        supply_pile.stack.splice(0, 0, card)
+        supply_pile.top_card = card
+        return_count += 1
+      }
     })
+    return return_count
   }
 
   take_from_supply(destination, supply_name) {
