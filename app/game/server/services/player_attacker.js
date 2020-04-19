ec262d5a56e42ee16c65d8474a76293ff9199a54
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 07:35:39 -0500

    Track CardPlayer In PlayerAttacker

--- app/game/server/services/player_attacker.js
+++ app/game/server/services/player_attacker.js
@@ -1,11 +1,12 @@
 PlayerAttacker = class PlayerAttacker {
 
-  constructor(game, card) {
+  constructor(game, card, card_player) {
     this.game = game
     this.card = card
+    this.card_player = card_player
   }
 
-  attack(attacker) {
+  attack(attacker_player_cards) {
     let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(this.game)
     ordered_player_cards.shift()
 
@@ -19,7 +20,7 @@ PlayerAttacker = class PlayerAttacker {
         delete attacked_player_cards.moat
         this.game.log.push(`&nbsp;&nbsp;<strong>${attacked_player_cards.username}</strong> is immune to the attack`)
       } else {
-        this.card.attack(this.game, attacked_player_cards, attacker)
+        this.card.attack(this.game, attacked_player_cards, attacker_player_cards, this.card_player)
       }
       GameModel.update(this.game._id, this.game)
       PlayerCardsModel.update(this.game._id, attacked_player_cards)
