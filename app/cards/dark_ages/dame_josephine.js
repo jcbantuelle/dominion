82b1096309b175e86640fafde90e6538f4d61e74
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 07:36:36 -0500

    Refactor Dame Josephine

--- app/cards/dark_ages/dame_josephine.js
+++ app/cards/dark_ages/dame_josephine.js
@@ -12,11 +12,9 @@ DameJosephine = class DameJosephine extends Knights {
     return 2
   }
 
-  play(game, player_cards, player) {
-    let player_attacker = new PlayerAttacker(game, this)
+  play(game, player_cards, card_player) {
+    let player_attacker = new PlayerAttacker(game, this, card_player)
     player_attacker.attack(player_cards)
-
-    this.trash_knight(game, player_cards, player.played_card)
   }
 
 }
