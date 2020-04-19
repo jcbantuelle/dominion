a6750bf645da1744322e4dfacf88397676ad7813
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 11:54:57 -0500

    Refactor Taxman

--- app/cards/guilds/taxman.js
+++ app/cards/guilds/taxman.js
@@ -19,7 +19,7 @@ Taxman = class Taxman extends Card {
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Choose a treasure to trash (Or none to skip):',
+        instructions: 'Choose a treasure to trash (or none to skip):',
         cards: eligible_cards,
         minimum: 0,
         maximum: 1
@@ -33,9 +33,9 @@ Taxman = class Taxman extends Card {
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
 
-    if (game.turn.taxman_trash) {
+    if (game.turn.taxman_attack) {
       let eligible_cards = _.filter(game.cards, function(card) {
-        return card.count > 0 && card.top_card.purchasable && _.includes(_.words(card.top_card.types), 'treasure') && CardCostComparer.card_less_than(game, game.turn.taxman_trash, card.top_card, 4)
+        return card.count > 0 && card.supply && _.includes(_.words(card.top_card.types), 'treasure') && CardCostComparer.card_less_than(game, game.turn.taxman_attack, card.top_card, 4)
       })
 
       if (_.size(eligible_cards) > 0) {
@@ -57,31 +57,33 @@ Taxman = class Taxman extends Card {
       }
     }
 
-    delete game.turn.taxman_trash
+    delete game.turn.taxman_attack
   }
 
   attack(game, player_cards) {
-    if (game.turn.taxman_trash) {
-
+    if (game.turn.taxman_attack) {
       let treasure_to_discard = _.find(player_cards.hand, (card) => {
-        card.name === game.turn.taxman_trash.name
+        return card.name === game.turn.taxman_attack.name
       })
 
       if (treasure_to_discard) {
         let card_discarder = new CardDiscarder(game, player_cards, 'hand', treasure_to_discard)
         card_discarder.discard()
       } else {
-        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.hand)}`)
+        let card_revealer = new CardRevealer(game, player_cards)
+        card_revealer.reveal('hand')
       }
     }
   }
 
   static trash_card(game, player_cards, selected_cards) {
     if (!_.isEmpty(selected_cards)) {
-      game.turn.taxman_trash = selected_cards[0]
+      game.turn.taxman_attack = selected_cards[0]
 
-      let card_trasher = new CardTrasher(game, player_cards, 'hand', game.turn.taxman_trash)
+      let card_trasher = new CardTrasher(game, player_cards, 'hand', game.turn.taxman_attack)
       card_trasher.trash()
+    } else {
+      game.log.push(`&nbsp;&nbsp;but does not trash anything`)
     }
   }
 
