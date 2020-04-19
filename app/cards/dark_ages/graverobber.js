8617b42116c51b40d6cf15e3481521b704d87d82
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 00:55:22 -0500

    Refactor Graverobber

--- app/cards/dark_ages/graverobber.js
+++ app/cards/dark_ages/graverobber.js
@@ -27,9 +27,8 @@ Graverobber = class Graverobber extends Card {
   }
 
   static process_response(game, player_cards, response) {
-    response = response[0]
-    if (response === 'gain') {
-      let eligible_cards = _.filter(game.trash, function(card) {
+    if (response[0] === 'gain') {
+      let eligible_cards = _.filter(game.trash, (card) => {
         return CardCostComparer.coin_between(game, card, 3, 6)
       })
 
@@ -50,10 +49,10 @@ Graverobber = class Graverobber extends Card {
       } else if (_.size(eligible_cards) === 1) {
         Graverobber.gain_from_trash(game, player_cards, eligible_cards)
       } else {
-        game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> chooses to gain from the trash, but there are no eligible cards to gain`)
       }
-    } else if (response === 'trash') {
-      let eligible_cards = _.filter(player_cards.hand, function(card) {
+    } else if (response[0] === 'trash') {
+      let eligible_cards = _.filter(player_cards.hand, (card) => {
         return _.includes(_.words(card.types), 'action')
       })
       if (_.size(eligible_cards) > 1) {
@@ -79,16 +78,14 @@ Graverobber = class Graverobber extends Card {
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-
-    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card)
-    card_trasher.trash()
-
-    let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_less_than(game, selected_card, card.top_card, 4)
+    let eligible_cards = _.filter(game.cards, (card) => {
+      return card.count > 0 && card.supply && CardCostComparer.card_less_than(game, selected_cards[0], card.top_card, 4)
     })
 
-    if (_.size(eligible_cards) > 0) {
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
+    card_trasher.trash()
+
+    if (_.size(eligible_cards) > 1) {
       GameModel.update(game._id, game)
       PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
@@ -104,6 +101,8 @@ Graverobber = class Graverobber extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Graverobber.gain_from_supply)
+    } else if (_.size(eligible_cards) === 1) {
+      Graverobber.gain_from_supply(game, player_cards, eligible_cards)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
     }
