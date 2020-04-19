28cacd4e51d3f67f637f141229db6a36e55a909f
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 00:22:07 -0500

    Refactor Bandit Camp

--- app/cards/dark_ages/bandit_camp.js
+++ app/cards/dark_ages/bandit_camp.js
@@ -12,8 +12,8 @@ BanditCamp = class BanditCamp extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 2
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(2)
 
     let card_gainer = new CardGainer(game, player_cards, 'discard', 'Spoils')
     card_gainer.gain()
