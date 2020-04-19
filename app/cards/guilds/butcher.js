b9f0caf54ee0db2006a8d670350f6c28e36b3d9a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 14:08:33 -0500

    Refactor Butcher

--- app/cards/guilds/butcher.js
+++ app/cards/guilds/butcher.js
@@ -9,49 +9,25 @@ Butcher = class Butcher extends Card {
   }
 
   play(game, player_cards) {
-    if (game.turn.possessed) {
-      possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-      possessing_player_cards.coin_tokens += 2
-      game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> takes 2 coin tokens`)
-      PlayerCardsModel.update(game._id, possessing_player_cards)
-    } else {
-      player_cards.coin_tokens += 2
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> takes 2 coin tokens`)
-    }
+    let coffer_gainer = new CofferGainer(game, player_cards)
+    coffer_gainer.gain(2)
 
     if (_.size(player_cards.hand) > 0) {
       GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Choose a card to trash (Or none to skip):',
+        instructions: 'Choose a card to trash (or none to skip):',
         cards: player_cards.hand,
         minimum: 0,
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Butcher.trash_card)
-
-      if (game.turn.butcher_card) {
-        GameModel.update(game._id, game)
-        PlayerCardsModel.update(game._id, player_cards)
-        let turn_event_id = TurnEventModel.insert({
-          game_id: game._id,
-          player_id: player_cards.player_id,
-          username: player_cards.username,
-          type: 'overpay',
-          instructions: 'Choose any number of coin tokens to pay:',
-          minimum: 0,
-          maximum: player_cards.coin_tokens
-        })
-        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-        turn_event_processor.process(Butcher.pay_tokens)
-
-        delete game.turn.butcher_card
-      }
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
@@ -59,27 +35,42 @@ Butcher = class Butcher extends Card {
 
   static trash_card(game, player_cards, selected_cards) {
     if (!_.isEmpty(selected_cards)) {
-      let selected_card = selected_cards[0]
-
-      let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_card)
+      let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
       card_trasher.trash()
 
-      game.turn.butcher_card = selected_card
+      if (player_cards.coffers > 0) {
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'overpay',
+          instructions: 'Choose any number of Coffers to remove:',
+          minimum: 0,
+          maximum: player_cards.coffers
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, selected_cards[0])
+        turn_event_processor.process(Butcher.pay_tokens)
+      } else {
+        Butcher.pay_tokens(game, player_cards, '0')
+      }
     } else {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> does not trash anything`)
+      game.log.push(`&nbsp;&nbsp;but does not trash anything`)
     }
   }
 
-  static pay_tokens(game, player_cards, amount) {
+  static pay_tokens(game, player_cards, amount, trashed_card) {
     amount = Number(amount)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> pays ${amount} coin tokens`)
-    player_cards.coin_tokens -= amount
+    let coffer_text = amount === 1 ? 'Coffer' : 'Coffers'
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> removes ${amount} ${coffer_text}`)
+    player_cards.coffers -= amount
 
     let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_less_than(game, game.turn.butcher_card, card.top_card, amount + 1)
+      return card.count > 0 && card.supply && CardCostComparer.card_less_than(game, trashed_card, card.top_card, amount + 1)
     })
 
-    if (_.size(eligible_cards) > 0) {
+    if (_.size(eligible_cards) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -93,6 +84,8 @@ Butcher = class Butcher extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Butcher.gain_card)
+    } else if (_.size(eligible_cards) === 1) {
+      Butcher.gain_card(game, player_cards, eligible_cards)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
     }
