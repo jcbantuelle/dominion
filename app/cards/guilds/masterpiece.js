ab60519de4b3a16e29c0b414a676407eba6da492
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 11:32:18 -0500

    Refactor Masterpiece

--- app/cards/guilds/masterpiece.js
+++ app/cards/guilds/masterpiece.js
@@ -9,7 +9,8 @@ Masterpiece = class Masterpiece extends Card {
   }
 
   play(game, player_cards) {
-    CoinGainer.gain(game, player_cards, 1)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(1, false)
   }
 
   buy_event(buyer) {
