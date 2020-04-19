d6262b38f278155d106e4bb595bf87e86b006d03
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 09:40:48 -0500

    Refactor Rogue

--- app/cards/dark_ages/rogue.js
+++ app/cards/dark_ages/rogue.js
@@ -9,8 +9,8 @@ Rogue = class Rogue extends Card {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 2)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2)
 
     let eligible_cards = _.filter(game.trash, function(card) {
       return CardCostComparer.coin_between(game, card, 3, 6)
@@ -32,47 +32,38 @@ Rogue = class Rogue extends Card {
       turn_event_processor.process(Rogue.gain_from_trash)
     } else if (_.size(eligible_cards) === 1) {
       Rogue.gain_from_trash(game, player_cards, eligible_cards)
+    } else {
+      game.turn.rogue_attack = true
     }
 
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
 
-    delete game.turn.rogue_gained
+    delete game.turn.rogue_attack
   }
 
   static gain_from_trash(game, player_cards, selected_cards) {
-    game.turn.rogue_gained = true
     let card_gainer = new CardGainer(game, player_cards, 'discard', selected_cards[0].name)
     card_gainer.gain(game.trash)
   }
 
   attack(game, player_cards) {
-    if (!game.turn.rogue_gained) {
+    if (game.turn.rogue_attack) {
       if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
         game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards in deck`)
       } else {
-        player_cards.revealed = _.take(player_cards.deck, 2)
-        player_cards.deck = _.drop(player_cards.deck, 2)
+        let card_revealer = new CardRevealer(game, player_cards)
+        card_revealer.reveal_from_deck(2)
 
-        let revealed_card_count = _.size(player_cards.revealed)
-        if (revealed_card_count < 2 && _.size(player_cards.discard) > 0) {
-          let deck_shuffler = new DeckShuffler(game, player_cards)
-          deck_shuffler.shuffle()
-          player_cards.revealed = player_cards.revealed.concat(_.take(player_cards.deck, 2 - revealed_card_count))
-          player_cards.deck = _.drop(player_cards.deck, 2 - revealed_card_count)
-        }
-
-        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(player_cards.revealed)}`)
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
 
         let eligible_cards = _.filter(player_cards.revealed, function(card) {
           return CardCostComparer.coin_between(game, card, 3, 6)
         })
-
-        if (_.isEmpty(eligible_cards)) {
-          game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards costing between $3 and $6`)
-        } else if (_.size(eligible_cards) === 1) {
+        if (_.size(eligible_cards) === 1) {
           Rogue.trash_revealed(game, player_cards, eligible_cards)
-        } else {
+        } else if (_.size(eligible_cards) > 1) {
           let turn_event_id = TurnEventModel.insert({
             game_id: game._id,
             player_id: player_cards.player_id,
@@ -87,7 +78,6 @@ Rogue = class Rogue extends Card {
           let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
           turn_event_processor.process(Rogue.trash_revealed)
         }
-
         let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
         card_discarder.discard()
       }
