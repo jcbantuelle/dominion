7659a16e7ab1036c461c3367be15a860d2317880
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 08:20:30 -0500

    Make Wishing Well Optional

--- app/cards/intrigue/wishing_well.js
+++ app/cards/intrigue/wishing_well.js
@@ -26,9 +26,9 @@ WishingWell = class WishingWell extends Card {
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Name a card:',
+        instructions: 'Name a card: (or none to skip)',
         cards: unique_cards,
-        minimum: 1,
+        minimum: 0,
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
@@ -39,18 +39,22 @@ WishingWell = class WishingWell extends Card {
   }
 
   static name_card(game, player_cards, selected_cards) {
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> names ${CardView.render(selected_cards[0])}`)
+    if (!_isEmpty(selected_cards)) {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> names ${CardView.render(selected_cards[0])}`)
 
-    let card_revealer = new CardRevealer(game, player_cards)
-    card_revealer.reveal_from_deck(1)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal_from_deck(1)
 
-    if (player_cards.revealed[0].name === selected_cards[0].name) {
-      let card_mover = new CardMover(game, player_cards)
-      card_mover.move(player_cards.revealed, player_cards.hand, player_cards.revealed[0])
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(selected_cards[0])} in hand`)
+      if (player_cards.revealed[0].name === selected_cards[0].name) {
+        let card_mover = new CardMover(game, player_cards)
+        card_mover.move(player_cards.revealed, player_cards.hand, player_cards.revealed[0])
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(selected_cards[0])} in hand`)
+      } else {
+        let card_returner = new CardReturner(game, player_cards)
+        card_returner.return_to_deck(player_cards.revealed)
+      }
     } else {
-      let card_returner = new CardReturner(game, player_cards)
-      card_returner.return_to_deck(player_cards.revealed)
+      game.log.push(`&nbsp;&nbsp;but chooses not to name a card`)
     }
   }
 
