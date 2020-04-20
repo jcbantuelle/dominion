73b9ba51f5a9e4f87ee660a51455792dd8ed6cf4
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 17:13:19 -0500

    Refactor Duplicate

--- app/cards/adventures/duplicate.js
+++ app/cards/adventures/duplicate.js
@@ -1,4 +1,4 @@
-Duplicate = class Duplicate extends Card {
+Duplicate = class Duplicate extends Reserve {
 
   types() {
     return ['action', 'reserve']
@@ -8,18 +8,17 @@ Duplicate = class Duplicate extends Card {
     return 4
   }
 
-  play(game, player_cards, player) {
-    this.move_to_tavern(game, player_cards, player.played_card)
+  play(game, player_cards, card_player) {
+    Reserve.move_to_tavern(game, player_cards, card_player.card)
   }
 
   gain_event(gainer, duplicate) {
-    let gained_card = ClassCreator.create(gainer.card_name)
     let turn_event_id = TurnEventModel.insert({
       game_id: gainer.game._id,
       player_id: gainer.player_cards.player_id,
       username: gainer.player_cards.username,
       type: 'choose_yes_no',
-      instructions: `Call ${CardView.render(duplicate)} to gain a copy of ${CardView.render(gained_card)}?`,
+      instructions: `Call ${CardView.render(duplicate)} to gain a copy of ${CardView.render(gainer.gained_card)}?`,
       minimum: 1,
       maximum: 1
     })
@@ -29,14 +28,9 @@ Duplicate = class Duplicate extends Card {
 
   static gain_copy(game, player_cards, response, params) {
     if (response === 'yes') {
-      let reserve_index = _.findIndex(player_cards.tavern, function(card) {
-        return card.id === params.duplicate.id
-      })
-      let reserve = player_cards.tavern.splice(reserve_index, 1)[0]
-      game.log.push(`<strong>${player_cards.username}</strong> calls ${CardView.render(reserve)}`)
-      player_cards.in_play.push(reserve)
+      Reserve.call_from_tavern(game, player_cards, params.duplicate)
 
-      let card_gainer = new CardGainer(game, player_cards, 'discard', params.gainer.card_name)
+      let card_gainer = new CardGainer(game, player_cards, 'discard', params.gainer.gained_card.name)
       card_gainer.gain()
     }
   }
