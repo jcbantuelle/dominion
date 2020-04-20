d9681d0797a30491c7f967e82ef333cf56361b68
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 19:17:44 -0500

    Add Sauna Functionality To Silver

--- app/cards/common/silver.js
+++ app/cards/common/silver.js
@@ -23,33 +23,38 @@ Silver = class Silver extends Card {
     }
     game.turn.played_silver = true
 
-    // let sauna_count = _.size(_.filter(player_cards.in_play.concat(player_cards.duration).concat(player_cards.permanent), function(card) {
-    //   return card.name === 'Sauna'
-    // }))
-    // if (sauna_count > 0 && _.size(player_cards.hand) > 0) {
-    //   let turn_event_id = TurnEventModel.insert({
-    //     game_id: game._id,
-    //     player_id: player_cards.player_id,
-    //     username: player_cards.username,
-    //     type: 'choose_cards',
-    //     player_cards: true,
-    //     instructions: `Choose up to ${sauna_count} card(s) to trash:`,
-    //     cards: player_cards.hand,
-    //     minimum: 0,
-    //     maximum: sauna_count
-    //   })
-    //   let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-    //   turn_event_processor.process(Silver.trash_cards)
-    // }
+    let sauna_count = _.size(_.filter(player_cards.in_play, (card) => {
+      return card.name === 'Sauna'
+    }))
+    if (sauna_count > 0) {
+      if (_.size(player_cards.hand) > 0) {
+        let card_text = sauna_count === 1 ? 'card' : 'cards'
+        let turn_event_id = TurnEventModel.insert({
+          game_id: game._id,
+          player_id: player_cards.player_id,
+          username: player_cards.username,
+          type: 'choose_cards',
+          player_cards: true,
+          instructions: `Choose up to ${sauna_count} ${card_text} to trash:`,
+          cards: player_cards.hand,
+          minimum: 0,
+          maximum: sauna_count
+        })
+        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+        turn_event_processor.process(Silver.trash_cards)
+      } else {
+        game.log.push(`&nbsp;&nbsp;but has no cards in hand`)
+      }
+    }
   }
 
-  // static trash_cards(game, player_cards, selected_cards) {
-  //   if (_.size(selected_cards) === 0) {
-  //     game.log.push(`&nbsp;&nbsp;but does not trash anything`)
-  //   } else {
-  //     let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
-  //     card_trasher.trash()
-  //   }
-  // }
+  static trash_cards(game, player_cards, selected_cards) {
+    if (_.size(selected_cards) === 0) {
+      game.log.push(`&nbsp;&nbsp;but does not trash anything`)
+    } else {
+      let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
+      card_trasher.trash()
+    }
+  }
 
 }
