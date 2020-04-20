9817491e83d39473b07873b30d66db6e0683c306
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-19 19:57:29 -0500

    Refactor Governor

--- app/cards/promo/governor.js
+++ app/cards/promo/governor.js
@@ -9,9 +9,10 @@ Governor = class Governor extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
+    GameModel.update(game._id, game)
     let turn_event_id = TurnEventModel.insert({
       game_id: game._id,
       player_id: player_cards.player_id,
@@ -33,20 +34,17 @@ Governor = class Governor extends Card {
   static process_response(game, player_cards, response) {
     let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(game, player_cards)
 
-    response = response[0]
     _.each(ordered_player_cards, function(current_player_cards) {
       let is_active_player = current_player_cards.player_id === player_cards.player_id
-      if (response === 'draw') {
+      if (response[0] === 'draw') {
         let card_number = is_active_player ? 3 : 1
-
         let card_drawer = new CardDrawer(game, current_player_cards)
         card_drawer.draw(card_number)
-      } else if (response === 'gold') {
+      } else if (response[0] === 'gold') {
         let treasure = is_active_player ? 'Gold' : 'Silver'
-
         let card_gainer = new CardGainer(game, current_player_cards, 'discard', treasure)
         card_gainer.gain()
-      } else if (response === 'trash') {
+      } else if (response[0] === 'trash') {
         if (_.size(current_player_cards.hand) > 0) {
           let turn_event_id = TurnEventModel.insert({
             game_id: game._id,
@@ -62,10 +60,10 @@ Governor = class Governor extends Card {
           let turn_event_processor = new TurnEventProcessor(game, current_player_cards, turn_event_id)
           turn_event_processor.process(Governor.trash_card)
         } else {
-          game.log.push(`&nbsp;&nbsp;<strong>${current_player_cards.username}</strong> has no cards in thier hand`)
+          game.log.push(`&nbsp;&nbsp;<strong>${current_player_cards.username}</strong> has no cards in their hand`)
         }
       }
-
+      GameModel.update(game._id, game)
       PlayerCardsModel.update(game._id, current_player_cards)
     })
   }
@@ -81,10 +79,10 @@ Governor = class Governor extends Card {
       card_trasher.trash()
 
       let eligible_cards = _.filter(game.cards, function(card) {
-        return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_equal_to(game, selected_card, card.top_card, remodel_amount)
+        return card.count > 0 && card.supply && CardCostComparer.card_equal_to(game, selected_card, card.top_card, remodel_amount)
       })
 
-      if (_.size(eligible_cards) > 0) {
+      if (_.size(eligible_cards) > 1) {
         let turn_event_id = TurnEventModel.insert({
           game_id: game._id,
           player_id: player_cards.player_id,
@@ -98,6 +96,8 @@ Governor = class Governor extends Card {
         })
         let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
         turn_event_processor.process(Governor.gain_card)
+      } else if (_.size(eligible_cards) === 1) {
+        Governor.gain_card(game, player_cards, eligible_cards)
       } else {
         game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
       }
