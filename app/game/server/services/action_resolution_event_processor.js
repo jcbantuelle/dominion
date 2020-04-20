e6c279056ee34ab5692d195ef452ae8c6da23bd8
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 08:10:11 -0500

    Refactor ActionResolutionEventProcessor

--- app/game/server/services/action_resolution_event_processor.js
+++ app/game/server/services/action_resolution_event_processor.js
@@ -8,24 +8,27 @@ ActionResolutionEventProcessor = class ActionResolutionEventProcessor {
     this.game = game
     this.player_cards = player_cards
     this.resolved_action = resolved_action
-    this.find_action_resolution_events()
+    this.action_resolution_events = this.find_action_resolution_events()
   }
 
   find_action_resolution_events() {
-    let reserve_events = _.filter(this.player_cards.tavern, (card) => {
-      if (_.includes(ActionResolutionEventProcessor.reserve_events(), card.name)) {
-        if (card.name === 'Royal Carriage') {
-          let still_in_play = _.findIndex(this.player_cards.playing, (playing_card) => {
-            return playing_card.id === this.resolved_action.id
+    reserve_events = []
+    _.each(this.player_cards.tavern, (tavern_card) => {
+      if (_.includes(ActionResolutionEventProcessor.reserve_events(), tavern_card.name)) {
+        if (tavern_card.name === 'Royal Carriage') {
+          let resolved_action = _.find(this.player_cards.in_play, (in_play_card) => {
+            return in_play_card.id === this.resolved_action.id
           })
-          return still_in_play !== -1
+          if (resolved_action) {
+            reserve_events.push(tavern_card)
+          }
         } else {
-          return true
+          reserve_events.push(tavern_card)
         }
       }
     })
 
-    this.action_resolution_events = reserve_events
+    return reserve_events
   }
 
   process() {
@@ -39,26 +42,22 @@ ActionResolutionEventProcessor = class ActionResolutionEventProcessor {
           instructions: 'Choose order to resolve action events (leftmost will be first):',
           cards: this.action_resolution_events
         })
-        let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, {events: this.action_resolution_events, resolved_action: this.resolved_action})
+        let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, this.resolved_action)
         turn_event_processor.process(ActionResolutionEventProcessor.event_order)
       } else {
-        ActionResolutionEventProcessor.event_order(this.game, this.player_cards, _.map(this.action_resolution_events, 'name'), {events: this.action_resolution_events, resolved_action: this.resolved_action})
+        ActionResolutionEventProcessor.event_order(this.game, this.player_cards, this.action_resolution_events, this.resolved_action)
       }
     }
   }
 
-  static event_order(game, player_cards, ordered_events, params) {
+  static event_order(game, player_cards, ordered_events, resolved_action) {
     _.each(ordered_events, function(ordered_event) {
-      let tavern_index = _.findIndex(player_cards.tavern, function(card) {
+      let card = _.find(player_cards.tavern, function(card) {
         return card.id === ordered_event.id
       })
-      if (tavern_index !== -1) {
-        let event_index = _.findIndex(params.events, function(event) {
-          return event.id === ordered_event.id
-        })
-        let event = params.events.splice(event_index, 1)[0]
-        let selected_event = ClassCreator.create(ordered_event.name)
-        selected_event.action_resolution_event(game, player_cards, params.resolved_action, event)
+      if (card) {
+        let card_object = ClassCreator.create(card.name)
+        card_object.action_resolution_event(game, player_cards, card, resolved_action)
         GameModel.update(game._id, game)
         PlayerCardsModel.update(game._id, player_cards)
       }
