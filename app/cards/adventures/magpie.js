3fbf398c6f9ee6ee4b353cc5c83e0a754b3bb742
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 17:41:14 -0500

    Refactor Magpie

--- app/cards/adventures/magpie.js
+++ app/cards/adventures/magpie.js
@@ -12,33 +12,28 @@ Magpie = class Magpie extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 action`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(1)
 
     if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards in deck`)
     } else {
-      if (_.size(player_cards.deck) === 0) {
-        let deck_shuffler = new DeckShuffler(game, player_cards)
-        deck_shuffler.shuffle()
-      }
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal_from_deck(1)
 
-      let revealed_card = player_cards.deck.shift()
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(revealed_card)}`)
-
-      let card_types = _.words(revealed_card.types)
+      let card_types = _.words(player_cards.revealed[0].types)
+      let card_mover = new CardMover(game, player_cards)
       if (_.includes(card_types, 'treasure')) {
-        player_cards.hand.push(revealed_card)
-        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(revealed_card)} in their hand`)
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(player_cards.revealed)} in their hand`)
+        card_mover.move_all(player_cards.revealed, player_cards.hand)
       } else {
-        player_cards.deck.unshift(revealed_card)
+        card_mover.move_all(player_cards.revealed, player_cards.deck)
       }
 
       if (_.includes(card_types, 'action') || _.includes(card_types, 'victory')) {
         let card_gainer = new CardGainer(game, player_cards, 'discard', 'Magpie')
         card_gainer.gain()
       }
-
     }
   }
 
