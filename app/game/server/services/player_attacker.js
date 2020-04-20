0f435d6c64ae5c9ed6414c04ce90681ae2999d02
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 09:08:25 -0500

    Allow Arbitrary Params Passed to Player Attacker

--- app/game/server/services/player_attacker.js
+++ app/game/server/services/player_attacker.js
@@ -6,7 +6,7 @@ PlayerAttacker = class PlayerAttacker {
     this.card_player = card_player
   }
 
-  attack(attacker_player_cards) {
+  attack(attacker_player_cards, params) {
     let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(this.game)
     ordered_player_cards.shift()
 
@@ -16,11 +16,11 @@ PlayerAttacker = class PlayerAttacker {
       let attack_event_processor = new AttackEventProcessor(this.game, attacked_player_cards)
       attack_event_processor.process()
 
-      if (attacked_player_cards.moat || attacked_player_cards.champion || this.lighthouse_in_play(attacked_player_cards)) {
+      if (attacked_player_cards.moat || attacked_player_cards.champions > 0 || this.lighthouse_in_play(attacked_player_cards)) {
         delete attacked_player_cards.moat
         this.game.log.push(`&nbsp;&nbsp;<strong>${attacked_player_cards.username}</strong> is immune to the attack`)
       } else {
-        this.card.attack(this.game, attacked_player_cards, attacker_player_cards, this.card_player)
+        this.card.attack(this.game, attacked_player_cards, attacker_player_cards, this.card_player, params)
       }
       GameModel.update(this.game._id, this.game)
       PlayerCardsModel.update(this.game._id, attacked_player_cards)
