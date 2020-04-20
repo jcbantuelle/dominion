84131bc7ef5620e2db65651481aeae4a4fd8508a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 09:09:12 -0500

    Refactor Warrior

--- app/cards/adventures/warrior.js
+++ app/cards/adventures/warrior.js
@@ -12,33 +12,32 @@ Warrior = class Warrior extends Traveller {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(2)
 
-    game.turn.traveller_count = _.size(_.filter(player_cards.in_play.concat(player_cards.playing), function(card) {
+    let traveller_count = _.size(_.filter(player_cards.in_play, function(card) {
       return _.includes(_.words(card.types), 'traveller')
     }))
 
     let player_attacker = new PlayerAttacker(game, this)
-    player_attacker.attack(player_cards)
-
-    delete game.turn.traveller_count
+    player_attacker.attack(player_cards, traveller_count)
   }
 
-  attack(game, player_cards) {
-    _.times(game.turn.traveller_count, function() {
+  attack(game, player_cards, attacker_player_cards, card_player, traveller_count) {
+    _.times(traveller_count, function() {
       if (_.size(player_cards.deck) > 0 || _.size(player_cards.discard) > 0) {
         if (_.size(player_cards.deck) === 0) {
           let deck_shuffler = new DeckShuffler(game, player_cards)
           deck_shuffler.shuffle()
         }
-        let revealed_card = player_cards.deck.shift()
-        player_cards.revealed.push(revealed_card)
+        let discarded_card = player_cards.deck[0]
+        let card_discarder = new CardDiscarder(game, player_cards, 'deck', discarded_card)
+        card_discarder.discard()
 
-        if (CardCostComparer.coin_between(game, revealed_card, 3, 4)) {
-          let card_trasher = new CardTrasher(game, player_cards, 'revealed', revealed_card)
+        if (CardCostComparer.coin_between(game, discarded_card, 3, 4)) {
+          let card_trasher = new CardTrasher(game, player_cards, 'discard', discarded_card)
           card_trasher.trash()
-        } else {
-          let card_discarder = new CardDiscarder(game, player_cards, 'revealed', revealed_card)
-          card_discarder.discard()
         }
+      } else {
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> has no cards left in their deck`)
+        return false
       }
     })
   }
