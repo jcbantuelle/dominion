078511d5ea88021b2beb764e22b9a0ad3aff90d4
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 08:10:47 -0500

    Refactor Coin of the Realm

--- app/cards/adventures/coin_of_the_realm.js
+++ app/cards/adventures/coin_of_the_realm.js
@@ -8,35 +8,31 @@ CoinOfTheRealm = class CoinOfTheRealm extends Card {
     return 2
   }
 
-  play(game, player_cards) {
-    CoinGainer.gain(game, player_cards, 1)
-    this.move_to_tavern(game, player_cards, 'Coin Of The Realm')
+  play(game, player_cards, card_player) {
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(1, false)
+    Card.move_to_tavern(game, player_cards, card_player.card)
   }
 
-  action_resolution_event(game, player_cards) {
+  action_resolution_event(game, player_cards, coin_of_the_realm) {
     let turn_event_id = TurnEventModel.insert({
       game_id: game._id,
       player_id: player_cards.player_id,
       username: player_cards.username,
       type: 'choose_yes_no',
-      instructions: `Call ${CardView.render(this)}?`,
+      instructions: `Call ${CardView.render(coin_of_the_realm)}?`,
       minimum: 1,
       maximum: 1
     })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, coin_of_the_realm)
     turn_event_processor.process(CoinOfTheRealm.call_coin)
   }
 
-  static call_coin(game, player_cards, response) {
+  static call_coin(game, player_cards, response, coin_of_the_realm) {
     if (response === 'yes') {
-      let coin_index = _.findIndex(player_cards.tavern, function(card) {
-        return card.name === 'Coin Of The Realm'
-      })
-      coin = player_cards.tavern.splice(coin_index, 1)[0]
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> calls ${CardView.render(coin)}`)
-      player_cards.in_play.push(coin)
-      game.turn.actions += 2
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+      Card.call_from_tavern(game, player_cards, coin_of_the_realm)
+      let action_gainer = new ActionGainer(game, player_cards)
+      action_gainer.gain(2)
     }
   }
 
