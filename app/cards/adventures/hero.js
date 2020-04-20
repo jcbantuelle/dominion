7f15c0f18603f9a35468202bfb7ddc04055cc14f
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 09:09:32 -0500

    Refactor Hero

--- app/cards/adventures/hero.js
+++ app/cards/adventures/hero.js
@@ -9,14 +9,14 @@ Hero = class Hero extends Traveller {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 2)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2)
 
     let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && _.includes(_.words(card.top_card.types), 'treasure')
+      return card.count > 0 && card.supply && _.includes(_.words(card.top_card.types), 'treasure')
     })
 
-    if (_.size(eligible_cards) > 0) {
+    if (_.size(eligible_cards) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -30,6 +30,8 @@ Hero = class Hero extends Traveller {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Hero.gain_card)
+    } else if (_.size(eligible_cards) === 1) {
+      Hero.gain_card(game, player_cards, eligible_cards)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no available treasures to gain`)
     }
