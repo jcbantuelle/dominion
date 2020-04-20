07cb603c868542480a0d794246572f203e150232
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 18:15:39 -0500

    Refactor Messenger

--- app/cards/adventures/messenger.js
+++ app/cards/adventures/messenger.js
@@ -9,11 +9,14 @@ Messenger = class Messenger extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.buys += 1
-    let gained_coins = CoinGainer.gain(game, player_cards, 2)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy and +$${gained_coins}`)
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
+
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2)
 
     if (_.size(player_cards.deck) > 0) {
+      GameModel.update(game._id, game)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -33,18 +36,18 @@ Messenger = class Messenger extends Card {
 
   static discard_deck(game, player_cards, response) {
     if (response === 'yes') {
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move_all(player_cards.deck, player_cards.discard)
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts their deck into their discard pile`)
-      player_cards.discard = player_cards.discard.concat(player_cards.deck)
-      player_cards.deck = []
     }
   }
 
   buy_event(buyer) {
     let eligible_cards = _.filter(buyer.game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.coin_less_than(buyer.game, card.top_card, 5)
+      return card.count > 0 && card.supply && CardCostComparer.coin_less_than(buyer.game, card.top_card, 5)
     })
 
-    if (_.size(eligible_cards) > 0) {
+    if (_.size(eligible_cards) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: buyer.game._id,
         player_id: buyer.player_cards.player_id,
@@ -58,6 +61,8 @@ Messenger = class Messenger extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(buyer.game, buyer.player_cards, turn_event_id)
       turn_event_processor.process(Messenger.gain_card)
+    } else if (_.size(eligible_cards) === 1) {
+      Messenger.gain_card(buyer.game, buyer.player_cards, eligible_cards)
     } else {
       buyer.game.log.push(`&nbsp;&nbsp;but there are no available cards to gain`)
     }
