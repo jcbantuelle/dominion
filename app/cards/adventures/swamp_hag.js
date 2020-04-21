07d344515db7e00365b368fba00c2bae80c81156
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 20:13:03 -0500

    Refactor Swamp Hag

--- app/cards/adventures/swamp_hag.js
+++ app/cards/adventures/swamp_hag.js
@@ -1,4 +1,4 @@
-SwampHag = class SwampHag extends Card {
+SwampHag = class SwampHag extends Duration {
 
   types() {
     return ['action', 'duration', 'attack']
@@ -8,24 +8,23 @@ SwampHag = class SwampHag extends Card {
     return 5
   }
 
-  play(game, player_cards) {
-    player_cards.duration_effects.push(this.to_h())
-
-    let player_attacker = new PlayerAttacker(game, this)
+  play(game, player_cards, card_player) {
+    let player_attacker = new PlayerAttacker(game, this, card_player)
     player_attacker.attack(player_cards)
 
+    player_cards.duration_effects.push(_.clone(card_player.card))
     return 'duration'
   }
 
-  attack(game, player_cards) {
-    let attack_card = this.to_h()
+  attack(game, player_cards, attacker_player_cards, card_player) {
+    let attack_card = _.clone(card_player.card)
     attack_card.player_source = game.turn.player
     player_cards.duration_attacks.push(attack_card)
   }
 
   duration(game, player_cards, duration_card) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 3)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins} from ${CardView.render(this)}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(3)
   }
 
   buy_event(buyer) {
