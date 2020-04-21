c332556f518944366777f81c8db2f25a7551323d
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 20:35:19 -0500

    Refactor Wine Merchant

--- app/cards/adventures/wine_merchant.js
+++ app/cards/adventures/wine_merchant.js
@@ -1,4 +1,4 @@
-WineMerchant = class WineMerchant extends Card {
+WineMerchant = class WineMerchant extends Reserve {
 
   types() {
     return ['action', 'reserve']
@@ -8,30 +8,33 @@ WineMerchant = class WineMerchant extends Card {
     return 5
   }
 
-  play(game, player_cards, player) {
-    game.turn.buys += 1
-    let gained_coins = CoinGainer.gain(game, player_cards, 4)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy and +$${gained_coins}`)
-    this.move_to_tavern(game, player_cards, player.played_card)
+  play(game, player_cards, card_player) {
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
+
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(4)
+
+    Reserve.move_to_tavern(game, player_cards, card_player.card)
   }
 
-  end_buy_event(game, player_cards, card) {
+  end_buy_event(game, player_cards, wine_merchant) {
     let turn_event_id = TurnEventModel.insert({
       game_id: game._id,
       player_id: player_cards.player_id,
       username: player_cards.username,
       type: 'choose_yes_no',
-      instructions: `Discard ${CardView.render(card)}?`,
+      instructions: `Discard ${CardView.render(wine_merchant)}?`,
       minimum: 1,
       maximum: 1
     })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, card)
+    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, wine_merchant)
     turn_event_processor.process(WineMerchant.discard_card)
   }
 
-  static discard_card(game, player_cards, response, card) {
+  static discard_card(game, player_cards, response, wine_merchant) {
     if (response === 'yes') {
-      let card_discarder = new CardDiscarder(game, player_cards, 'tavern', card)
+      let card_discarder = new CardDiscarder(game, player_cards, 'tavern', wine_merchant)
       card_discarder.discard()
     }
   }
