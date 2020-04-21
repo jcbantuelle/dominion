471c4975db2def3231b0f5c64156e1c88a1c0867
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 19:25:38 -0500

    Refactor Relic

--- app/cards/adventures/relic.js
+++ app/cards/adventures/relic.js
@@ -9,7 +9,8 @@ Relic = class Relic extends Card {
   }
 
   play(game, player_cards) {
-    CoinGainer.gain(game, player_cards, 2)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(2, false)
 
     let player_attacker = new PlayerAttacker(game, this)
     player_attacker.attack(player_cards)
