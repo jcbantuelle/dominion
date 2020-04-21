3f72fc8a727e9daeb5bbf3af734c16209fe361f4
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 20:28:04 -0500

    Refactor EndBuyEventProcessor

--- app/game/server/services/end_buy_event_processor.js
+++ app/game/server/services/end_buy_event_processor.js
@@ -7,21 +7,25 @@ EndBuyEventProcessor = class EndBuyEventProcessor {
   constructor(game, player_cards) {
     this.game = game
     this.player_cards = player_cards
-    this.find_end_buy_events()
+    this.end_buy_events = this.find_end_buy_events()
   }
 
   find_end_buy_events() {
-    let reserve_events = _.filter(this.player_cards.tavern, (card) => {
+    let events = []
+
+    _.each(this.player_cards.tavern, (card) => {
       if (_.includes(EndBuyEventProcessor.reserve_events(), card.name)) {
         if (card.name === 'Wine Merchant') {
-          return this.game.turn.coins >= 2
+          if (this.game.turn.coins >= 2) {
+            events.push(card)
+          }
         } else {
-          return true
+          events.push(card)
         }
       }
     })
 
-    this.end_buy_events = reserve_events
+    return events
   }
 
   process() {
@@ -36,22 +40,18 @@ EndBuyEventProcessor = class EndBuyEventProcessor {
           instructions: 'Choose order to resolve end of buy phase events (leftmost will be first):',
           cards: this.end_buy_events
         })
-        let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id, this.end_buy_events)
+        let turn_event_processor = new TurnEventProcessor(this.game, this.player_cards, turn_event_id)
         turn_event_processor.process(EndBuyEventProcessor.event_order)
       } else {
-        EndBuyEventProcessor.event_order(this.game, this.player_cards, _.map(this.end_buy_events, 'name'), this.end_buy_events)
+        EndBuyEventProcessor.event_order(this.game, this.player_cards, this.end_buy_events)
       }
     }
   }
 
-  static event_order(game, player_cards, ordered_events, events) {
-    _.each(ordered_events, function(ordered_event) {
-      let event_index = _.findIndex(events, function(event) {
-        return event.id === ordered_event.id
-      })
-      let event = events.splice(event_index, 1)[0]
-      let selected_event = ClassCreator.create(event.name)
-      selected_event.end_buy_event(game, player_cards, event)
+  static event_order(game, player_cards, ordered_events) {
+    _.each(ordered_events, function(event) {
+      let event_object = ClassCreator.create(event.name)
+      event_object.end_buy_event(game, player_cards, event)
       GameModel.update(game._id, game)
       PlayerCardsModel.update(game._id, player_cards)
     })
