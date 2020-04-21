1978c301db3d68d97e1bf74ec88ed501c01d422d
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-20 22:34:38 -0500

    Refactor Scouting Party

--- app/cards/adventures/events/scouting_party.js
+++ app/cards/adventures/events/scouting_party.js
@@ -5,24 +5,14 @@ ScoutingParty = class ScoutingParty extends Event {
   }
 
   buy(game, player_cards) {
-    game.turn.buys += 1
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 buy`)
+    let buy_gainer = new BuyGainer(game, player_cards)
+    buy_gainer.gain(1)
 
     if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
       game.log.push(`&nbsp;&nbsp;but has no cards in deck`)
     } else {
-      player_cards.revealed = _.take(player_cards.deck, 5)
-      player_cards.deck = _.drop(player_cards.deck, 5)
-
-      let revealed_card_count = _.size(player_cards.revealed)
-      if (revealed_card_count < 5 && _.size(player_cards.discard) > 0) {
-        let deck_shuffler = new DeckShuffler(game, player_cards)
-        deck_shuffler.shuffle()
-        player_cards.revealed = player_cards.revealed.concat(_.take(player_cards.deck, 5 - revealed_card_count))
-        player_cards.deck = _.drop(player_cards.deck, 5 - revealed_card_count)
-      }
-
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> looks at the top ${_.size(player_cards.revealed)} cards of their deck`)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal_from_deck(5, false)
 
       if (_.size(player_cards.revealed < 4)) {
         ScoutingParty.discard_cards(game, player_cards, player_cards.revealed)
@@ -48,32 +38,10 @@ ScoutingParty = class ScoutingParty extends Event {
     let card_discarder = new CardDiscarder(game, player_cards, 'revealed', selected_cards)
     card_discarder.discard()
 
-    if (_.size(player_cards.revealed) > 1) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'sort_cards',
-        instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
-        cards: player_cards.revealed
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(ScoutingParty.replace_cards)
-    } else if (_.size(player_cards.revealed) === 1) {
-      ScoutingParty.replace_cards(game, player_cards, _.map(player_cards.revealed, 'name'))
+    if (!_.isEmpty(player_cards.revealed)) {
+      let card_returner = new CardReturner(game, player_cards)
+      card_returner.return_to_deck(player_cards.revealed)
     }
-  }
-
-  static replace_cards(game, player_cards, ordered_cards) {
-    _.each(ordered_cards.reverse(), function(ordered_card) {
-      let revealed_card_index = _.findIndex(player_cards.revealed, function(card) {
-        return card.id === ordered_card.id
-      })
-      let revealed_card = player_cards.revealed.splice(revealed_card_index, 1)[0]
-      player_cards.deck.unshift(revealed_card)
-    })
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places the cards back on their deck`)
-    player_cards.revealed = []
   }
 
 }
