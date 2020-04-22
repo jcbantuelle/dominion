5545358cef43ef4e3707e49ee6c679eae447a0e0
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-21 22:11:27 -0500

    Refactor Bustling Village

--- app/cards/empires/bustling_village.js
+++ app/cards/empires/bustling_village.js
@@ -16,36 +16,40 @@ BustlingVillage = class BustlingVillage extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(1)
 
-    game.turn.actions += 3
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +3 actions`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(3)
 
-    let settlers_index = _.findIndex(player_cards.discard, function(card) {
-      return card.inherited_name === 'Settlers'
+    let settlers = _.find(player_cards.discard, function(card) {
+      return card.name === 'Settlers'
     })
-    if (settlers_index != -1) {
+    if (settlers) {
+      PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
         username: player_cards.username,
         type: 'choose_yes_no',
-        instructions: `Reveal ${CardView.render(player_cards.discard[settlers_index])}?`,
+        instructions: `Reveal ${CardView.render(settlers)}?`,
         minimum: 1,
         maximum: 1
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, settlers_index)
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, settlers)
       turn_event_processor.process(BustlingVillage.reveal_settlers)
     } else {
       game.log.push(`&nbsp;&nbsp;but does not reveal a ${CardView.render(new Settlers())}`)
     }
   }
 
-  static reveal_settlers(game, player_cards, response, settlers_index) {
+  static reveal_settlers(game, player_cards, response, settlers) {
     if (response === 'yes') {
-      settlers = player_cards.discard.splice(settlers_index, 1)[0]
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(settlers)}`)
-      player_cards.hand.push(settlers)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal(settlers)
+
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move(player_cards.discard, player_cards.hand, settlers)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts ${CardView.render(settlers)} in hand`)
     } else {
-      game.log.push(`&nbsp;&nbsp;but does not reveal a ${CardView.render(player_cards.discard[settlers_index])}`)
+      game.log.push(`&nbsp;&nbsp;but does not reveal a ${CardView.render(settlers)}`)
     }
   }
 
