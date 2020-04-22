b67f22f4187d946da56418f6b087ed13775825f4
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 12:40:03 -0500

    Refactor Haunted Castle

--- app/cards/empires/haunted_castle.js
+++ app/cards/empires/haunted_castle.js
@@ -9,20 +9,21 @@ HauntedCastle = class HauntedCastle extends Castles {
   }
 
   gain_event(gainer) {
-    if (gainer.game.turn.player._id === gainer.player_cards.player_id) {
-      let card_gainer = new CardGainer(gainer.game, gainer.player_cards, 'discard', 'Gold')
-      card_gainer.gain()
+    let card_gainer = new CardGainer(gainer.game, gainer.player_cards, 'discard', 'Gold')
+    card_gainer.gain()
 
-      let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(gainer.game, gainer.player_cards)
-      ordered_player_cards.shift()
-      _.each(ordered_player_cards, (other_player_cards) => {
-        this.attack(gainer.game, other_player_cards)
-        PlayerCardsModel.update(gainer.game._id, other_player_cards)
-      })
-    }
+    GameModel.update(gainer.game._id, gainer.game)
+    PlayerCardsModel.update(gainer.game._id, gainer.player_cards)
+
+    let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(gainer.game, gainer.player_cards)
+    ordered_player_cards.shift()
+    _.each(ordered_player_cards, (other_player_cards) => {
+      this.return_cards(gainer.game, other_player_cards)
+      PlayerCardsModel.update(gainer.game._id, other_player_cards)
+    })
   }
 
-  attack(game, player_cards) {
+  return_cards(game, player_cards) {
     if (_.size(player_cards.hand) >= 5) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
@@ -43,27 +44,8 @@ HauntedCastle = class HauntedCastle extends Castles {
   }
 
   static return_to_deck(game, player_cards, selected_cards) {
-    let turn_event_id = TurnEventModel.insert({
-      game_id: game._id,
-      player_id: player_cards.player_id,
-      username: player_cards.username,
-      type: 'sort_cards',
-      instructions: 'Choose order to place cards on deck: (leftmost will be top card)',
-      cards: selected_cards
-    })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-    turn_event_processor.process(HauntedCastle.replace_cards)
-  }
-
-  static replace_cards(game, player_cards, ordered_cards) {
-    _.each(ordered_cards.reverse(), function(ordered_card) {
-      let returned_card_index = _.findIndex(player_cards.hand, function(card) {
-        return card.id === ordered_card.id
-      })
-      let returned_card = player_cards.hand.splice(returned_card_index, 1)[0]
-      player_cards.deck.unshift(returned_card)
-    })
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> places 2 cards back on their deck`)
+    let card_returner = new CardReturner(game, player_cards)
+    card_returner.return_to_deck(player_cards.hand, selected_cards)
   }
 
 }
