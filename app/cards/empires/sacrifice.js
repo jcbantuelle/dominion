0a1bc9e436290d7df5c63963516b1ffc03acf641
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 15:36:46 -0500

    Refactor Sacrifice

--- app/cards/empires/sacrifice.js
+++ app/cards/empires/sacrifice.js
@@ -9,7 +9,7 @@ Sacrifice = class Sacrifice extends Card {
   }
 
   play(game, player_cards) {
-    if (_.size(player_cards.hand) > 0) {
+    if (_.size(player_cards.hand) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -23,6 +23,8 @@ Sacrifice = class Sacrifice extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(Sacrifice.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      Sacrifice.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
@@ -30,33 +32,27 @@ Sacrifice = class Sacrifice extends Card {
 
   static trash_card(game, player_cards, selected_cards) {
     let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
-    card_trasher.trash()
+    let trashed_card = card_trasher.trash()[0]
 
-    let trashed_card = selected_cards[0]
-    let selected_card_types = _.words(trashed_card.types)
+    if (trashed_card) {
+      let selected_card_types = _.words(trashed_card.types)
 
-    if (_.includes(selected_card_types, 'action')) {
-      let card_drawer = new CardDrawer(game, player_cards)
-      card_drawer.draw(2)
+      if (_.includes(selected_card_types, 'action')) {
+        let card_drawer = new CardDrawer(game, player_cards)
+        card_drawer.draw(2)
 
-      game.turn.actions += 2
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
-    }
+        let action_gainer = new ActionGainer(game, player_cards)
+        action_gainer.gain(2)
+      }
 
-    if (_.includes(selected_card_types, 'treasure')) {
-      let gained_coins = CoinGainer.gain(game, player_cards, 2)
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
-    }
+      if (_.includes(selected_card_types, 'treasure')) {
+        let coin_gainer = new CoinGainer(game, player_cards)
+        coin_gainer.gain(2)
+      }
 
-    if (_.includes(selected_card_types, 'victory')) {
-      if (game.turn.possessed) {
-        possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-        possessing_player_cards.victory_tokens += 2
-        game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +2 &nabla;`)
-        PlayerCardsModel.update(game._id, possessing_player_cards)
-      } else {
-        player_cards.victory_tokens += 2
-        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 &nabla;`)
+      if (_.includes(selected_card_types, 'victory')) {
+        let victory_token_gainer = new VictoryTokenGainer(game, player_cards)
+        victory_token_gainer.gain(2)
       }
     }
   }
