62e75e760146167963a7acf8a3597c3d8d317715
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 13:10:08 -0500

    Refactor Catapult

--- app/cards/empires/catapult.js
+++ app/cards/empires/catapult.js
@@ -13,10 +13,12 @@ Catapult = class Catapult extends Card {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 1)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(1)
 
-    if (_.size(player_cards.hand) > 0) {
+    let trashed_card
+
+    if (_.size(player_cards.hand) > 1) {
       PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
@@ -30,43 +32,46 @@ Catapult = class Catapult extends Card {
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Catapult.trash_card)
+      trashed_card = turn_event_processor.process(Catapult.trash_card)
+    } else if (_.size(player_cards.hand) === 1) {
+      trashed_card = Catapult.trash_card(game, player_cards, player_cards.hand)
     } else {
       game.log.push(`&nbsp;&nbsp;but there are no cards in hand`)
     }
 
     let player_attacker = new PlayerAttacker(game, this)
-    player_attacker.attack(player_cards)
-
-    delete game.turn.catapult_trash
+    player_attacker.attack(player_cards, trashed_card)
   }
 
   static trash_card(game, player_cards, selected_cards) {
     let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
-    card_trasher.trash()
+    let trashed_card = card_trasher.trash()
 
-    game.turn.catapult_trash = selected_cards[0]
+    return trashed_card[0]
   }
 
-  attack(game, player_cards) {
-    if (game.turn.catapult_trash) {
+  attack(game, player_cards, attacker_player_cards, card_player, trashed_card) {
+    if (trashed_card) {
 
-      if (CardCostComparer.coin_greater_than(game, game.turn.catapult_trash, 2)) {
+      if (CardCostComparer.coin_greater_than(game, trashed_card, 2)) {
         let card_gainer = new CardGainer(game, player_cards, 'discard', 'Curse')
         card_gainer.gain()
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
       }
 
-      if (_.includes(_.words(game.turn.catapult_trash.types), 'treasure')) {
+      if (_.includes(_.words(trashed_card.types), 'treasure')) {
         let number_to_discard = _.size(player_cards.hand) - 3
 
         if (number_to_discard > 0) {
+          let card_text = number_to_discard === 1 ? 'card' : 'cards'
           let turn_event_id = TurnEventModel.insert({
             game_id: game._id,
             player_id: player_cards.player_id,
             username: player_cards.username,
             type: 'choose_cards',
             player_cards: true,
-            instructions: `Choose ${number_to_discard} card(s) to discard from hand:`,
+            instructions: `Choose ${number_to_discard} ${card_text} to discard from hand:`,
             cards: player_cards.hand,
             minimum: number_to_discard,
             maximum: number_to_discard
