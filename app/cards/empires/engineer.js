ac552b33a0892fe25d6a1c901d515afce3779a82
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-21 19:01:03 -0500

    Refactor Engineer

--- app/cards/empires/engineer.js
+++ app/cards/empires/engineer.js
@@ -12,13 +12,12 @@ Engineer = class Engineer extends Card {
     return 4
   }
 
-  play(game, player_cards) {
+  play(game, player_cards, card_player) {
     Engineer.choose_gain_card(game, player_cards)
 
-    let engineer = _.find(player_cards.playing, function(card) {
-      return card.inherited_name === 'Engineer'
+    let engineer = _.find(player_cards.in_play, (card) => {
+      return card.id === card_player.card.id
     })
-
     if (engineer) {
       GameModel.update(game._id, game)
       PlayerCardsModel.update(game._id, player_cards)
@@ -33,12 +32,14 @@ Engineer = class Engineer extends Card {
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, engineer)
       turn_event_processor.process(Engineer.trash_card)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but ${card_player.card} is not in play`)
     }
   }
 
   static choose_gain_card(game, player_cards) {
     let eligible_cards = _.filter(game.cards, function(card) {
-      return card.count > 0 && card.top_card.purchasable && CardCostComparer.coin_less_than(game, card.top_card, 5)
+      return card.count > 0 && card.supply && CardCostComparer.coin_less_than(game, card.top_card, 5)
     })
 
     if (_.size(eligible_cards) > 0) {
@@ -67,10 +68,14 @@ Engineer = class Engineer extends Card {
 
   static trash_card(game, player_cards, response, engineer) {
     if (response === 'yes') {
-      let card_trasher = new CardTrasher(game, player_cards, 'playing', engineer)
+      let card_trasher = new CardTrasher(game, player_cards, 'in_play', engineer)
       card_trasher.trash()
 
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
       Engineer.choose_gain_card(game, player_cards)
+    } else {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> chooses not to trash ${CardView.render(engineer)}`)
     }
   }
 
