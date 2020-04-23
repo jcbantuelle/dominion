1c79a2128eb1dc75f852bfeb909a9e92b0642e4e
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 21:21:16 -0500

    Refactor Aqueduct

--- app/cards/empires/landmarks/aqueduct.js
+++ app/cards/empires/landmarks/aqueduct.js
@@ -1,6 +1,6 @@
 Aqueduct = class Aqueduct extends Landmark {
 
-  gain_event(gainer) {
+  gain_event(gainer, aqueduct) {
     if (_.includes(_.words(gainer.gained_card.types), 'treasure')) {
       let gained_card_stack = _.find(gainer.game.cards, (card) => {
         return card.stack_name === gainer.gained_card.stack_name
@@ -12,7 +12,7 @@ Aqueduct = class Aqueduct extends Landmark {
         if (aqueduct_stack) {
           aqueduct_stack.victory_tokens += 1
           gained_card_stack.victory_tokens -= 1
-          gainer.game.log.push(`&nbsp;&nbsp;<strong>${gainer.player_cards.username}</strong> moves +1 &nabla; from ${CardView.render(gainer.gained_card)} to ${CardView.render(this)}`)
+          gainer.game.log.push(`&nbsp;&nbsp;<strong>${gainer.player_cards.username}</strong> moves +1 &nabla; from ${CardView.render(gainer.gained_card)} to ${CardView.render(aqueduct)}`)
         }
       }
     }
@@ -22,11 +22,9 @@ Aqueduct = class Aqueduct extends Landmark {
         return card.name === 'Aqueduct'
       })
       if (aqueduct_stack && aqueduct_stack.victory_tokens > 0) {
-        let victory_tokens = aqueduct_stack.victory_tokens
+        let victory_token_gainer = new VictoryTokenGainer(gainer.game, gainer.player_cards, aqueduct)
+        victory_token_gainer.gain(aqueduct_stack.victory_tokens)
         aqueduct_stack.victory_tokens = 0
-
-        gainer.player_cards.victory_tokens += victory_tokens
-        gainer.game.log.push(`&nbsp;&nbsp;<strong>${gainer.player_cards.username}</strong> gets +${victory_tokens} &nabla; from ${CardView.render(this)}`)
       }
     }
   }
