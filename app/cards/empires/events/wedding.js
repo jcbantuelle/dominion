b16b110b682a145ba7026e4ebb70085d1d194223
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 20:57:07 -0500

    Refactor Wedding

--- app/cards/empires/events/wedding.js
+++ app/cards/empires/events/wedding.js
@@ -9,15 +9,8 @@ Wedding = class Wedding extends Event {
   }
 
   buy(game, player_cards) {
-    if (game.turn.possessed) {
-      possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-      possessing_player_cards.victory_tokens += 1
-      game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +1 &nabla;`)
-      PlayerCardsModel.update(game._id, possessing_player_cards)
-    } else {
-      player_cards.victory_tokens += 1
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +1 &nabla;`)
-    }
+    let victory_token_gainer = new VictoryTokenGainer(game, player_cards)
+    victory_token_gainer.gain(1)
 
     let card_gainer = new CardGainer(game, player_cards, 'discard', 'Gold')
     card_gainer.gain()
