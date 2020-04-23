050170edb203bd6551643a7ed423bff82a4c28ea
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 21:10:52 -0500

    Refactor Conquest

--- app/cards/empires/events/conquest.js
+++ app/cards/empires/events/conquest.js
@@ -13,18 +13,8 @@ Conquest = class Conquest extends Event {
     let gained_silvers = _.filter(game.turn.gained_cards, function(card) {
       return card.name === 'Silver'
     })
-
-    let victory_tokens = _.size(gained_silvers)
-
-    if (game.turn.possessed) {
-      possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-      possessing_player_cards.victory_tokens += victory_tokens
-      game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +${victory_tokens} &nabla;`)
-      PlayerCardsModel.update(game._id, possessing_player_cards)
-    } else {
-      player_cards.victory_tokens += victory_tokens
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +${victory_tokens} &nabla;`)
-    }
+    let victory_token_gainer = new VictoryTokenGainer(game, player_cards)
+    victory_token_gainer.gain(_.size(gained_silvers))
   }
 
 }
