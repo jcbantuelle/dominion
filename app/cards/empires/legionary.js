3c232a244b27cbf032686ec4428f310c2731373f
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-22 19:40:20 -0500

    Refactor Legionary

--- app/cards/empires/legionary.js
+++ app/cards/empires/legionary.js
@@ -9,11 +9,10 @@ Legionary = class Legionary extends Card {
   }
 
   play(game, player_cards) {
-    let gained_coins = CoinGainer.gain(game, player_cards, 3)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +$${gained_coins}`)
+    let coin_gainer = new CoinGainer(game, player_cards)
+    coin_gainer.gain(3)
 
-    game.turn.legionary_revealed_gold = false
-
+    let revealed_gold = false
     let gold = _.find(player_cards.hand, function(card) {
       return card.name === 'Gold'
     })
@@ -28,43 +27,46 @@ Legionary = class Legionary extends Card {
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, gold)
-      turn_event_processor.process(Legionary.reveal_gold)
+      revealed_gold = turn_event_processor.process(Legionary.reveal_gold)
+    } else {
+      game.log.push(`&nbsp;&nbsp;but does not reveal a ${CardView.render(new Gold())}`)
     }
 
-    let player_attacker = new PlayerAttacker(game, this)
-    player_attacker.attack(player_cards)
-
-    delete game.turn.legionary_revealed_gold
+    if (revealed_gold) {
+      let player_attacker = new PlayerAttacker(game, this)
+      player_attacker.attack(player_cards)
+    }
   }
 
   static reveal_gold(game, player_cards, response, gold) {
     if (response === 'yes') {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(gold)}`)
-      game.turn.legionary_revealed_gold = true
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal('hand', gold)
+      return true
+    } else {
+      game.log.push(`&nbsp;&nbsp;but does not reveal a ${CardView.render(gold)}`)
     }
   }
 
   attack(game, player_cards) {
-    if (game.turn.legionary_revealed_gold) {
-      let number_to_discard = _.size(player_cards.hand) - 2
+    let number_to_discard = _.size(player_cards.hand) - 2
 
-      if (number_to_discard > 0) {
-        let turn_event_id = TurnEventModel.insert({
-          game_id: game._id,
-          player_id: player_cards.player_id,
-          username: player_cards.username,
-          type: 'choose_cards',
-          player_cards: true,
-          instructions: `Choose ${number_to_discard} cards to discard from hand:`,
-          cards: player_cards.hand,
-          minimum: number_to_discard,
-          maximum: number_to_discard
-        })
-        let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-        turn_event_processor.process(Legionary.discard_from_hand)
-      } else {
-        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
-      }
+    if (number_to_discard > 0) {
+      let turn_event_id = TurnEventModel.insert({
+        game_id: game._id,
+        player_id: player_cards.player_id,
+        username: player_cards.username,
+        type: 'choose_cards',
+        player_cards: true,
+        instructions: `Choose ${number_to_discard} cards to discard from hand:`,
+        cards: player_cards.hand,
+        minimum: number_to_discard,
+        maximum: number_to_discard
+      })
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+      turn_event_processor.process(Legionary.discard_from_hand)
+    } else {
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> only has ${_.size(player_cards.hand)} cards in hand`)
     }
   }
 
