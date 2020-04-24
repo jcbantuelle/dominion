b5e5042de37fb6509b79295567f388e6ed78943f
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 17:43:53 -0500

    Refactor Zombie Mason

--- app/cards/nocturne/zombie_mason.js
+++ app/cards/nocturne/zombie_mason.js
@@ -17,26 +17,23 @@ ZombieMason = class ZombieMason extends Card {
         deck_shuffler.shuffle()
       }
 
-      let top_card = player_cards.deck.shift()
-      player_cards.revealed.push(top_card)
-
-      let card_trasher = new CardTrasher(game, player_cards, 'revealed', top_card)
-      card_trasher.trash()
-
-      GameModel.update(game._id, game)
+      let card_trasher = new CardTrasher(game, player_cards, 'deck', player_cards.deck[0])
+      let trashed_card = card_trasher.trash()[0]
 
       let eligible_cards = _.filter(game.cards, function(card) {
-        return card.count > 0 && card.top_card.purchasable && CardCostComparer.card_less_than(game, top_card, card.top_card, 2)
+        return card.count > 0 && card.supply && CardCostComparer.card_less_than(game, trashed_card, card.top_card, 2)
       })
 
       if (_.size(eligible_cards) > 0) {
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
         let turn_event_id = TurnEventModel.insert({
           game_id: game._id,
           player_id: player_cards.player_id,
           username: player_cards.username,
           type: 'choose_cards',
           game_cards: true,
-          instructions: `Choose a card to gain (Or none to skip):`,
+          instructions: `Choose a card to gain: (or none to skip)`,
           cards: eligible_cards,
           minimum: 0,
           maximum: 1
