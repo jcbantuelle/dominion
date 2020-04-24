e3fd79330b1edadb536d3c7b7d0b133616706e1a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 08:26:03 -0500

    Allow Duration Cards To Belong To Other Cards

--- app/cards/lib/duration.js
+++ app/cards/lib/duration.js
@@ -3,7 +3,7 @@ Duration = class Duration extends Card {
   stay_in_play(game, player_cards, card) {
     return _.find(player_cards.duration_effects, (duration_effect) => {
       return duration_effect.id === card.id
-    })
+    }) || super.stay_in_play(game, player_cards, card)
   }
 
 }
