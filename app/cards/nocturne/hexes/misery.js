a9bc81cc60a85201ad5040239c2d2b9ebf8f7379
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 22:01:18 -0500

    Refactor Misery

--- app/cards/nocturne/hexes/misery.js
+++ app/cards/nocturne/hexes/misery.js
@@ -1,28 +1,26 @@
 Misery = class Misery extends Hex {
 
   receive(game, player_cards) {
-    let miserable_index = _.findIndex(player_cards.states, function(state) {
+    let miserable_card = _.find(player_cards.states, function(state) {
       return _.includes(['Miserable', 'Twice Miserable'], state.name)
     })
-    if (miserable_index === -1) {
-      let miserable_index = _.findIndex(game.states, function(state) {
+    if (!miserable_card) {
+      let miserable = _.find(game.states, function(state) {
         return state.name === 'Miserable'
       })
-      let miserable = game.states.splice(miserable_index, 1)[0]
-      player_cards.states.push(miserable)
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move(game.states, player_cards.states, miserable)
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> takes ${CardView.render(miserable)}`)
-    } else if (player_cards.states[miserable_index].name === 'Miserable') {
-      let miserable = player_cards.states.splice(miserable_index, 1)[0]
-      game.states.push(miserable)
-
-      let twice_miserable_index = _.findIndex(game.states, function(state) {
+    } else if (miserable_card.name === 'Miserable') {
+      let twice_miserable = _.find(game.states, function(state) {
         return state.name === 'Twice Miserable'
       })
-      let twice_miserable = game.states.splice(twice_miserable_index, 1)[0]
-      player_cards.states.push(twice_miserable)
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(miserable)} and takes ${CardView.render(twice_miserable)}`)
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move(player_cards.states, game.states, miserable_card)
+      card_mover.move(game.states, player_cards.states, twice_miserable)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(miserable_card)} and takes ${CardView.render(twice_miserable)}`)
     } else {
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> already has ${CardView.render(player_cards.states[miserable_index])}`)
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> already has ${CardView.render(miserable_card)}`)
     }
   }
 }
