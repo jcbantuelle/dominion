8c2dd0cc55f3feae9f45755231cb5ae74d710ba6
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 22:07:30 -0500

    Refactor Deluded

--- app/cards/nocturne/states/deluded.js
+++ app/cards/nocturne/states/deluded.js
@@ -1,14 +1,11 @@
 Deluded = class Deluded extends State {
 
-  start_buy_event(game, player_cards) {
+  start_buy_event(game, player_cards, deluded) {
     game.turn.deluded = true
 
-    let deluded_index = _.findIndex(player_cards.states, function(state) {
-      return state.name === 'Deluded'
-    })
-    deluded = player_cards.states.splice(deluded_index, 1)[0]
-    game.states.push(deluded)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(this)}`)
+    let card_mover = new CardMover(game, player_cards)
+    card_mover.move(player_cards.states, game.states, deluded)
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(deluded)}`)
   }
 
 }
