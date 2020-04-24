0b9424d12556f4756f3eca7ee718875ae577488a
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 21:48:57 -0500

    Refactor Envious

--- app/cards/nocturne/states/envious.js
+++ app/cards/nocturne/states/envious.js
@@ -1,14 +1,11 @@
 Envious = class Envious extends State {
 
-  start_buy_event(game, player_cards) {
+  start_buy_event(game, player_cards, envious) {
     game.turn.envious = true
 
-    let envious_index = _.findIndex(player_cards.states, function(state) {
-      return state.name === 'Envious'
-    })
-    envious = player_cards.states.splice(envious_index, 1)[0]
-    game.states.push(envious)
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(this)}`)
+    let card_mover = new CardMover(game, player_cards)
+    card_mover.move(player_cards.states, game.states, envious)
+    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> returns ${CardView.render(envious)}`)
   }
 
 }
