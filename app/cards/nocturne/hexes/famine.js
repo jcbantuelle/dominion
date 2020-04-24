5e000db3401af928c5fec3923a2d85f069e2a932
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 22:06:14 -0500

    Refactor Famine

--- app/cards/nocturne/hexes/famine.js
+++ app/cards/nocturne/hexes/famine.js
@@ -4,33 +4,21 @@ Famine = class Famine extends Hex {
     if (_.size(player_cards.deck) === 0 && _.size(player_cards.discard) === 0) {
       game.log.push(`&nbsp;&nbsp;but has no cards in deck`)
     } else {
-      revealed_cards = _.take(player_cards.deck, 3)
-      player_cards.deck = _.drop(player_cards.deck, 3)
+      let card_revealer = new CardRevealer(game, player_cards)
+      card_revealer.reveal_from_deck(3)
 
-      let revealed_card_count = _.size(revealed_cards)
-      if (revealed_card_count < 3 && _.size(player_cards.discard) > 0) {
-        let deck_shuffler = new DeckShuffler(game, player_cards)
-        deck_shuffler.shuffle()
-        revealed_cards = revealed_cards.concat(_.take(player_cards.deck, 3 - revealed_card_count))
-        player_cards.deck = _.drop(player_cards.deck, 3 - revealed_card_count)
-      }
-
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(revealed_cards)}`)
-
-      let non_action_cards = []
-      _.each(revealed_cards, function(card) {
-        if (_.includes(_.words(card.types), 'action')) {
-          player_cards.revealed.push(card)
-        } else {
-          non_action_cards.push(card)
-        }
+      let actions = _.filter(player_cards.revealed, (card) => {
+        return _.includes(_.words(card.types), 'action')
       })
 
-      let card_discarder = new CardDiscarder(game, player_cards, 'revealed')
+      let card_discarder = new CardDiscarder(game, player_cards, 'revealed', actions)
       card_discarder.discard()
 
-      player_cards.deck = _.shuffle(player_cards.deck.concat(non_action_cards))
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> shuffles ${CardView.render(non_action_cards)} into their deck`)
+      if (_.size(player_cards.revealed) > 0) {
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> shuffles ${CardView.render(player_cards.revealed)} into their deck`)
+        let deck_shuffler = new DeckShuffler(game, player_cards)
+        deck_shuffler.shuffle('revealed')
+      }
     }
   }
 
