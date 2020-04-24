bb19cba7b05fe59d5132cfd4ecbdef41d8c6766e
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 16:55:42 -0500

    Refactor Imp

--- app/cards/nocturne/imp.js
+++ app/cards/nocturne/imp.js
@@ -8,41 +8,40 @@ Imp = class Imp extends Card {
     return 2
   }
 
-  play(game, player_cards) {
+  play(game, player_cards, card_player) {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(2)
 
-    GameModel.update(game._id, game)
-    PlayerCardsModel.update(game._id, player_cards)
-
-    let in_play_card_names = _.map(player_cards.playing.concat(player_cards.in_play).concat(player_cards.duration).concat(player_cards.permanent), 'name')
-    let eligible_cards = _.filter(player_cards.hand, function(card) {
+    let in_play_card_names = _.map(player_cards.in_play, 'name')
+    let eligible_cards = _.filter(player_cards.hand, (card) => {
       return _.includes(_.words(card.types), 'action') && !_.includes(in_play_card_names, card.name)
     })
 
     if (_.size(eligible_cards) > 0) {
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
         username: player_cards.username,
         type: 'choose_cards',
         player_cards: true,
-        instructions: 'Choose an action to play (Or none to skip):',
+        instructions: 'Choose an action to play: (or none to skip)',
         cards: eligible_cards,
         minimum: 0,
         maximum: 1
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-      turn_event_processor.process(Conclave.play_card)
+      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, card_player.card)
+      turn_event_processor.process(Imp.play_card)
     } else {
       game.log.push(`&nbsp;&nbsp;but does not play an action`)
     }
   }
 
-  static play_card(game, player_cards, selected_cards) {
+  static play_card(game, player_cards, selected_cards, imp) {
     if (!_.isEmpty(selected_cards)) {
-      let card_player = new CardPlayer(game, player_cards, selected_cards[0].id, true)
-      card_player.play()
+      let card_player = new CardPlayer(game, player_cards, selected_cards[0], imp)
+      card_player.play(true)
     } else {
       game.log.push(`&nbsp;&nbsp;but does not play an action`)
     }
