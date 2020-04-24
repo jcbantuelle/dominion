4b2bd15feb156687f10d339e0a3c0daddc6d1e23
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 21:08:13 -0500

    Refactor Lost In The Woods

--- app/cards/nocturne/states/lost_in_the_woods.js
+++ app/cards/nocturne/states/lost_in_the_woods.js
@@ -1,45 +1,32 @@
 LostInTheWoods = class LostInTheWoods extends State {
 
-  start_turn_event(game, player_cards) {
+  start_turn_event(game, player_cards, lost_in_the_woods) {
     let turn_event_id = TurnEventModel.insert({
       game_id: game._id,
       player_id: player_cards.player_id,
       username: player_cards.username,
-      type: 'choose_yes_no',
-      instructions: `Discard a card to receive a boon?`,
-      minimum: 1,
+      type: 'choose_cards',
+      player_cards: true,
+      instructions: `Choose a card to discard for ${CardView.render(lost_in_the_woods)}: (or none to skip)`,
+      cards: player_cards.hand,
+      minimum: 0,
       maximum: 1
-    })
-    let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
-    turn_event_processor.process(LostInTheWoods.receive_boon)
-  }
-
-  static receive_boon(game, player_cards, response) {
-    if (response === 'yes') {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: 'Choose a card to discard:',
-        cards: player_cards.hand,
-        minimum: 1,
-        maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(LostInTheWoods.discard_card)
-    }
   }
 
   static discard_card(game, player_cards, selected_cards) {
-    let card_discarder = new CardDiscarder(game, player_cards, 'hand', selected_cards)
-    card_discarder.discard()
+    if (!_.isEmpty(selected_cards)) {
+      let card_discarder = new CardDiscarder(game, player_cards, 'hand', selected_cards)
+      card_discarder.discard()
 
-    PlayerCardsModel.update(game._id, player_cards)
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
 
-    let boon_receiver = new EffectReceiver(game, player_cards, 'boon')
-    boon_receiver.receive()
+      let boon_receiver = new EffectReceiver(game, player_cards, 'boon')
+      boon_receiver.receive()
+    }
   }
 
 }
