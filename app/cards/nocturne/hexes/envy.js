4b667e29e04041a6016ae3991e7bbcfc87585cdb
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 21:48:47 -0500

    Refactor Envy

--- app/cards/nocturne/hexes/envy.js
+++ app/cards/nocturne/hexes/envy.js
@@ -7,11 +7,11 @@ Envy = class Envy extends Hex {
     if (existing_state) {
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> already has ${CardView.render(existing_state)}`)
     } else {
-      let envious_index = _.findIndex(game.states, function(state) {
+      let envious = _.find(game.states, function(state) {
         return state.name === 'Envious'
       })
-      let envious = game.states.splice(envious_index, 1)[0]
-      player_cards.states.push(envious)
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move(game.states, player_cards.states, envious)
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> takes ${CardView.render(envious)}`)
     }
   }
