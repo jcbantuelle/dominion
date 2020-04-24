f70c5ec2e48c00c81c10e1b887e6444ac873ba73
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-23 21:08:24 -0500

    Refactor Fool

--- app/cards/nocturne/fool.js
+++ app/cards/nocturne/fool.js
@@ -9,31 +9,29 @@ Fool = class Fool extends Card {
   }
 
   play(game, player_cards) {
-    let lost_in_the_woods_index = _.findIndex(player_cards.states, function(state) {
-      return state.name === 'Lost In The Woods'
-    })
+    this.source = player_cards.states
+    let lost_in_the_woods = this.find_lost_in_the_woods()
 
-    if (lost_in_the_woods_index === -1) {
-      let lost_in_the_woods = null
-      let game_lost_in_the_woods_index = _.findIndex(game.states, function(state) {
-        return state.name === 'Lost In The Woods'
-      })
-      if (game_lost_in_the_woods_index === -1) {
+    if (!lost_in_the_woods) {
+      this.source = game.states
+      lost_in_the_woods = this.find_lost_in_the_woods()
+      if (!lost_in_the_woods) {
         let ordered_player_cards = TurnOrderedPlayerCardsQuery.turn_ordered_player_cards(game, player_cards)
         ordered_player_cards.shift()
-        _.each(ordered_player_cards, function(other_player_cards) {
-          let other_player_lost_in_the_woods_index = _.findIndex(other_player_cards.states, function(state) {
-            return state.name === 'Lost In The Woods'
-          })
-          if (other_player_lost_in_the_woods_index !== -1) {
-            lost_in_the_woods = other_player_cards.states.splice(other_player_lost_in_the_woods_index, 1)[0]
+        _.each(ordered_player_cards, (next_player_cards) => {
+          this.next_player_cards = next_player_cards
+          this.source = next_player_cards.states
+          lost_in_the_woods = this.find_lost_in_the_woods()
+          if (lost_in_the_woods) {
+            return false
           }
-          PlayerCardsModel.update(game._id, other_player_cards)
         })
-      } else {
-        lost_in_the_woods = game.states.splice(game_lost_in_the_woods_index, 1)[0]
       }
-      player_cards.states.push(lost_in_the_woods)
+      let card_mover = new CardMover(game, player_cards)
+      card_mover.move(this.source, player_cards.states, lost_in_the_woods)
+      if (this.next_player_cards) {
+        PlayerCardsModel.update(game._id, this.next_player_cards)
+      }
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> takes ${CardView.render(lost_in_the_woods)}`)
 
       revealed_boons = _.take(game.boons_deck, 3)
@@ -48,62 +46,55 @@ Fool = class Fool extends Card {
       }
       game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> reveals ${CardView.render(revealed_boons)}`)
       GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
 
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        instructions: 'Choose which Boon to receive next:',
-        cards: revealed_boons,
-        minimum: 1,
-        maximum: 1
+      _.times(3, () => {
+        let choice = revealed_boons[0]
+        if (_.size(revealed_boons) > 1) {
+          let turn_event_id = TurnEventModel.insert({
+            game_id: game._id,
+            player_id: player_cards.player_id,
+            username: player_cards.username,
+            type: 'choose_cards',
+            player_cards: true,
+            instructions: 'Choose which Boon to receive next:',
+            cards: revealed_boons,
+            minimum: 1,
+            maximum: 1
+          })
+          let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
+          choice = turn_event_processor.process(Fool.choose_boon)
+        }
+        game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> receives ${CardView.render(choice)}`)
+        GameModel.update(game._id, game)
+
+        let keep = ClassCreator.create(choice.name).receive(game, player_cards)
+        if (keep) {
+          player_cards.boons.push(choice)
+        } else {
+          game.boons_discard.unshift(choice)
+        }
+        GameModel.update(game._id, game)
+        PlayerCardsModel.update(game._id, player_cards)
+
+        let choice_index = _.findIndex(revealed_boons, function(boon) {
+          return choice.id === boon.id
+        })
+        revealed_boons.splice(choice_index, 1)
       })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, revealed_boons)
-      turn_event_processor.process(Fool.receive_boon)
     } else {
-      game.log.push(`&nbsp;&nbsp;but already has ${CardView.render(player_cards.states[lost_in_the_woods_index])}`)
+      game.log.push(`&nbsp;&nbsp;but already has ${CardView.render(lost_in_the_woods)}`)
     }
   }
 
-  static receive_boon(game, player_cards, selected_cards, revealed_boons) {
-    let selected_boon = selected_cards[0]
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> receives ${CardView.render(selected_boon)}`)
-    GameModel.update(game._id, game)
-    let boon = ClassCreator.create(selected_boon.name)
-    let keep_boon = boon.receive(game, player_cards)
-    if (keep_boon) {
-      player_cards.boons.push(selected_boon)
-    } else {
-      game.boons_discard.unshift(selected_boon)
-    }
-    PlayerCardsModel.update(game._id, player_cards)
-    GameModel.update(game._id, game)
-
-    let selected_boon_index = _.findIndex(revealed_boons, function(revealed_boon) {
-      return revealed_boon.id === selected_boon.id
+  find_lost_in_the_woods() {
+    return _.find(this.source, (state) => {
+      return state.name === 'Lost In The Woods'
     })
-    revealed_boons.splice(selected_boon_index, 1)
+  }
 
-    if (_.size(revealed_boons) > 1) {
-      let turn_event_id = TurnEventModel.insert({
-        game_id: game._id,
-        player_id: player_cards.player_id,
-        username: player_cards.username,
-        type: 'choose_cards',
-        player_cards: true,
-        show_images: true,
-        instructions: 'Choose which Boon to receive next:',
-        cards: revealed_boons,
-        minimum: 1,
-        maximum: 1
-      })
-      let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id, revealed_boons)
-      turn_event_processor.process(Fool.receive_boon)
-    } else if (_.size(revealed_boons) === 1) {
-      Fool.receive_boon(game, player_cards, revealed_boons, revealed_boons)
-    }
+  static choose_boon(game, player_cards, selected_cards) {
+    return selected_cards[0]
   }
 
 }
