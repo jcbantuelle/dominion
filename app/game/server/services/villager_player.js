f0a9dabdd041235759fb10414294942438bf6dbc
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 23:29:55 -0500

    Fix Villager Player Bug

--- app/game/server/services/villager_player.js
+++ app/game/server/services/villager_player.js
@@ -9,7 +9,7 @@ VillagerPlayer = class VillagerPlayer {
     if (this.can_play()) {
       this.game.log.push(`<strong>${this.player_cards.username}</strong> plays a Villager`)
       this.player_cards.villagers -= 1
-      let action_gainer = new ActionGainer(game, player_cards)
+      let action_gainer = new ActionGainer(this.game, this.player_cards)
       action_gainer.gain(1)
       GameModel.update(this.game._id, this.game)
       PlayerCardsModel.update(this.game._id, this.player_cards)
