af9204d37a520521e4415624a11cd4cf1dc07511
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-25 18:57:52 -0500

    Fix Project Popover Width Bug

--- app/game/server/view_objects/card_view.js
+++ app/game/server/view_objects/card_view.js
@@ -23,7 +23,7 @@ CardView = class CardView {
     let width = 220
     let height = 341
     types = _.words(types)
-    if (_.includes(types, 'boon') || _.includes(types, 'state') || _.includes(types, 'hex') || _.includes(types, 'artifact') || _.includes(types, 'event') || _.includes(types, 'landmark')) {
+    if (_.includes(types, 'boon') || _.includes(types, 'state') || _.includes(types, 'hex') || _.includes(types, 'artifact') || _.includes(types, 'event') || _.includes(types, 'landmark') || _.includes(types, 'project')) {
       width = 341
       height = 220
     }
