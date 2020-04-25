04a90d811221ea99cf6459be9c444781d25b4b05
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-25 00:03:39 -0500

    Refactor Recruiter

--- app/cards/renaissance/recruiter.js
+++ app/cards/renaissance/recruiter.js
@@ -12,9 +12,9 @@ Recruiter = class Recruiter extends Card {
     let card_drawer = new CardDrawer(game, player_cards)
     card_drawer.draw(2)
 
-    PlayerCardsModel.update(game._id, player_cards)
-
     if (_.size(player_cards.hand) > 1) {
+      GameModel.update(game._id, game)
+      PlayerCardsModel.update(game._id, player_cards)
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -36,24 +36,12 @@ Recruiter = class Recruiter extends Card {
   }
 
   static trash_card(game, player_cards, selected_cards) {
-    let trashed_card = selected_cards[0]
-    if (trashed_card) {
-      let coin_cost = CostCalculator.calculate(game, trashed_card)
+    let coin_cost = CostCalculator.calculate(game, selected_cards[0])
 
-      let card_trasher = new CardTrasher(game, player_cards, 'hand', trashed_card)
-      card_trasher.trash()
+    let card_trasher = new CardTrasher(game, player_cards, 'hand', selected_cards)
+    card_trasher.trash()
 
-      if (game.turn.player._id === player_cards.player_id) {
-        if (game.turn.possessed) {
-          possessing_player_cards = PlayerCardsModel.findOne(game._id, game.turn.possessed._id)
-          possessing_player_cards.villagers += coin_cost
-          game.log.push(`&nbsp;&nbsp;<strong>${possessing_player_cards.username}</strong> gets +${coin_cost} villagers`)
-          PlayerCardsModel.update(game._id, possessing_player_cards)
-        } else {
-          player_cards.villagers += coin_cost
-          game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +${coin_cost} villagers`)
-        }
-      }
-    }
+    let villager_gainer = new VillagerGainer(game, player_cards)
+    villager_gainer.gain(coin_cost)
   }
 }
