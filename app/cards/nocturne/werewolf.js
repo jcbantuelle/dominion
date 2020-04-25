6beb93cbd56075d5535b706b0c722443615080d9
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 20:09:47 -0500

    Refactor Werewolf

--- app/cards/nocturne/werewolf.js
+++ app/cards/nocturne/werewolf.js
@@ -15,25 +15,23 @@ Werewolf = class Werewolf extends Card {
         game.hexes_discard = []
       }
 
-      game.turn.werewolf_hex = game.hexes_deck.shift()
-      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> draws ${CardView.render(game.turn.werewolf_hex)} from the Hex Deck`)
+      hex = game.hexes_deck.shift()
+      game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> draws ${CardView.render(hex)} from the Hex Deck`)
 
       let player_attacker = new PlayerAttacker(game, this)
-      player_attacker.attack(player_cards)
+      player_attacker.attack(player_cards, hex)
 
-      game.hexes_discard.push(game.turn.werewolf_hex)
-      delete game.turn.werewolf_hex
+      game.hexes_discard.push(hex)
     } else {
       let card_drawer = new CardDrawer(game, player_cards)
       card_drawer.draw(3)
     }
   }
 
-  attack(game, player_cards) {
-    let hex = ClassCreator.create(game.turn.werewolf_hex.name)
+  attack(game, player_cards, attacker_player_cards, card_player, hex) {
     game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> receives ${CardView.render(hex)}`)
     GameModel.update(game._id, game)
-    hex.receive(game, player_cards)
+    ClassCreator.create(hex.name).receive(game, player_cards)
   }
 
 }
