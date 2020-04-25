e433e44a5da2c94b7d5e501e59af215cb082ad17
Justin Bantuelle <justin@mudbugmedia.com>
2020-04-24 23:41:31 -0500

    Refactor Mountain Village

--- app/cards/renaissance/mountain_village.js
+++ app/cards/renaissance/mountain_village.js
@@ -9,12 +9,10 @@ MountainVillage = class MountainVillage extends Card {
   }
 
   play(game, player_cards) {
-    game.turn.actions += 2
-    game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> gets +2 actions`)
+    let action_gainer = new ActionGainer(game, player_cards)
+    action_gainer.gain(2)
 
-    let discarded_cards = player_cards.discard
-
-    if (_.size(discarded_cards) > 0) {
+    if (_.size(player_cards.discard) > 1) {
       let turn_event_id = TurnEventModel.insert({
         game_id: game._id,
         player_id: player_cards.player_id,
@@ -22,12 +20,14 @@ MountainVillage = class MountainVillage extends Card {
         type: 'choose_cards',
         player_cards: true,
         instructions: 'Choose a card to put in hand:',
-        cards: discarded_cards,
+        cards: player_cards.discard,
         minimum: 1,
         maximum: 1
       })
       let turn_event_processor = new TurnEventProcessor(game, player_cards, turn_event_id)
       turn_event_processor.process(MountainVillage.put_in_hand)
+    } else if (_.size(player_cards.discard) === 1) {
+      MountainVillage.put_in_hand(game, player_cards, player_cards.discard)
     } else {
       game.log.push(`&nbsp;&nbsp;but the discard pile is empty`)
       let card_drawer = new CardDrawer(game, player_cards)
@@ -36,13 +36,8 @@ MountainVillage = class MountainVillage extends Card {
   }
 
   static put_in_hand(game, player_cards, selected_cards) {
-    let selected_card = selected_cards[0]
-    let card_index = _.findIndex(player_cards.discard, function (card) {
-      return card.id === selected_card.id
-    })
-    player_cards.hand.unshift(player_cards.discard[card_index])
-    player_cards.discard.splice(card_index, 1)
-
+    let card_mover = new CardMover(game, player_cards)
+    card_mover.move(player_cards.discard, player_cards.hand, selected_cards[0])
     game.log.push(`&nbsp;&nbsp;<strong>${player_cards.username}</strong> puts a card in hand from their discard`)
   }
 
