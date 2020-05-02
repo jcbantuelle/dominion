0f1fced45ce45f39c65a0561a42de18e8ede539c
Justin Bantuelle <justin@mudbugmedia.com>
2020-05-02 17:01:48 -0500

    Update Base Set CardDrawer For Way of the Chameleon

--- app/cards/base/poacher.js
+++ app/cards/base/poacher.js
@@ -12,14 +12,14 @@ Poacher = class Poacher extends Card {
     return 4
   }
 
-  play(game, player_cards) {
-    let card_drawer = new CardDrawer(game, player_cards)
+  play(game, player_cards, card_player) {
+    let card_drawer = new CardDrawer(game, player_cards, card_player)
     card_drawer.draw(1)
 
     let action_gainer = new ActionGainer(game, player_cards)
     action_gainer.gain(1)
 
-    let coin_gainer = new CoinGainer(game, player_cards)
+    let coin_gainer = new CoinGainer(game, player_cards, card_player)
     coin_gainer.gain(1)
 
     let number_to_discard = _.size(_.filter(game.cards, (game_card) => {
