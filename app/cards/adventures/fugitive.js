58c66f49999d24b15f816268f8aa44f98b4b1510
Justin Bantuelle <justin@mudbugmedia.com>
2020-05-02 16:44:34 -0500

    Update Fugitive For Way of the Chameleon

--- app/cards/adventures/fugitive.js
+++ app/cards/adventures/fugitive.js
@@ -8,8 +8,8 @@ Fugitive = class Fugitive extends Traveller {
     return 4
   }
 
-  play(game, player_cards) {
-    let card_drawer = new CardDrawer(game, player_cards)
+  play(game, player_cards, card_player) {
+    let card_drawer = new CardDrawer(game, player_cards, card_player)
     card_drawer.draw(2)
 
     let action_gainer = new ActionGainer(game, player_cards)
