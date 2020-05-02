81984d4da6bd3e2b858d03b393c64ceeb52c8553
Justin Bantuelle <justin@mudbugmedia.com>
2020-05-02 13:21:49 -0500

    Fix CardMover Bug

--- app/game/server/services/card_mover.js
+++ app/game/server/services/card_mover.js
@@ -35,16 +35,18 @@ CardMover = class CardMover {
       return pile.stack_name === supply_name
     })
     let return_count = 0
-    _.each(cards, (card) => {
-      let card_index = this.card_index(source, card)
-      if (card_index !== -1) {
-        source.splice(card_index, 1)
-        supply_pile.count += 1
-        supply_pile.stack.splice(0, 0, card)
-        supply_pile.top_card = card
-        return_count += 1
-      }
-    })
+    if (supply_pile) {
+      _.each(cards, (card) => {
+        let card_index = this.card_index(source, card)
+        if (card_index !== -1) {
+          source.splice(card_index, 1)
+          supply_pile.count += 1
+          supply_pile.stack.splice(0, 0, card)
+          supply_pile.top_card = card
+          return_count += 1
+        }
+      })
+    }
     return return_count
   }
 
