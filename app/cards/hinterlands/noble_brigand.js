1c539849b8c4cc6e9ed71776462067e7487c2a7d
Justin Bantuelle <justin@mudbugmedia.com>
2020-05-03 09:01:23 -0500

    Update Hinterlands Set CoinGainer For Way of the Chameleon

--- app/cards/hinterlands/noble_brigand.js
+++ app/cards/hinterlands/noble_brigand.js
@@ -12,8 +12,8 @@ NobleBrigand = class NobleBrigand extends Card {
     return 4
   }
 
-  play(game, player_cards) {
-    let coin_gainer = new CoinGainer(game, player_cards)
+  play(game, player_cards, card_player) {
+    let coin_gainer = new CoinGainer(game, player_cards, card_player)
     coin_gainer.gain(1)
 
     game.turn.trashed_treasures = []
