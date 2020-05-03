2ec7e76208d7ac607e6ad022280835992843e20d
Justin Bantuelle <justin@mudbugmedia.com>
2020-05-02 19:22:22 -0500

    Update Renaissance Set CardDrawer For Way of the Chameleon

--- app/cards/renaissance/recruiter.js
+++ app/cards/renaissance/recruiter.js
@@ -8,8 +8,8 @@ Recruiter = class Recruiter extends Card {
     return 5
   }
 
-  play(game, player_cards) {
-    let card_drawer = new CardDrawer(game, player_cards)
+  play(game, player_cards, card_player) {
+    let card_drawer = new CardDrawer(game, player_cards, card_player)
     card_drawer.draw(2)
 
     if (_.size(player_cards.hand) > 1) {
